f464c95b466b5536ce37e1217d709fc362a09366
Bruno Barbieri <brunobar79@gmail.com>
2022-09-29 14:49:27 -0400

    add live reload

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,5 +1,11 @@
 INPUT=$1
 OUTPUT=$2
+LIVE_RELOAD=$3
 
-
-watchify $INPUT -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -o $OUTPUT
+if [[ $LIVE_RELOAD == true ]]; then
+    echo "Watching $INPUT with live reload enabled on PORT $PORT"
+    watchify $INPUT -t [ babelify --presets [ @babel/preset-env @babel/preset-react  ] ] -p [tsify --noImplicitAny] -p  [ browserify-hmr --disableHostCheck ]  -o $OUTPUT
+else
+    echo "Watching $INPUT"
+    watchify $INPUT -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -o $OUTPUT
+fi
