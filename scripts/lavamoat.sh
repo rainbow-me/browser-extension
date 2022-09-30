dc0962fb6d89f09a2d5f5a0c574b8c8e39177f0f
Bruno Barbieri <brunobar79@gmail.com>
2022-09-29 22:11:28 -0400

    fix overrides

--- scripts/lavamoat.sh
+++ scripts/lavamoat.sh
@@ -9,11 +9,11 @@ for FILE in "${FILES[@]}"
 do
     if [[ $BUILD == true ]]; then
         echo "Bundling with LavaMoat..."
-        browserify src/scripts/$FILE.ts -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -p [ lavamoat-browserify  --autopolicy --policy "./lavamoat/browserify/policy-$FILE.json" ] > build/$FILE.js
+        browserify src/scripts/$FILE.ts -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -p [ lavamoat-browserify  --autopolicy --policy "./lavamoat/browserify/policy-$FILE.json" --override "./lavamoat/browserify/policy-$FILE-override.json" ] > build/$FILE.js
         sed -i '' 's/\$h‍_/\$h_/g' build/$FILE.js
     else
         echo "Updating LavaMoat policies..."
-        browserify src/scripts/$FILE.ts -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -p [ lavamoat-browserify  --autopolicy --policy "./lavamoat/browserify/policy-$FILE.json" ] > /dev/null
+        browserify src/scripts/$FILE.ts -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] -p [ lavamoat-browserify  --autopolicy --policy "./lavamoat/browserify/policy-$FILE.json" --override "./lavamoat/browserify/policy-$FILE-override.json" ] > /dev/null
     fi
 done
 
