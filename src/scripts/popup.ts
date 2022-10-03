e8755923b5d1c8e5b3da52c84944ffb5baf27bdd
moxey.eth <jakemoxey@gmail.com>
2022-10-03 13:52:45 +1100

    vanilla extract

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,6 +1,7 @@
 import { createElement } from 'react';
 import ReactDOM from 'react-dom';
 import { Popup } from '../pages/popup';
+import '../styles/global.css';
 
 const domContainer = document.querySelector('#app');
 ReactDOM.render(createElement(Popup), domContainer);
