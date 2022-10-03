d149eaef3d60d6a3edf211420b7e14aee34f109f
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2022-10-03 15:44:00 -0400

    Add basic lavamoat support (#1)
    * lavamoat initial config
    
    * Update lavamoat/browserify/policy-popup.json
    
    * preserve override
    
    * create overrides
    
    * fix overrides
    
    * extension working. disabled lavamoat contentscript and provider

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,6 +1,4 @@
 INPUT=$1
 OUTPUT=$2
 
-browserify $INPUT -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] --plugin [ lavamoat-browserify ] > $OUTPUT
-
-sed -i '' 's/\$h‍_/\$h_/g' $OUTPUT
+browserify $INPUT -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] ] -p [ tsify --noImplicitAny ] > $OUTPUT
