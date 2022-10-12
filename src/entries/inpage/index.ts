3f827200b3a8995a2e45424cde2dcb76b47a2905
moxey.eth <jakemoxey@gmail.com>
2022-10-12 12:08:10 +1100

    remove window.rainbow

--- src/entries/inpage/index.ts
+++ src/entries/inpage/index.ts
@@ -3,13 +3,10 @@ import { RainbowProvider } from '~/core/providers';
 declare global {
   interface Window {
     ethereum?: RainbowProvider;
-    rainbow?: boolean;
   }
 }
 
 const provider = new RainbowProvider();
-
-window.rainbow = true;
 window.ethereum = provider;
 
 console.log('injection complete in window');
