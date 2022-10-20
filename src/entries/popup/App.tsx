630052f29baa503e1266c24d020274e6536526bb
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2022-10-20 12:02:23 -0400

    Logger + Sentry setup (#44)
    * add sentry
    
    * add sentry to background and popup
    
    * add logger interface from mobile app
    
    * clean up

--- src/entries/popup/App.tsx
+++ src/entries/popup/App.tsx
@@ -9,6 +9,7 @@ import { persistOptions, queryClient } from '~/core/react-query';
 import { RainbowConnector } from './wagmi/RainbowConnector';
 import { PlaygroundComponents } from './pages/_playgrounds';
 import { Routes } from './Routes';
+import { initializeSentry } from '~/core/sentry';
 
 const playground = process.env.PLAYGROUND as 'default' | 'ds';
 
@@ -19,6 +20,10 @@ const wagmiClient = createWagmiClient({
 });
 
 export function App() {
+  React.useEffect(() => {
+    initializeSentry();
+  }, []);
+
   return (
     <PersistQueryClientProvider
       client={queryClient}
