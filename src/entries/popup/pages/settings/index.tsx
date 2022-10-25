57b472bfb6bfde3794c77b318e3194a5fb995b35
Nico Glennon <nicoglennon@icloud.com>
2022-10-25 14:32:08 -0400

    chore: add lang button to settings

--- src/entries/popup/pages/settings/index.tsx
+++ src/entries/popup/pages/settings/index.tsx
@@ -2,9 +2,12 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Language } from '~/core/languages';
+import { useCurrentLanguageStore } from '~/core/state/currentLanguage';
 import { Box, Text } from '~/design-system';
 
 export function Settings() {
+  const { currentLanguage, setCurrentLanguage } = useCurrentLanguageStore();
   return (
     <Box
       as={motion.div}
@@ -20,6 +23,34 @@ export function Settings() {
       <Text as="h1" size="20pt" weight="bold">
         Settings
       </Text>
+
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Text size="14pt" weight="bold">
+          Language: {currentLanguage}
+        </Text>
+        <Box
+          as="button"
+          background="surfaceSecondary"
+          padding="16px"
+          style={{ borderRadius: 999 }}
+          onClick={() =>
+            setCurrentLanguage(
+              [Language.EN, Language.ES, Language.FR, Language.PR][
+                Math.round(Math.random() * 10) % 4
+              ],
+            )
+          }
+        >
+          <Text color="labelSecondary" size="14pt" weight="bold">
+            Toggle
+          </Text>
+        </Box>
+      </Box>
 
       <Link to="/">
         <Box
