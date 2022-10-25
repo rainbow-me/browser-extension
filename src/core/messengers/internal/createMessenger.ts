689bf81a561f3eb0a5955f91a958b6e6ceb868d0
moxey.eth <jakemoxey@gmail.com>
2022-10-26 10:28:55 +1100

    Add ability for messengers to be scoped via an id

--- src/core/messengers/internal/createMessenger.ts
+++ src/core/messengers/internal/createMessenger.ts
@@ -1,6 +1,10 @@
 export type CallbackOptions = {
+  /** The sender of the message. */
   sender: chrome.runtime.MessageSender;
+  /** The topic provided. */
   topic: string;
+  /** An optional scoped identifier. */
+  id?: number;
 };
 
 export type CallbackFunction<TPayload, TResponse> = (
@@ -11,13 +15,24 @@ export type CallbackFunction<TPayload, TResponse> = (
 export type Source = 'background' | 'content' | 'inpage' | 'popup';
 
 export type Messenger = {
+  /** Whether or not the messenger is available in the context. */
   available: boolean;
+  /** Name of the messenger */
   name: string;
+  /** Sends a message to the `reply` handler. */
   send: <TPayload, TResponse>(
+    /** A scoped topic that the `reply` will listen for. */
     topic: string,
+    /** The payload to send to the `reply` handler. */
     payload: TPayload,
+    options?: {
+      /** Identify & scope the request via an ID. */
+      id?: string | number;
+    },
   ) => Promise<TResponse>;
+  /** Replies to `send`. */
   reply: <TPayload, TResponse>(
+    /** A scoped topic that was sent from `send`. */
     topic: string,
     callback: CallbackFunction<TPayload, TResponse>,
   ) => () => void;
