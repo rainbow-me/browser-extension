1468733c4a923dc3ba73ecda72849e0ae13cb7e0
derHowie <derhowiedesigns@gmail.com>
2022-10-26 16:24:40 -0500

    intro ParsedAddressAsset

--- src/core/references/index.ts
+++ src/core/references/index.ts
@@ -1,6 +1,24 @@
+import { ChainName } from '../types/chains';
+
 export { supportedCurrencies } from './supportedCurrencies';
 export type {
   SupportedCurrency,
   SupportedCurrencyKey,
 } from './supportedCurrencies';
 export const ETH_ADDRESS = 'eth';
+export const ARBITRUM_ETH_ADDRESS =
+  '0x0000000000000000000000000000000000000000';
+export const BNB_BSC_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const OPTIMISM_ETH_ADDRESS =
+  '0x0000000000000000000000000000000000000000';
+export const MATIC_MAINNET_ADDRESS =
+  '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0';
+export const MATIC_POLYGON_ADDRESS =
+  '0x0000000000000000000000000000000000001010';
+export const NATIVE_ASSETS_PER_NETWORK = {
+  [ChainName.arbitrum]: ARBITRUM_ETH_ADDRESS,
+  [ChainName.bsc]: BNB_BSC_ADDRESS,
+  [ChainName.mainnet]: ETH_ADDRESS,
+  [ChainName.optimism]: OPTIMISM_ETH_ADDRESS,
+  [ChainName.polygon]: MATIC_POLYGON_ADDRESS,
+};
--- src/core/utils/chains.ts
+++ src/core/utils/chains.ts
@@ -1,11 +1,12 @@
 import { ChainName } from '~/core/types/chains';
+import { NATIVE_ASSETS_PER_NETWORK } from '~/core/references';
 
 /**
  * @desc Checks if the given chain is a Layer 2.
  * @param chain The chain name to check.
  * @return Whether or not the chain is an L2 network.
  */
-export const isL2Chain = (chain: ChainName): boolean => {
+export const isL2Chain = (chain: string): boolean => {
   switch (chain) {
     case ChainName.arbitrum:
     case ChainName.bsc:
@@ -16,3 +17,9 @@ export const isL2Chain = (chain: ChainName): boolean => {
       return false;
   }
 };
+
+export function isNativeAsset(address: string, network: ChainName) {
+  return (
+    NATIVE_ASSETS_PER_NETWORK[network]?.toLowerCase() === address?.toLowerCase()
+  );
+}
