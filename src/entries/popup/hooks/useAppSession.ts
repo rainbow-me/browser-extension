03d08742dc7bfce1a27335adc44e1b2de6f6df95
Esteban Miño <efmino@uc.cl>
2022-11-01 14:40:30 -0300

    rn DappSession to AppSession everywhere

--- src/entries/popup/hooks/useAppSession.ts
+++ src/entries/popup/hooks/useAppSession.ts
@@ -3,22 +3,22 @@ import { Address } from 'wagmi';
 
 import { useAppSessionsStore } from '~/core/state';
 
-export function useDappSession({ host }: { host: string }) {
+export function useAppSession({ host }: { host: string }) {
   const { updateSessionAddress, updateSessionChainId } = useAppSessionsStore();
 
-  const updateDappSessionAddress = React.useCallback(
+  const updateAppSessionAddress = React.useCallback(
     (address: Address) => {
       updateSessionAddress({ host, address });
     },
     [host, updateSessionAddress],
   );
 
-  const updateDappSessionChainId = React.useCallback(
+  const updateAppSessionChainId = React.useCallback(
     (chainId: number) => {
       updateSessionChainId({ host, chainId });
     },
     [host, updateSessionChainId],
   );
 
-  return { updateDappSessionAddress, updateDappSessionChainId };
+  return { updateAppSessionAddress, updateAppSessionChainId };
 }
