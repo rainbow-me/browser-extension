4caff401fb9556bd157b16bab5f61c72a9890176
Esteban Miño <efmino@uc.cl>
2022-11-04 12:26:34 -0300

    approve screen almost done

--- src/entries/popup/pages/messages/ApproveRequestAccounts.tsx
+++ src/entries/popup/pages/messages/ApproveRequestAccounts.tsx
@@ -4,13 +4,19 @@ import { useEnsAvatar, useEnsName } from 'wagmi';
 import { ChainType } from '~/core/references';
 import { useCurrentAddressStore } from '~/core/state';
 import { ProviderRequestPayload } from '~/core/transports/providerRequestTransport';
-import { getConnectedAppIcon } from '~/core/utils/connectedApps';
+import {
+  dappLogoOverride,
+  dappNameOverride,
+  getDappHostname,
+} from '~/core/utils/connectedApps';
 import { truncateAddress } from '~/core/utils/truncateAddress';
 import {
   Box,
   Column,
   Columns,
   Inline,
+  Row,
+  Rows,
   Separator,
   Stack,
   Text,
@@ -31,34 +37,49 @@ export function ApproveRequestAccounts({
   request,
 }: ApproveRequestProps) {
   const { currentAddress } = useCurrentAddressStore();
-
   const { data: ensAvatar } = useEnsAvatar({ addressOrName: currentAddress });
   const { data: ensName } = useEnsName({ address: currentAddress });
 
   const meta = request?.meta;
+  const url = meta?.sender.url || '';
 
-  console.log('METAAAA', meta);
-  const host = new URL(meta?.sender.url || '').host;
-  const title = meta?.sender?.tab?.title;
-  const image = getConnectedAppIcon(host);
+  const hostName = getDappHostname(url);
+  const logo = dappLogoOverride(url);
+  const name = dappNameOverride(url);
 
   return (
-    <>
-      <Box paddingHorizontal="52px" paddingTop="60px" paddingBottom="44px">
+    <Box>
+      <Box
+        style={{
+          paddingTop: 64,
+          paddingBottom: 42,
+          paddingLeft: 50,
+          paddingRight: 50,
+        }}
+        background="surfacePrimaryElevatedSecondary"
+      >
         <Stack space="32px">
-          <Box alignItems="center">
-            <Inline alignHorizontal="center">
-              <img src={image} width="60px" height="60px" />
-            </Inline>
-          </Box>
+          <Inline alignHorizontal="center">
+            <Box
+              style={{
+                width: 60,
+                height: 60,
+                overflow: 'hidden',
+              }}
+              borderRadius="round"
+              alignItems="center"
+            >
+              {logo ? <img src={logo} width="60px" height="60px" /> : null}
+            </Box>
+          </Inline>
 
           <Stack space="24px">
             <Text align="center" color="label" size="20pt" weight="semibold">
-              {title} wants to connect to your wallet
+              {name} wants to connect to your wallet
             </Text>
 
             <Text align="center" color="accent" size="14pt" weight="bold">
-              {host}
+              {hostName}
             </Text>
           </Stack>
           <Inline alignHorizontal="center">
@@ -73,106 +94,119 @@ export function ApproveRequestAccounts({
             size="14pt"
             weight="regular"
           >
-            Allow {title} to view your wallets address, balance, activity and
+            Allow {name} to view your wallets address, balance, activity and
             request approval for transactions.
           </Text>
         </Stack>
       </Box>
 
-      <Box background="surfacePrimary" padding="20px">
-        <Columns alignVertical="center" alignHorizontal="justify">
-          <Column width="1/3">
-            <Stack space="8px">
-              <Text size="12pt" weight="semibold" color="labelQuaternary">
-                Wallet
-              </Text>
-              <Inline alignVertical="center" space="4px">
-                <Box
-                  background="fill"
-                  borderRadius="30px"
-                  style={{
-                    width: '18px',
-                    height: '18px',
-                    overflow: 'hidden',
-                  }}
-                >
-                  {ensAvatar && (
-                    /* TODO: Convert to <Image> & Imgix/Cloudinary */
-                    <img
-                      src={ensAvatar}
-                      width="100%"
-                      height="100%"
-                      loading="lazy"
-                    />
-                  )}
-                </Box>
-                <Text size="14pt" weight="semibold" color="labelSecondary">
-                  {ensName || truncateAddress(currentAddress)}
+      <Box padding="20px">
+        <Stack space="24px">
+          <Columns alignVertical="center" alignHorizontal="justify">
+            <Column>
+              <Stack space="8px">
+                <Text size="12pt" weight="semibold" color="labelQuaternary">
+                  Wallet
                 </Text>
-                <SFSymbol
-                  color="labelTertiary"
-                  size={14}
-                  symbol="chevronDownCircle"
-                />
-              </Inline>
-            </Stack>
-          </Column>
-          <Column width="2/3">
-            <Stack space="8px">
-              <Text
-                align="right"
-                size="12pt"
-                weight="semibold"
-                color="labelQuaternary"
-              >
-                Network
-              </Text>
-              <Inline alignVertical="center" space="4px">
-                <ChainBadge chainType={ChainType.arbitrum} size={'small'} />
+                <Box>
+                  <Inline alignVertical="center" space="4px">
+                    <Box
+                      background="fill"
+                      borderRadius="30px"
+                      style={{
+                        width: '18px',
+                        height: '18px',
+                        overflow: 'hidden',
+                      }}
+                    >
+                      {ensAvatar && (
+                        /* TODO: Convert to <Image> & Imgix/Cloudinary */
+                        <img
+                          src={ensAvatar}
+                          width="100%"
+                          height="100%"
+                          loading="lazy"
+                        />
+                      )}
+                    </Box>
+                    <Text size="14pt" weight="semibold" color="labelSecondary">
+                      {ensName || truncateAddress(currentAddress)}
+                    </Text>
+                    <SFSymbol
+                      color="labelSecondary"
+                      size={14}
+                      symbol="chevronDownCircle"
+                    />
+                  </Inline>
+                </Box>
+              </Stack>
+            </Column>
+            <Column>
+              <Stack space="8px">
                 <Text
                   align="right"
-                  size="14pt"
+                  size="12pt"
                   weight="semibold"
-                  color="labelSecondary"
+                  color="labelQuaternary"
                 >
                   Network
                 </Text>
-                <SFSymbol
-                  color="labelTertiary"
-                  size={14}
-                  symbol="chevronDownCircle"
-                />
-              </Inline>
-            </Stack>
-          </Column>
-        </Columns>
+                <Inline
+                  alignHorizontal="right"
+                  alignVertical="center"
+                  space="4px"
+                >
+                  <ChainBadge chainType={ChainType.arbitrum} size={'small'} />
+                  <Text
+                    align="right"
+                    size="14pt"
+                    weight="semibold"
+                    color="labelSecondary"
+                  >
+                    Network
+                  </Text>
+                  <SFSymbol
+                    color="labelSecondary"
+                    size={14}
+                    symbol="chevronDownCircle"
+                  />
+                </Inline>
+              </Stack>
+            </Column>
+          </Columns>
+          <Rows space="8px">
+            <Row>
+              <Box
+                as="button"
+                id="accept-button"
+                background="accent"
+                width="full"
+                onClick={approveRequest}
+                padding="16px"
+                borderRadius="round"
+              >
+                <Text color="label" size="14pt" weight="bold">
+                  Connect to {name}
+                </Text>
+              </Box>
+            </Row>
+            <Row>
+              <Box
+                as="button"
+                id="reject-button"
+                onClick={rejectRequest}
+                width="full"
+                padding="16px"
+                borderRadius="round"
+              >
+                <Text color="labelSecondary" size="14pt" weight="bold">
+                  Cancel
+                </Text>
+              </Box>
+            </Row>
+          </Rows>
+        </Stack>
       </Box>
-      <Box>
-        <Box
-          as="button"
-          id="accept-button"
-          background="surfaceSecondary"
-          onClick={approveRequest}
-          padding="16px"
-          style={{ borderRadius: 999 }}
-        >
-          <Text color="labelSecondary" size="14pt" weight="bold">
-            ACCEPT
-          </Text>
-        </Box>
-        <Box
-          as="button"
-          id="reject-button"
-          background="surfaceSecondary"
-          onClick={rejectRequest}
-          padding="16px"
-          style={{ borderRadius: 999 }}
-        >
-          <Text color="labelSecondary" size="14pt" weight="bold">
-            REJECT
-          </Text>
-        </Box>
-      </Box>
-    </>
+    </Box>
   );
 }
