7f85067f120fd3834cfafe820e739020d7b3b45a
Esteban Miño <efmino@uc.cl>
2022-11-07 16:16:15 -0300

    approval cleanup

--- src/entries/popup/pages/messages/ApproveMessage.tsx
+++ src/entries/popup/pages/messages/ApproveMessage.tsx
@@ -44,26 +44,6 @@ export function ApproveMessage() {
     );
   }
 
-  if (pendingRequest.method === 'eth_requestAccounts') {
-    return (
-      <ApproveRequestAccounts
-        approveRequest={approveRequest}
-        rejectRequest={rejectRequest}
-        request={pendingRequest}
-      />
-    );
-  }
-
-  if (pendingRequest.method === 'eth_requestAccounts') {
-    return (
-      <ApproveRequestAccounts
-        approveRequest={approveRequest}
-        rejectRequest={rejectRequest}
-        request={pendingRequest}
-      />
-    );
-  }
-
   return (
     <>
       <Box padding="16px" style={{ borderRadius: 999 }}>
