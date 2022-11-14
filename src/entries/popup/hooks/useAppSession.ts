a4aed8dc89b76557b1588b9724a6eea6e3af4fa6
jxom <jakemoxey@gmail.com>
2022-11-15 10:17:00 +1100

    refactor: encapsulate events inside useAppSession (#90)

--- src/entries/popup/hooks/useAppSession.ts
+++ src/entries/popup/hooks/useAppSession.ts
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { Address } from 'wagmi';
 
+import { initializeMessenger } from '~/core/messengers';
 import { useAppSessionsStore } from '~/core/state';
+
+const messenger = initializeMessenger({ connect: 'inpage' });
 
 export function useAppSession({ host }: { host: string }) {
   const {
@@ -14,6 +17,7 @@ export function useAppSession({ host }: { host: string }) {
   const updateAppSessionAddress = React.useCallback(
     (address: Address) => {
       updateSessionAddress({ host, address });
+      messenger.send(`accountsChanged:${host}`, [address]);
     },
     [host, updateSessionAddress],
   );
@@ -21,6 +25,7 @@ export function useAppSession({ host }: { host: string }) {
   const updateAppSessionChainId = React.useCallback(
     (chainId: number) => {
       updateSessionChainId({ host, chainId });
+      messenger.send(`chainChanged:${host}`, chainId);
     },
     [host, updateSessionChainId],
   );
@@ -30,10 +35,10 @@ export function useAppSession({ host }: { host: string }) {
     [appSessions, host],
   );
 
-  const disconnectAppSession = React.useCallback(
-    () => removeSession({ host }),
-    [host, removeSession],
-  );
+  const disconnectAppSession = React.useCallback(() => {
+    removeSession({ host });
+    messenger.send(`disconnect:${host}`, null);
+  }, [host, removeSession]);
 
   return {
     updateAppSessionAddress,
