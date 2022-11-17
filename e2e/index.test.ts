3985a1f0b43048c2de60ccbbbf06025a1189a118
Bruno Barbieri <brunobar79@gmail.com>
2022-11-17 14:46:48 -0500

    restore e2e

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -47,7 +47,7 @@ it('should display account name', async () => {
     '[data-testid="header"] [data-testid="account-name"]',
   );
   const actual = await label.getText();
-  const expected = 'djweth.eth';
+  const expected = '0x70c1...43C4';
   expect(actual).toEqual(expected);
 });
 
@@ -57,14 +57,9 @@ it('should shuffle account', async () => {
     driver,
     '[data-testid="header"] [data-testid="account-name"]',
   );
-
-  const accountName = await label.getText();
-  const expectedAddress = '0x70c1...43C4';
-  const expectedName = 'djweth.eth';
-
-  const isCorrectName =
-    accountName === expectedAddress || accountName === expectedName;
-  expect(isCorrectName).toBeTruthy();
+  const actual = await label.getText();
+  const expected = '0x5B57...7C35';
+  expect(actual).toEqual(expected);
 });
 
 it('should be able to connect to bx test dapp', async () => {
