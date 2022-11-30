ed5d826beccdddeb2f64c439a401f9f20fd016bc
Bruno Barbieri <brunobar79@gmail.com>
2022-11-30 16:33:46 -0500

    fix bump script

--- scripts/bump.js
+++ scripts/bump.js
@@ -6,18 +6,17 @@ const manifest = require('../static/manifest.json');
 const currentVersion = valid(pkgJson.version);
 const newVersion = inc(currentVersion, 'patch');
 
-console.log('bumping from %s to %s', currentVersion, newVersion);
+console.log('bumping from v%s to v%s', currentVersion, newVersion);
 
 pkgJson.version = newVersion;
 
 // Update package.json
-require('fs').writeFileSync(
-  '../package.json',
-  JSON.stringify(pkgJson, null, 2),
-);
+require('fs').writeFileSync('./package.json', JSON.stringify(pkgJson, null, 2));
 // Update manifest.json
 manifest.version = newVersion;
 require('fs').writeFileSync(
-  '../static/manifest.json',
+  './static/manifest.json',
   JSON.stringify(manifest, null, 2),
 );
+
+console.log('Done.');
