6930a02e78e2d64076a61fd5f0a19c3d98b77ec4
Esteban Miño <efmino@uc.cl>
2022-12-08 13:30:16 -0300

    send tx state (#145)

---
+++ src/entries/popup/hooks/useSendTransactionState.ts
@@ -0,0 +1,45 @@
+import { useMemo, useState } from 'react';
+import { Address, useAccount } from 'wagmi';
+
+import { ParsedAddressAsset } from '~/core/types/assets';
+import { ChainId } from '~/core/types/chains';
+
+import { useEns } from './useEns';
+
+export const useSendTransactionState = () => {
+  const [toAddressOrName, setToAddressOrName] = useState<Address | string>('');
+  const [asset, setAsset] = useState<ParsedAddressAsset>();
+  const [amount, setAmount] = useState<string>();
+  const [independentField, setIndependentField] = useState<
+    'native' | 'asset'
+  >();
+  const { address: fromAddress } = useAccount();
+
+  const { ensAddress: toAddress, ensName: toEnsName } = useEns({
+    addressOrName: toAddressOrName ?? '',
+  });
+
+  const chainId = useMemo(
+    () => asset?.chainId ?? ChainId.mainnet,
+    [asset?.chainId],
+  );
+
+  const data = useMemo(() => {
+    return undefined;
+  }, []);
+
+  return {
+    toAddressOrName,
+    amount,
+    chainId,
+    data,
+    fromAddress,
+    independentField,
+    toAddress,
+    toEnsName,
+    setAmount,
+    setAsset,
+    setIndependentField,
+    setToAddressOrName,
+  };
+};
