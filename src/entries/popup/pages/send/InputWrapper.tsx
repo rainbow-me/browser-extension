8a392d4709c195bd69c75b7f2da73a720f1e2696
Esteban Miño <efmino@uc.cl>
2022-12-13 17:45:16 -0300

    dropdown animation

--- src/entries/popup/pages/send/InputWrapper.tsx
+++ src/entries/popup/pages/send/InputWrapper.tsx
@@ -1,7 +1,7 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { ReactElement, useState } from 'react';
 
-import { Box, Column, Columns, Text } from '~/design-system';
+import { Box, Column, Columns, Separator, Stack, Text } from '~/design-system';
 
 import { InputActionButon } from './InputActionButton';
 
@@ -19,86 +19,104 @@ export const InputWrapper = ({
   const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <Box
-      background="surfaceSecondaryElevated"
-      paddingVertical="20px"
-      paddingHorizontal="16px"
-      borderRadius="24px"
-      width="full"
-      onClick={() => setShowDropdown(true)}
-      onBlur={() => setShowDropdown(false)}
-    >
-      <Columns alignVertical="center" alignHorizontal="justify" space="8px">
-        <Column width="content">{leftComponent}</Column>
+    <Box style={{ height: 68 }}>
+      <Box
+        width="full"
+        onClick={() => setShowDropdown(true)}
+        onBlur={() => setShowDropdown(false)}
+        position="absolute"
+        paddingHorizontal="12px"
+        style={{ left: 0 }}
+      >
+        <Box
+          background="surfaceSecondaryElevated"
+          borderRadius="24px"
+          paddingHorizontal="20px"
+          paddingVertical="16px"
+        >
+          <Columns alignVertical="center" alignHorizontal="justify" space="8px">
+            <Column width="content">{leftComponent}</Column>
 
-        <Column>
-          <>{centerComponent}</>
-        </Column>
+            <Column>
+              <>{centerComponent}</>
+            </Column>
 
-        <Column width="content">
-          <InputActionButon
-            showClose={showActionClose}
-            onClose={onActionClose}
-          />
-        </Column>
-      </Columns>
-      <AnimatePresence initial={false}>
-        {showDropdown && (
-          <Box
-            as={motion.div}
-            paddingHorizontal="12px"
-            style={{
-              left: 0,
-            }}
-            width="full"
-            position="absolute"
-            key="address-dropdown"
-            initial={{ opacity: 0, height: 0 }}
-            animate={{
-              opacity: 1,
-              height: 'auto',
-              transition: {
-                height: {
-                  duration: 0.4,
-                },
-                opacity: {
-                  duration: 0.25,
-                  delay: 0.15,
-                },
-              },
-            }}
-            exit={{
-              opacity: 0,
-              height: 0,
-              transition: {
-                height: {
-                  duration: 0.25,
-                },
-                opacity: {
-                  duration: 0.25,
-                },
-              },
-            }}
-          >
-            <Box
-              background="surfaceSecondaryElevated"
-              width="full"
-              style={{
-                height: 200,
-                borderEndEndRadius: 24,
-                borderEndStartRadius: 24,
-              }}
-            >
-              <Text weight="bold" size="11pt">
-                AAAAAA
-              </Text>
-              <Text weight="bold" size="11pt">
-                AAAAAA
-              </Text>
-            </Box>
-          </Box>
-        )}
-      </AnimatePresence>
+            <Column width="content">
+              <InputActionButon
+                showClose={showActionClose}
+                onClose={onActionClose}
+              />
+            </Column>
+          </Columns>
+
+          <AnimatePresence initial={false}>
+            {showDropdown && (
+              <Box
+                as={motion.div}
+                paddingHorizontal="12px"
+                style={{
+                  left: 0,
+                }}
+                width="full"
+                background="surfaceSecondaryElevated"
+                key="address-dropdown"
+                // position="absolute"
+                initial={{ opacity: 0, height: 0 }}
+                animate={{
+                  opacity: 1,
+                  height: 'auto',
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.2,
+                      delay: 0.15,
+                    },
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  height: 0,
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.2,
+                    },
+                  },
+                }}
+              >
+                <Box paddingVertical="16px">
+                  <Separator />
+                </Box>
+
+                <Stack space="12px">
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                  <Text weight="bold" size="11pt">
+                    AAAAAA
+                  </Text>
+                </Stack>
+              </Box>
+            )}
+          </AnimatePresence>
+        </Box>
+      </Box>
     </Box>
   );
 };
