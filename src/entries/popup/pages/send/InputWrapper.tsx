3dec216facdccc609e63da218bb779e841b9069b
Esteban Miño <efmino@uc.cl>
2022-12-13 17:07:02 -0300

    dropdown basic animation

--- src/entries/popup/pages/send/InputWrapper.tsx
+++ src/entries/popup/pages/send/InputWrapper.tsx
@@ -1,6 +1,7 @@
-import React, { ReactElement } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { ReactElement, useState } from 'react';
 
-import { Box, Column, Columns } from '~/design-system';
+import { Box, Column, Columns, Text } from '~/design-system';
 
 import { InputActionButon } from './InputActionButton';
 
@@ -15,6 +16,8 @@ export const InputWrapper = ({
   showActionClose: boolean;
   onActionClose: () => void;
 }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <Box
       background="surfaceSecondaryElevated"
@@ -22,11 +25,15 @@ export const InputWrapper = ({
       paddingHorizontal="16px"
       borderRadius="24px"
       width="full"
+      onClick={() => setShowDropdown(true)}
+      onBlur={() => setShowDropdown(false)}
     >
       <Columns alignVertical="center" alignHorizontal="justify" space="8px">
         <Column width="content">{leftComponent}</Column>
 
-        <Column>{centerComponent}</Column>
+        <Column>
+          <>{centerComponent}</>
+        </Column>
 
         <Column width="content">
           <InputActionButon
@@ -35,6 +42,63 @@ export const InputWrapper = ({
           />
         </Column>
       </Columns>
+      <AnimatePresence initial={false}>
+        {showDropdown && (
+          <Box
+            as={motion.div}
+            paddingHorizontal="12px"
+            style={{
+              left: 0,
+            }}
+            width="full"
+            position="absolute"
+            key="address-dropdown"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{
+              opacity: 1,
+              height: 'auto',
+              transition: {
+                height: {
+                  duration: 0.4,
+                },
+                opacity: {
+                  duration: 0.25,
+                  delay: 0.15,
+                },
+              },
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              transition: {
+                height: {
+                  duration: 0.25,
+                },
+                opacity: {
+                  duration: 0.25,
+                },
+              },
+            }}
+          >
+            <Box
+              background="surfaceSecondaryElevated"
+              width="full"
+              style={{
+                height: 200,
+                borderEndEndRadius: 24,
+                borderEndStartRadius: 24,
+              }}
+            >
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+            </Box>
+          </Box>
+        )}
+      </AnimatePresence>
     </Box>
   );
 };
--- src/entries/popup/pages/send/ToAddressInput.tsx
+++ src/entries/popup/pages/send/ToAddressInput.tsx
@@ -32,57 +32,59 @@ export const ToAddressInput = ({
     [toAddressOrName, toEnsName],
   );
   return (
-    <InputWrapper
-      leftComponent={
-        <Box background="fillSecondary" borderRadius="18px">
-          <WalletAvatar address={toAddress} size={36} emojiSize="20pt" />
-        </Box>
-      }
-      centerComponent={
-        <AnimatePresence initial={false} mode="wait">
-          {inputVisible && (
-            <Box
-              as={motion.div}
-              key="input"
-              initial={{ y: 0, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -10, opacity: 0 }}
-            >
-              <Input
-                value={toAddressOrName}
-                placeholder={i18n.t('send.input_to_address_placeholder')}
-                onChange={handleToAddressChange}
-                height="32px"
-                variant="transparent"
-                style={{ paddingLeft: 0, paddingRight: 0 }}
-                innerRef={inputRef}
-              />
-            </Box>
-          )}
-          {!inputVisible && (
-            <Box
-              as={motion.div}
-              key="wallet"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <Stack space="8px">
-                <Text weight="semibold" size="14pt" color="label">
-                  {toEnsName || truncateAddress(toAddress)}
-                </Text>
-                {toEnsName && (
-                  <Text weight="semibold" size="12pt" color="labelTertiary">
-                    {truncateAddress(toAddress)}
+    <>
+      <InputWrapper
+        leftComponent={
+          <Box background="fillSecondary" borderRadius="18px">
+            <WalletAvatar address={toAddress} size={36} emojiSize="20pt" />
+          </Box>
+        }
+        centerComponent={
+          <AnimatePresence initial={false} mode="wait">
+            {inputVisible && (
+              <Box
+                as={motion.div}
+                key="input"
+                initial={{ y: 0, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+              >
+                <Input
+                  value={toAddressOrName}
+                  placeholder={i18n.t('send.input_to_address_placeholder')}
+                  onChange={handleToAddressChange}
+                  height="32px"
+                  variant="transparent"
+                  style={{ paddingLeft: 0, paddingRight: 0 }}
+                  innerRef={inputRef}
+                />
+              </Box>
+            )}
+            {!inputVisible && (
+              <Box
+                as={motion.div}
+                key="wallet"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <Stack space="8px">
+                  <Text weight="semibold" size="14pt" color="label">
+                    {toEnsName || truncateAddress(toAddress)}
                   </Text>
-                )}
-              </Stack>
-            </Box>
-          )}
-        </AnimatePresence>
-      }
-      showActionClose={!!toAddress}
-      onActionClose={clearToAddress}
-    />
+                  {toEnsName && (
+                    <Text weight="semibold" size="12pt" color="labelTertiary">
+                      {truncateAddress(toAddress)}
+                    </Text>
+                  )}
+                </Stack>
+              </Box>
+            )}
+          </AnimatePresence>
+        }
+        showActionClose={!!toAddress}
+        onActionClose={clearToAddress}
+      />
+    </>
   );
 };
