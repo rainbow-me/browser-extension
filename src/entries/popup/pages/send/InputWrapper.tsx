c47ada4d8c3047cb9218a1b6c3a52f02b168e5bc
Esteban Miño <efmino@uc.cl>
2022-12-14 12:04:23 -0300

    dropdown animation looking good

--- src/entries/popup/pages/send/InputWrapper.tsx
+++ src/entries/popup/pages/send/InputWrapper.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import React, { ReactElement, useState } from 'react';
 
 import { Box, Column, Columns, Separator, Stack, Text } from '~/design-system';
@@ -17,7 +17,6 @@ export const InputWrapper = ({
   onActionClose: () => void;
 }) => {
   const [showDropdown, setShowDropdown] = useState(false);
-  console.log('-- showDropdown', showDropdown);
   return (
     <Box style={{ height: 68 }}>
       <Box
@@ -34,7 +33,7 @@ export const InputWrapper = ({
           borderRadius="24px"
           paddingHorizontal="20px"
           paddingTop="16px"
-          paddingBottom="8px"
+          paddingBottom="6px"
         >
           <Columns alignVertical="center" alignHorizontal="justify" space="8px">
             <Column width="content">{leftComponent}</Column>
@@ -50,107 +49,115 @@ export const InputWrapper = ({
               />
             </Column>
           </Columns>
+          <Box
+            as={motion.div}
+            paddingHorizontal="12px"
+            style={{
+              left: 0,
+            }}
+            width="full"
+            background="surfaceSecondaryElevated"
+            key="address-dropdown"
+            initial={{ opacity: 0, height: 8 }}
+            transition={{ duration: 0.01 }}
+            animate={
+              showDropdown
+                ? {
+                    opacity: 1,
+                    height: 'auto',
+                    transition: {
+                      height: {
+                        type: 'tween',
+                        ease: 'backOut',
+                        duration: 0.3,
+                      },
+                      opacity: {
+                        duration: 0.2,
+                      },
+                    },
+                  }
+                : {
+                    opacity: 0,
+                    height: 10,
+                    transition: {
+                      height: {
+                        type: 'tween',
+                        ease: 'backOut',
+                        duration: 0.3,
+                      },
+                      opacity: {
+                        duration: 0.23,
+                      },
+                    },
+                  }
+            }
+          >
+            <Box paddingVertical="16px">
+              <Separator />
+            </Box>
 
-          <AnimatePresence initial={false}>
-            {!showDropdown && (
-              <Box
-                as={motion.div}
-                initial={{ opacity: 0, height: 0 }}
-                animate={{
-                  opacity: 1,
-                  height: 8,
-                  transition: {
-                    height: {
-                      duration: 0.1,
-                      delay: 0.2,
-                    },
-                    opacity: {
-                      duration: 0.05,
-                      delay: 0.2,
-                    },
-                  },
-                }}
-                exit={{
-                  opacity: 0,
-                  height: 0,
-                  transition: {
-                    height: {
-                      duration: 0.1,
-                    },
-                    opacity: {
-                      duration: 0.05,
-                    },
-                  },
-                }}
-                key="2"
-                style={{ height: 0 }}
-              />
-            )}
-
-            {showDropdown && (
-              <Box
-                as={motion.div}
-                paddingHorizontal="12px"
-                style={{
-                  left: 0,
-                }}
-                transition={{ type: 'spring', bounce: 1 }}
-                width="full"
-                background="surfaceSecondaryElevated"
-                key="address-dropdown"
-                initial={{ opacity: 0, height: 0 }}
-                animate={{
-                  opacity: 1,
-                  height: 'auto',
-                  transition: {
-                    height: {
-                      duration: 0.2,
-                    },
-                    opacity: {
-                      duration: 0.1,
-                    },
-                  },
-                }}
-                exit={{
-                  opacity: 0,
-                  height: 0,
-                  transition: {
-                    height: {
-                      duration: 0.2,
-                    },
-                    opacity: {
-                      duration: 0.1,
-                    },
-                  },
-                }}
-              >
-                <Box paddingVertical="16px">
-                  <Separator />
-                </Box>
-
-                <Stack space="12px">
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                  <Text weight="bold" size="11pt">
-                    AAAAAA
-                  </Text>
-                </Stack>
-              </Box>
-            )}
-          </AnimatePresence>
+            <Stack space="12px">
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>{' '}
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>{' '}
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>{' '}
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>{' '}
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>{' '}
+              <Text weight="bold" size="11pt">
+                AAAAAA
+              </Text>
+            </Stack>
+          </Box>
+          {/* )} */}
+          {/* </AnimatePresence> */}
         </Box>
       </Box>
     </Box>
