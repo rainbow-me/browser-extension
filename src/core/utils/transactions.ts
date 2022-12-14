08ab34a1bd0c057733744025d68b0a703df264d9
derHowie <derhowiedesigns@gmail.com>
2022-12-14 13:25:04 -0600

    hook up pending TX flow to send

--- src/core/utils/transactions.ts
+++ src/core/utils/transactions.ts
@@ -621,6 +621,7 @@ export async function addNewTransaction({
   );
 
   setPendingTransactions({
+    address,
     pendingTransactions: [newPendingTransaction, ...pendingTransactions],
   });
   setNonce({
