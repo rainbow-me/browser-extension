0f5023a2e0b4330f05f8fff138c2fe6c38258d6e
derHowie <derhowiedesigns@gmail.com>
2022-12-14 10:58:22 -0600

    swap pending tx callback on address change

--- src/core/utils/transactions.ts
+++ src/core/utils/transactions.ts
@@ -539,6 +539,8 @@ export async function watchPendingTransactions({
     address,
   });
 
+  if (!pendingTransactions?.length) return;
+
   const updatedPendingTransactions = await Promise.all(
     pendingTransactions.map(async (tx) => {
       let updatedTransaction = { ...tx };
