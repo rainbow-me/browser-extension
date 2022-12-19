70dbf7992f454dd24e16dcac9180082ca19d9161
Esteban Miño <efmino@uc.cl>
2022-12-19 15:08:16 -0300

    load assets

--- src/entries/popup/pages/send/TokenInput.tsx
+++ src/entries/popup/pages/send/TokenInput.tsx
@@ -1,12 +1,40 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { ReactNode, useCallback, useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
 
 import { i18n } from '~/core/languages';
+import { selectUserAssetsList } from '~/core/resources/_selectors';
+import { useUserAssets } from '~/core/resources/assets';
+import { useCurrentCurrencyStore } from '~/core/state';
+import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
 import { ParsedAddressAsset } from '~/core/types/assets';
-import { Box, Text } from '~/design-system';
+import { Box, Inline, Inset, Stack, Symbol, Text } from '~/design-system';
 
 import { CoinIcon } from '../../components/CoinIcon/CoinIcon';
+import { AssetRow } from '../home/Tokens';
 
 import { InputWrapper } from './InputWrapper';
+import {
+  addressToInputHighlightWrapperStyleDark,
+  addressToInputHighlightWrapperStyleLight,
+} from './ToAddressInpnut.css';
+
+const RowHighlightWrapper = ({ children }: { children: ReactNode }) => {
+  const { currentTheme } = useCurrentThemeStore();
+  return (
+    <Inset>
+      <Box
+        borderRadius="12px"
+        className={
+          currentTheme === 'dark'
+            ? addressToInputHighlightWrapperStyleDark
+            : addressToInputHighlightWrapperStyleLight
+        }
+      >
+        {children}
+      </Box>
+    </Inset>
+  );
+};
 
 export const TokenInput = ({
   asset,
@@ -21,6 +49,13 @@ export const TokenInput = ({
   const onDropdownAction = useCallback(
     () => setDropdownVisible((dropdownVisible) => !dropdownVisible),
     [],
+  );
+
+  const { address } = useAccount();
+  const { currentCurrency: currency } = useCurrentCurrencyStore();
+  const { data: assets = [] } = useUserAssets(
+    { address, currency },
+    { select: selectUserAssetsList },
   );
 
   useEffect(() => {
@@ -49,14 +84,47 @@ export const TokenInput = ({
           </Text>
         </Box>
       }
-      showActionClose={false}
+      showActionClose={!!asset}
       onActionClose={() => shuffleAssetIndex(-1)}
       dropdownComponent={
-        <Box>
-          <Text size="12pt" weight="bold">
-            aaaaaaaa
-          </Text>
-        </Box>
+        <Stack space="8px">
+          <Box paddingHorizontal="20px">
+            <Inline alignHorizontal="justify">
+              <Inline space="4px" alignVertical="center">
+                <Symbol
+                  symbol="record.circle.fill"
+                  color="labelTertiary"
+                  weight="semibold"
+                  size={14}
+                />
+                <Text size="14pt" weight="semibold" color="labelTertiary">
+                  Tokens
+                </Text>
+              </Inline>
+              <Inline space="4px" alignVertical="center">
+                <Symbol
+                  symbol="arrow.up.arrow.down"
+                  color="labelTertiary"
+                  weight="semibold"
+                  size={14}
+                />
+                <Text size="14pt" weight="semibold" color="labelTertiary">
+                  Sort
+                </Text>
+              </Inline>
+            </Inline>
+          </Box>
+
+          {assets?.map((asset, i) => (
+            <Box paddingHorizontal="8px" key={`${asset?.uniqueId}-${i}`}>
+              <RowHighlightWrapper>
+                <Box marginHorizontal="-8px">
+                  <AssetRow uniqueId={asset?.uniqueId} />
+                </Box>
+              </RowHighlightWrapper>
+            </Box>
+          ))}
+        </Stack>
       }
       dropdownVisible={dropdownVisible}
       onDropdownAction={onDropdownAction}
