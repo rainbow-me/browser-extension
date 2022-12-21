43bfcb6e63cb35d20f4da26c3a39099846022c81
Nico Glennon <nicoglennon@icloud.com>
2022-12-21 11:31:48 -0500

    feat: refactor warning screens to use shared comp

--- src/entries/popup/pages/settings/privacy/walletsAndKeys/recoveryPhrase/warning.tsx
+++ src/entries/popup/pages/settings/privacy/walletsAndKeys/recoveryPhrase/warning.tsx
@@ -2,11 +2,8 @@ import React, { useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { i18n } from '~/core/languages';
-import { Box, Button, Rows, Symbol, Text } from '~/design-system';
-import {
-  IconAndCopyItem,
-  IconAndCopyList,
-} from '~/entries/popup/components/IconAndCopyList.tsx/IconAndCopyList';
+import { IconAndCopyItem } from '~/entries/popup/components/IconAndCopyList.tsx/IconAndCopyList';
+import WarningInfo from '~/entries/popup/components/WarningInfo/WarningInfo';
 
 const iconAndCopyList: IconAndCopyItem[] = [
   {
@@ -58,73 +55,13 @@ export function RecoveryPhraseWarning() {
   }, [navigate, state.password, state.wallet]);
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      padding="20px"
-      paddingTop="2px"
-    >
-      <Box
-        borderRadius="round"
-        boxShadow="18px accent"
-        style={{
-          height: 60,
-          width: 60,
-          overflow: 'hidden',
-          position: 'absolute',
-        }}
-      >
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          height="full"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          position="relative"
-          style={{
-            background:
-              'radial-gradient(100% 144.46% at 0% 50%, #FF9233 0%, #FA423C 100%)',
-            zIndex: 1,
-          }}
-        >
-          <Symbol
-            symbol="exclamationmark.triangle.fill"
-            size={26}
-            color="label"
-            weight={'bold'}
-          />
-        </Box>
-      </Box>
-      <Box
-        alignItems="center"
-        style={{ paddingTop: '84px', paddingBottom: '60px' }}
-      >
-        <Text size="16pt" weight="bold" color="label" align="center">
-          {i18n.t('common_titles.before_you_proceed')}
-        </Text>
-      </Box>
-      <IconAndCopyList iconAndCopyList={iconAndCopyList} />
-      <Box width="full" style={{ paddingTop: 76 }}>
-        <Rows alignVertical="top" space="8px">
-          <Button
-            color="accent"
-            height="44px"
-            variant="flat"
-            width="full"
-            symbol="doc.plaintext.fill"
-            blur="26px"
-            onClick={handleShowRecoveryPhraseClick}
-          >
-            {i18n.t(
-              'settings.privacy_and_security.wallets_and_keys.recovery_phrase.show',
-            )}
-          </Button>
-        </Rows>
-      </Box>
-    </Box>
+    <WarningInfo
+      iconAndCopyList={iconAndCopyList}
+      onProceed={handleShowRecoveryPhraseClick}
+      proceedButtonLabel={i18n.t(
+        'settings.privacy_and_security.wallets_and_keys.recovery_phrase.show',
+      )}
+      proceedButtonSymbol="doc.plaintext.fill"
+    />
   );
 }
