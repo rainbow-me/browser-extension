929fcf8e883e3b064e227b2950c349c9952e48b7
Nico Glennon <nicoglennon@icloud.com>
2023-01-04 15:33:16 -0500

    feat: obfuscate token balances

--- src/entries/popup/pages/home/Tokens.tsx
+++ src/entries/popup/pages/home/Tokens.tsx
@@ -1,13 +1,38 @@
 import React, { useMemo } from 'react';
 import { useAccount } from 'wagmi';
 
+import { supportedCurrencies } from '~/core/references';
 import { selectUserAssetsList } from '~/core/resources/_selectors';
 import { useUserAssets } from '~/core/resources/assets';
 import { useCurrentCurrencyStore } from '~/core/state';
+import { useHideAssetBalancesStore } from '~/core/state/currentSettings/hideAssetBalances';
 import { UniqueId } from '~/core/types/assets';
-import { Box, Column, Columns, Text } from '~/design-system';
+import { Box, Column, Columns, Inline, Symbol, Text } from '~/design-system';
+import { SymbolProps } from '~/design-system/components/Symbol/Symbol';
 import { CoinRow } from '~/entries/popup/components/CoinRow/CoinRow';
 import { useUserAsset } from '~/entries/popup/hooks/useUserAsset';
+
+const Asterisks = ({
+  color,
+  size,
+}: {
+  color: SymbolProps['color'];
+  size: SymbolProps['size'];
+}) => (
+  <Inline>
+    {Array(4)
+      .fill(0)
+      .map((_, i) => (
+        <Symbol
+          symbol={'asterisk'}
+          weight={'bold'}
+          size={size}
+          color={color}
+          key={i}
+        />
+      ))}
+  </Inline>
+);
 
 export function Tokens() {
   const { address } = useAccount();
@@ -32,14 +57,48 @@ type AssetRowProps = {
 export function AssetRow({ uniqueId }: AssetRowProps) {
   const asset = useUserAsset(uniqueId);
   const name = asset?.name;
+  const { hideAssetBalances } = useHideAssetBalancesStore();
+  const { currentCurrency } = useCurrentCurrencyStore();
 
   const priceChange = asset?.native?.price?.change;
   const priceChangeDisplay = priceChange?.length ? priceChange : '-';
   const priceChangeColor =
     priceChangeDisplay[0] !== '-' ? 'green' : 'labelTertiary';
 
-  const balanceDisplay = asset?.balance?.display;
-  const nativeBalanceDisplay = asset?.native?.balance?.display;
+  const balanceDisplay = useMemo(
+    () =>
+      hideAssetBalances ? (
+        <Inline space="4px">
+          <Asterisks color="labelTertiary" size={8} />
+          <Text color="labelTertiary" size="12pt" weight="semibold">
+            {asset?.symbol}
+          </Text>
+        </Inline>
+      ) : (
+        <Text color="labelTertiary" size="12pt" weight="semibold">
+          {asset?.balance?.display}
+        </Text>
+      ),
+    [asset?.balance?.display, asset?.symbol, hideAssetBalances],
+  );
+  const nativeBalanceDisplay = useMemo(
+    () =>
+      hideAssetBalances ? (
+        <Inline alignHorizontal="right">
+          <Text size="14pt" weight="semibold" align="right">
+            {supportedCurrencies[currentCurrency].symbol}
+          </Text>
+          <Asterisks color="label" size={10} />
+        </Inline>
+      ) : (
+        <Text size="14pt" weight="semibold" align="right">
+          {asset?.native?.balance?.display}
+        </Text>
+      ),
+    [asset?.native?.balance?.display, hideAssetBalances, currentCurrency],
+  );
+
+  console.log('asset', asset);
 
   const topRow = useMemo(
     () => (
@@ -52,11 +111,7 @@ export function AssetRow({ uniqueId }: AssetRowProps) {
           </Box>
         </Column>
         <Column>
-          <Box paddingVertical="4px">
-            <Text size="14pt" weight="semibold" align="right">
-              {nativeBalanceDisplay}
-            </Text>
-          </Box>
+          <Box paddingVertical="4px">{nativeBalanceDisplay}</Box>
         </Column>
       </Columns>
     ),
@@ -67,11 +122,7 @@ export function AssetRow({ uniqueId }: AssetRowProps) {
     () => (
       <Columns>
         <Column width="content">
-          <Box paddingVertical="4px">
-            <Text color="labelTertiary" size="12pt" weight="semibold">
-              {balanceDisplay}
-            </Text>
-          </Box>
+          <Box paddingVertical="4px">{balanceDisplay}</Box>
         </Column>
         <Column>
           <Box paddingVertical="4px">
