ea8115db7825a75cf94036599a75bfd3bbb81775
Esteban Miño <efmino@uc.cl>
2023-01-12 13:50:58 -0300

    navigate

--- src/entries/popup/pages/send/index.tsx
+++ src/entries/popup/pages/send/index.tsx
@@ -7,6 +7,7 @@ import React, {
   useMemo,
   useState,
 } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Address } from 'wagmi';
 
 import { i18n } from '~/core/languages';
@@ -34,6 +35,7 @@ import { sendTransaction } from '../../handlers/wallet';
 import { useSendTransactionAsset } from '../../hooks/send/useSendTransactionAsset';
 import { useSendTransactionInputs } from '../../hooks/send/useSendTransactionInputs';
 import { useSendTransactionState } from '../../hooks/send/useSendTransactionState';
+import { ROUTES } from '../../urls';
 
 import { ContactAction, ContactPrompt } from './ContactPrompt';
 import { NavbarContactButton } from './NavbarContactButton';
@@ -69,6 +71,8 @@ export function Send() {
     action: ContactAction;
   }>({ show: false, action: 'save' });
   const [toAddressDropdownOpen, setToAddressDropdownOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   const { isContact } = useContactsStore();
   const { connectedToHardhat } = useConnectedToHardhatStore();
@@ -151,7 +155,6 @@ export function Send() {
       });
 
       if (result) {
-        alert(`Transaction sent successfully: ${JSON.stringify(result.hash)}`);
         setTxHash(result?.hash as string);
         const transaction = {
           amount: assetAmount,
@@ -173,6 +176,7 @@ export function Send() {
           });
         }
       }
+      navigate(ROUTES.HOME, { state: { activeTab: 'activity' } });
     } catch (e) {
       alert('Transaction failed');
     }
@@ -185,6 +189,7 @@ export function Send() {
     data,
     assetAmount,
     asset,
+    navigate,
   ]);
 
   const selectAsset = useCallback(
