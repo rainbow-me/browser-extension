f19448d991b1f273f645925d70f916579d22f8ab
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2023-01-12 13:22:14 -0500

    Disable sentry dev (#211)

--- src/core/sentry/index.ts
+++ src/core/sentry/index.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser';
 import { BrowserTracing } from '@sentry/tracing';
 
 export function initializeSentry(context: 'popup' | 'background') {
-  if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN) {
+  if (process.env.IS_DEV !== 'true' && process.env.SENTRY_DSN) {
     try {
       const integrations = context === 'popup' ? [new BrowserTracing()] : [];
       Sentry.init({
