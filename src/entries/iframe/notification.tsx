14e21d0952caf1dbb0d5f80b8e2829ce338e3bb4
Esteban Miño <efmino@uc.cl>
2023-02-02 13:43:59 -0300

    almost there

--- src/entries/iframe/notification.tsx
+++ src/entries/iframe/notification.tsx
@@ -1,8 +1,7 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import { i18n } from '~/core/languages';
-import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
 import { ChainId } from '~/core/types/chains';
 import { chainNameFromChainId } from '~/core/utils/chains';
 import {
@@ -16,6 +15,13 @@ import {
   ThemeProvider,
 } from '~/design-system';
 
+const isDarkColor = (rgb: string) => {
+  const from = rgb.indexOf('(');
+  const to = rgb.indexOf(')');
+  const [r, g, b] = rgb.substring(from + 1, to).split(',');
+  return !(Number(r) > 40 || Number(g) > 40 || Number(b) > 40);
+};
+
 // 161 (figma width spec) + 48 (radius shadow) since we need space for the shadow to be visible in the iframe
 const NOTIFICATION_WIDTH = '609px';
 // const NOTIFICATION_WIDTH = '209px';
@@ -26,25 +32,11 @@ const NOTIFICATION_HEIGHT = '552px';
 // 9 (figma top spec) - 41 (extra iframe height for shadow, 122 - 40 /2 )
 // since we need space for the shadow to be visible in the iframe
 const NOTIFICATION_TOP = '-32px';
-const NOTIFICATION_RIGHT = '600px';
+const NOTIFICATION_RIGHT = '50px';
 
 export const Notification = () => {
-  const { currentTheme } = useCurrentThemeStore();
-  return (
-    <IFrame theme={currentTheme}>
-      <NotificationComponent theme={currentTheme} />
-    </IFrame>
-  );
-};
-
-function IFrame({
-  children,
-  theme,
-}: {
-  children: ReactNode;
-  theme: 'dark' | 'light';
-}) {
   const [ref, setRef] = useState<HTMLIFrameElement>();
+  const [siteTheme, setSiteTheme] = useState<'dark' | 'light'>('dark');
 
   const onRef = (ref: HTMLIFrameElement) => {
     setRef(ref);
@@ -57,18 +49,62 @@ function IFrame({
     iframeLink.href =
       'chrome-extension://gjmdpkmgceafaiefjdekbelbcjigmaed/popup.css';
     iframeLink.rel = 'stylesheet';
-    const root = ref?.contentDocument?.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
+
+    const iframeMeta = document.createElement('meta');
+    iframeMeta.name = 'color-scheme';
+
+    // const isDarkMode =
+    //   window.matchMedia && window.matchMedia('color-scheme: dark').matches;
+    const colorScheme = (
+      document.querySelector('meta[name="color-scheme"]') as HTMLMetaElement
+    )?.content;
+    const dataColorMode =
+      window?.document.documentElement?.getAttribute('data-color-mode');
+    const dataTheme =
+      window?.document.documentElement?.getAttribute('data-theme');
+    const style = window?.document.documentElement?.getAttribute('style');
+    const dark = window?.document.documentElement?.getAttributeNode('dark');
+    const backgroundColro = window
+      .getComputedStyle(document.body, null)
+      .getPropertyValue('background-color');
+
+    console.log('backgroundColro dark', `-${backgroundColro}-`);
+    console.log(
+      'backgroundColro parseRGB',
+      `-${isDarkColor(backgroundColro)}-`,
+    );
+    console.log('dark dark', `-${dark}-`);
+    console.log('style style', `-${style}-`);
+    console.log('COLOR SCHEME', `-${colorScheme}-`);
+    console.log('DATA COLOR MODE', dataColorMode);
+
+    const extractedTheme =
+      isDarkColor(backgroundColro) ||
+      dataTheme === 'dark' ||
+      style?.includes('color-scheme: dark') ||
+      dataColorMode === 'dark' ||
+      colorScheme === 'dark'
+        ? 'dark'
+        : 'light';
+
+    setSiteTheme(extractedTheme);
+    iframeMeta.content = extractedTheme;
+
+    const root = ref?.contentDocument?.getElementsByTagName('html')[0];
     if (root) {
-      //   root.style.background = 'none transparent !important';
-      //   root.style.backgroundColor = 'none transparent !important';
+      root.style.background = 'none transparent !important';
+      root.style.backgroundColor = 'none transparent !important';
       root.style.position = 'fixed';
       root.style.height = NOTIFICATION_HEIGHT;
       root.style.width = NOTIFICATION_WIDTH;
-      //   root.style.cssText = 'background-color: transparent !important';
+      root.style.cssText = 'background-color: transparent !important';
     }
-    // root?.setAttribute('class', theme === 'dark' ? 'dt' : 'lt');
+    root?.setAttribute('class', colorScheme === 'dark' ? 'dt' : 'lt');
     ref?.contentDocument?.head?.appendChild(iframeLink);
-  }, [ref?.contentDocument, theme]);
+    ref?.contentDocument?.head?.appendChild(iframeMeta);
+  }, [ref?.contentDocument]);
+
+  console.log('SITE THEME', siteTheme);
 
   return (
     <iframe
@@ -85,55 +121,75 @@ function IFrame({
       title="iframe"
       ref={onRef}
     >
-      {container && createPortal(children, container)}
+      {container &&
+        createPortal(
+          <NotificationComponent siteTheme={siteTheme} />,
+          container,
+        )}
     </iframe>
   );
-}
+};
 
-const NotificationComponent = ({ theme }: { theme: 'dark' | 'light' }) => {
+const NotificationComponent = ({
+  siteTheme,
+}: {
+  siteTheme: 'dark' | 'light';
+}) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={siteTheme}>
       <Box
         height="full"
-        style={{ height: NOTIFICATION_HEIGHT, width: NOTIFICATION_WIDTH }}
+        style={{
+          height: NOTIFICATION_HEIGHT,
+          width: NOTIFICATION_WIDTH,
+        }}
       >
         <Inline height="full" alignVertical="center" alignHorizontal="center">
           <Box
-            alignItems="center"
             borderRadius="28px"
-            backdropFilter="blur(26px)"
-            background="surfaceMenu"
-            paddingLeft="8px"
-            paddingRight="16px"
-            paddingVertical="8px"
-            style={{
-              boxShadow:
-                '0px 8px 24px rgba(37, 41, 46, 0.12), 0px 2px 6px rgba(0, 0, 0, 0.02)',
-            }}
+            style={
+              {
+                //   backgroundColor: siteTheme === 'dark' ? '#191A1C' : '#FFFFFF',
+              }
+            }
           >
-            <Columns space="8px">
-              <Column width="content">
-                <img
-                  src="chrome-extension://gjmdpkmgceafaiefjdekbelbcjigmaed/assets/badges/arbitrumBadge.png"
-                  width={24}
-                  height={24}
-                />
-              </Column>
-              <Column>
-                <Rows alignVertical="center" space="6px">
-                  <Row>
-                    <Text color="label" size="12pt" weight="bold">
-                      {i18n.t('injected_notifications.network_changed')}
-                    </Text>
-                  </Row>
-                  <Row>
-                    <Text color="labelTertiary" size="11pt" weight="medium">
-                      {chainNameFromChainId(ChainId.optimism)}
-                    </Text>
-                  </Row>
-                </Rows>
-              </Column>
-            </Columns>
+            <Box
+              borderRadius="28px"
+              paddingLeft="8px"
+              paddingRight="16px"
+              paddingVertical="8px"
+              alignItems="center"
+              backdropFilter="blur(26px)"
+              background="surfaceMenu"
+              style={{
+                boxShadow:
+                  '0px 8px 24px rgba(37, 41, 46, 0.12), 0px 2px 6px rgba(0, 0, 0, 0.02)',
+              }}
+            >
+              <Columns space="8px">
+                <Column width="content">
+                  <img
+                    src="chrome-extension://gjmdpkmgceafaiefjdekbelbcjigmaed/assets/badges/arbitrumBadge.png"
+                    width={24}
+                    height={24}
+                  />
+                </Column>
+                <Column>
+                  <Rows alignVertical="center" space="6px">
+                    <Row>
+                      <Text color="label" size="12pt" weight="bold">
+                        {i18n.t('injected_notifications.network_changed')}
+                      </Text>
+                    </Row>
+                    <Row>
+                      <Text color="labelTertiary" size="11pt" weight="medium">
+                        {chainNameFromChainId(ChainId.optimism)}
+                      </Text>
+                    </Row>
+                  </Rows>
+                </Column>
+              </Columns>
+            </Box>
           </Box>
         </Inline>
       </Box>
