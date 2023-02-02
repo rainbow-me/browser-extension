06a4549dcc5c9341c169f5e46166badf2e1f2682
derHowie <derhowiedesigns@gmail.com>
2023-02-02 04:03:38 -0600

    useSwappableAssets

---
+++ src/entries/popup/hooks/useSwappableAssets.ts
@@ -0,0 +1,105 @@
+import { useAccount } from 'wagmi';
+
+import {
+  selectUserAssetAddressMapByChainId,
+  selectUserAssetsList,
+} from '~/core/resources/_selectors/assets';
+import { useUserAssets } from '~/core/resources/assets';
+import { useSwappableAddresses } from '~/core/resources/search/swappableAddresses';
+import { useCurrentCurrencyStore } from '~/core/state';
+import { ParsedAddressAsset } from '~/core/types/assets';
+import { ChainId } from '~/core/types/chains';
+
+export function useSwappableAssets(toChainId?: ChainId) {
+  const { address } = useAccount();
+
+  const { currentCurrency: currency } = useCurrentCurrencyStore();
+
+  const { data: userAssets } = useUserAssets({
+    address,
+    currency,
+  });
+
+  const fullUserAssetList = selectUserAssetsList(userAssets || {});
+
+  const assetAddressMap = selectUserAssetAddressMapByChainId(userAssets || {});
+
+  const {
+    data: swappableMainnetAddresses,
+    isLoading: swappableMainnetAddressesAreLoading,
+  } = useSwappableAddresses({
+    addresses: assetAddressMap[ChainId.mainnet],
+    fromChainId: ChainId.mainnet,
+    toChainId,
+  });
+
+  const {
+    data: swappableOptimismAddresses,
+    isLoading: swappableOptimismAddressesAreLoading,
+  } = useSwappableAddresses({
+    addresses: assetAddressMap[ChainId.optimism],
+    fromChainId: ChainId.optimism,
+    toChainId,
+  });
+
+  const {
+    data: swappableBscAddresses,
+    isLoading: swappableBscAddressesAreLoading,
+  } = useSwappableAddresses({
+    addresses: assetAddressMap[ChainId.bsc],
+    fromChainId: ChainId.bsc,
+    toChainId,
+  });
+
+  const {
+    data: swappablePolygonAddresses,
+    isLoading: swappablePolygonAddressesAreLoading,
+  } = useSwappableAddresses({
+    addresses: assetAddressMap[ChainId.polygon],
+    fromChainId: ChainId.polygon,
+    toChainId,
+  });
+
+  const {
+    data: swappableArbitrumAddresses,
+    isLoading: swappableArbitrumAddressesAreLoading,
+  } = useSwappableAddresses({
+    addresses: assetAddressMap[ChainId.arbitrum],
+    fromChainId: ChainId.arbitrum,
+    toChainId,
+  });
+
+  const swappableInfo = {
+    [ChainId.mainnet]: {
+      addresses: swappableMainnetAddresses,
+      loading: swappableMainnetAddressesAreLoading,
+    },
+    [ChainId.optimism]: {
+      addresses: swappableOptimismAddresses,
+      loading: swappableOptimismAddressesAreLoading,
+    },
+    [ChainId.bsc]: {
+      addresses: swappableBscAddresses,
+      loading: swappableBscAddressesAreLoading,
+    },
+    [ChainId.polygon]: {
+      addresses: swappablePolygonAddresses,
+      loading: swappablePolygonAddressesAreLoading,
+    },
+    [ChainId.arbitrum]: {
+      addresses: swappableArbitrumAddresses,
+      loading: swappableArbitrumAddressesAreLoading,
+    },
+  };
+
+  const isSwappableAsset = (asset: ParsedAddressAsset) => {
+    const { address, chainId } = asset;
+    if (chainId === toChainId) return true;
+    const { addresses, loading } = swappableInfo[chainId];
+    return loading || addresses?.includes(address);
+  };
+
+  if (!toChainId) return fullUserAssetList;
+
+  return fullUserAssetList.filter((asset) => isSwappableAsset(asset));
+}
