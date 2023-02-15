ea67ceeef8d2db45c37fa8d4ab8fd1255c2a36a8
Esteban Miño <efmino@uc.cl>
2023-02-15 16:02:41 -0300

    create hook to get balance

---
+++ src/entries/popup/hooks/useUserAssetsBalance.ts
@@ -0,0 +1,25 @@
+import { useAccount } from 'wagmi';
+
+import { selectUserAssetsBalance } from '~/core/resources/_selectors/assets';
+import { useUserAssets } from '~/core/resources/assets';
+import { useCurrentCurrencyStore } from '~/core/state';
+import { convertAmountToNativeDisplay } from '~/core/utils/numbers';
+
+export function useUserAssetsBalance() {
+  const { address } = useAccount();
+  const { currentCurrency: currency } = useCurrentCurrencyStore();
+  const { data: totalAssetsBalance } = useUserAssets(
+    {
+      address,
+      currency,
+    },
+    {
+      select: selectUserAssetsBalance(),
+    },
+  );
+
+  return {
+    amount: totalAssetsBalance,
+    display: convertAmountToNativeDisplay(totalAssetsBalance, currency),
+  };
+}
