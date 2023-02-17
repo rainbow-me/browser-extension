011ee2bc5c741eaa34bdc22e68e6d9d4618a33ad
Esteban Miño <efmino@uc.cl>
2023-02-17 11:26:18 -0300

    wallets UI polish (#282)

--- src/entries/popup/components/AccountItem/AccountItem.tsx
+++ src/entries/popup/components/AccountItem/AccountItem.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Address, useBalance } from 'wagmi';
 
+import { supportedCurrencies } from '~/core/references';
+import { useCurrentCurrencyStore } from '~/core/state';
 import { useHideAssetBalancesStore } from '~/core/state/currentSettings/hideAssetBalances';
+import { ChainId } from '~/core/types/chains';
 import { truncateAddress } from '~/core/utils/address';
-import { handleSignificantDecimals } from '~/core/utils/numbers';
-import { Box, Inline, Symbol, Text } from '~/design-system';
+import { convertAmountAndPriceToNativeDisplay } from '~/core/utils/numbers';
+import { Box, Inline, Row, Rows, Symbol, Text } from '~/design-system';
 
-import { Asterisks } from '../../components/Asterisks/Asterisks';
+import { useNativeAssetForNetwork } from '../../hooks/useNativeAssetForNetwork';
 import { useWalletName } from '../../hooks/useWalletName';
+import { Asterisks } from '../Asterisks/Asterisks';
+import { rowHighlightWrapperStyle } from '../CoinRow/CoinRow.css';
 import { MenuItem } from '../Menu/MenuItem';
 import { WalletAvatar } from '../WalletAvatar/WalletAvatar';
 
@@ -22,6 +27,7 @@ export default function AccountItem({
   onClick,
   labelType,
   isSelected,
+  rowHighlight,
 }: {
   account: Address;
   rightComponent?: React.ReactNode;
@@ -29,10 +35,19 @@ export default function AccountItem({
   isSelected?: boolean;
   labelType?: LabelOption;
   searchTerm?: string;
+  rowHighlight?: boolean;
 }) {
   const { displayName, showAddress } = useWalletName({ address: account });
   const { data: balance } = useBalance({ addressOrName: account });
+  const nativeAsset = useNativeAssetForNetwork({ chainId: ChainId.mainnet });
+  const { currentCurrency } = useCurrentCurrencyStore();
   const { hideAssetBalances } = useHideAssetBalancesStore();
+
+  const nativeDisplay = convertAmountAndPriceToNativeDisplay(
+    balance?.formatted || 0,
+    nativeAsset?.native?.price?.amount || 0,
+    currentCurrency,
+  );
 
   let labelComponent = null;
   if (labelType === LabelOption.address) {
@@ -43,53 +58,64 @@ export default function AccountItem({
     labelComponent = hideAssetBalances ? (
       <Inline alignVertical="center">
         <Text color="labelTertiary" size="12pt" weight="medium">
-          {'Ξ'}
+          {supportedCurrencies[currentCurrency]?.symbol}
         </Text>
         <Asterisks color="labelTertiary" size={10} />
       </Inline>
     ) : (
-      <MenuItem.Label
-        text={`Ξ${handleSignificantDecimals(balance?.formatted || 0, 4)}`}
-      />
+      <MenuItem.Label text={`${nativeDisplay?.display}`} />
     );
   }
 
   return (
-    <MenuItem
+    <Box
+      className={rowHighlight ? rowHighlightWrapperStyle : undefined}
       onClick={onClick}
       key={account}
-      titleComponent={<MenuItem.Title text={displayName || ''} />}
-      labelComponent={labelComponent}
-      leftComponent={
-        <Box marginRight="-8px" height="fit" position="relative">
-          {isSelected && (
-            <Box
-              style={{
-                width: 20,
-                height: 20,
-                zIndex: 1,
-                bottom: -4,
-                left: -4,
-              }}
-              position="absolute"
-              padding="3px"
-              borderRadius="round"
-              background="surfacePrimaryElevated"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Symbol
-                symbol="checkmark.circle.fill"
-                color="accent"
-                weight="bold"
-                size={14}
-              />
-            </Box>
-          )}
-          <WalletAvatar address={account} size={36} emojiSize="20pt" />
-        </Box>
-      }
-      rightComponent={rightComponent}
-    />
+      paddingHorizontal="12px"
+      paddingVertical="8px"
+    >
+      <Inline alignHorizontal="justify" alignVertical="center">
+        <Inline space="8px" alignHorizontal="left">
+          <Box height="fit" position="relative">
+            {isSelected && (
+              <Box
+                style={{
+                  width: 20,
+                  height: 20,
+                  zIndex: 1,
+                  bottom: -4,
+                  left: -4,
+                }}
+                position="absolute"
+                padding="3px"
+                borderRadius="round"
+                background="surfacePrimaryElevated"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Symbol
+                  symbol="checkmark.circle.fill"
+                  color="accent"
+                  weight="bold"
+                  size={14}
+                />
+              </Box>
+            )}
+            <WalletAvatar address={account} size={36} emojiSize="20pt" />
+          </Box>
+          <Box>
+            <Rows space="8px" alignVertical="center">
+              <Row height="content">
+                <MenuItem.Title text={displayName || ''} />
+              </Row>
+              <Row height="content">{labelComponent}</Row>
+            </Rows>
+          </Box>
+        </Inline>
+
+        <Box>{rightComponent}</Box>
+      </Inline>
+    </Box>
   );
 }
