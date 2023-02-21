1243c646b71f0f79ad1d556427253b85a0d5d81b
Daniel Sinclair <d@niel.nyc>
2023-02-21 11:35:08 -0500

    feat: added multiple release tracks with generate-manifest script

--- scripts/bump.js
+++ scripts/bump.js
@@ -2,7 +2,6 @@
 const { inc, valid } = require('semver');
 
 const pkgJson = require('../package.json');
-const manifest = require('../static/manifest.json');
 const currentVersion = valid(pkgJson.version);
 const newVersion = inc(currentVersion, 'patch');
 
@@ -12,11 +11,5 @@ pkgJson.version = newVersion;
 
 // Update package.json
 require('fs').writeFileSync('./package.json', JSON.stringify(pkgJson, null, 2));
-// Update manifest.json
-manifest.version = newVersion;
-require('fs').writeFileSync(
-  './static/manifest.json',
-  JSON.stringify(manifest, null, 2),
-);
 
 console.log('Done.');
---
+++ scripts/generate-manifest.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('lodash');
+const { valid } = require('semver');
+
+const manifestBase = require('../manifest/base.json');
+const manifestInternal = require('../manifest/internal.json');
+const manifestProd = require('../manifest/prod.json');
+const pkgJson = require('../package.json');
+
+const tracks = {
+  internal: manifestInternal,
+  prod: manifestProd,
+};
+
+const track = process.argv[2];
+if (!Object.keys(tracks).includes(track)) {
+  console.error('invalid track: %s', track);
+  process.exit();
+}
+
+console.log('generating %s manifest.json', track);
+
+const currentVersion = valid(pkgJson.version);
+console.log('applying v%s version to manifest.json', currentVersion);
+manifestBase.version = currentVersion;
+
+console.log('applying %s.json to manifest base.json', track);
+const manifest = merge(tracks[track], manifestBase);
+
+// Sort manifest.json keys
+function orderedStringify(obj, space) {
+  const allKeys = new Set();
+  JSON.stringify(obj, (key, value) => (allKeys.add(key), value));
+  return JSON.stringify(obj, Array.from(allKeys).sort(), space);
+}
+
+// Update static/manifest.json
+require('fs').writeFileSync(
+  './static/manifest.json',
+  orderedStringify(manifest, 2),
+);
+
+console.log('Done.');
