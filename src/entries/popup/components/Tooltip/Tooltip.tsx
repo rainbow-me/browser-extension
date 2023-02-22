c7628e041d31a02da75f1d58c91124f604674a6b
Esteban Miño <efmino@uc.cl>
2023-02-22 16:56:10 -0300

    fix shadow

--- src/entries/popup/components/Tooltip/Tooltip.tsx
+++ src/entries/popup/components/Tooltip/Tooltip.tsx
@@ -15,31 +15,33 @@ export const Tooltip = ({
       <TooltipPrimitive.Root>
         <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
-          <TooltipPrimitive.Content className="TooltipContent" sideOffset={5}>
-            <Inline alignHorizontal="center">
+          <TooltipPrimitive.Content className="TooltipContent" sideOffset={10}>
+            <Box borderRadius="6px" boxShadow="24px">
+              <Inline alignHorizontal="center">
+                <Box
+                  background="surfaceSecondaryElevated"
+                  backdropFilter="blur(26px)"
+                  position="absolute"
+                  borderRadius="2px"
+                  marginBottom="-3px"
+                  bottom="0"
+                  style={{
+                    height: 10,
+                    width: 10,
+                    rotate: '45deg',
+                  }}
+                />
+              </Inline>
               <Box
                 background="surfaceSecondaryElevated"
+                padding="7px"
+                borderRadius="6px"
                 backdropFilter="blur(26px)"
-                position="absolute"
-                borderRadius="2px"
-                marginBottom="-3px"
-                bottom="0"
-                style={{
-                  height: 10,
-                  width: 10,
-                  rotate: '45deg',
-                }}
-              />
-            </Inline>
-            <Box
-              background="surfaceSecondaryElevated"
-              padding="7px"
-              borderRadius="6px"
-              backdropFilter="blur(26px)"
-            >
-              <Text color="label" size="16pt" weight="bold">
-                {text}
-              </Text>
+              >
+                <Text color="label" size="16pt" weight="bold">
+                  {text}
+                </Text>
+              </Box>
             </Box>
           </TooltipPrimitive.Content>
         </TooltipPrimitive.Portal>
