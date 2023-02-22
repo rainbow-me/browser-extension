6edcaa7f61dd1a4839a9676226e744bd5f6cc955
Esteban Miño <efmino@uc.cl>
2023-02-22 15:29:20 -0300

    placeholder button

--- src/entries/popup/pages/swap/index.tsx
+++ src/entries/popup/pages/swap/index.tsx
@@ -3,7 +3,16 @@ import React, { useCallback } from 'react';
 import { Address } from 'wagmi';
 
 import { i18n } from '~/core/languages';
-import { Box, ButtonSymbol, Inline, Stack } from '~/design-system';
+import {
+  Box,
+  Button,
+  ButtonSymbol,
+  Inline,
+  Row,
+  Rows,
+  Stack,
+  Text,
+} from '~/design-system';
 import { AccentColorProviderWrapper } from '~/design-system/components/Box/ColorContext';
 import {
   transformScales,
@@ -74,82 +83,103 @@ export function Swap() {
         paddingBottom="20px"
         paddingHorizontal="12px"
       >
-        <Stack space="8px">
-          <AccentColorProviderWrapper
-            color={
-              assetToSwap?.colors?.primary || assetToSwap?.colors?.fallback
-            }
-          >
-            <SwapTokenInput
-              asset={assetToSwap}
-              assets={assets}
-              selectAssetAddress={selectAssetToSwapAddress}
-              onDropdownOpen={onAssetToSwapInputClick}
-              dropdownClosed={assetToReceiveDropdownVisible}
-              setSortMethod={setSortMethod}
-              sortMethod={sortMethod}
-              zIndex={2}
-              placeholder={i18n.t('swap.input_token_to_swap_placeholder')}
-            />
-          </AccentColorProviderWrapper>
-
-          <Box
-            marginVertical="-20px"
-            style={{ zIndex: assetToSwapDropdownVisible ? 1 : 3 }}
-          >
-            <Inline alignHorizontal="center">
-              <Box
-                as={motion.div}
-                initial={{ zIndex: 0 }}
-                whileHover={{ scale: transformScales['1.04'] }}
-                whileTap={{ scale: transformScales['0.96'] }}
-                transition={transitions.bounce}
-                boxShadow="12px surfaceSecondaryElevated"
-                background="surfaceSecondaryElevated"
-                borderRadius="32px"
-                borderWidth={'1px'}
-                borderColor="buttonStroke"
-                style={{ width: 42, height: 32, zIndex: 10 }}
-                onClick={onFlip}
+        <Rows alignVertical="justify">
+          <Row height="content">
+            <Stack space="8px">
+              <AccentColorProviderWrapper
+                color={
+                  assetToSwap?.colors?.primary || assetToSwap?.colors?.fallback
+                }
               >
-                <Box width="full" height="full" alignItems="center">
-                  <Inline
-                    height="full"
-                    alignHorizontal="center"
-                    alignVertical="center"
+                <SwapTokenInput
+                  asset={assetToSwap}
+                  assets={assets}
+                  selectAssetAddress={selectAssetToSwapAddress}
+                  onDropdownOpen={onAssetToSwapInputClick}
+                  dropdownClosed={assetToReceiveDropdownVisible}
+                  setSortMethod={setSortMethod}
+                  sortMethod={sortMethod}
+                  zIndex={2}
+                  placeholder={i18n.t('swap.input_token_to_swap_placeholder')}
+                />
+              </AccentColorProviderWrapper>
+
+              <Box
+                marginVertical="-20px"
+                style={{ zIndex: assetToSwapDropdownVisible ? 1 : 3 }}
+              >
+                <Inline alignHorizontal="center">
+                  <Box
+                    as={motion.div}
+                    initial={{ zIndex: 0 }}
+                    whileHover={{ scale: transformScales['1.04'] }}
+                    whileTap={{ scale: transformScales['0.96'] }}
+                    transition={transitions.bounce}
+                    boxShadow="12px surfaceSecondaryElevated"
+                    background="surfaceSecondaryElevated"
+                    borderRadius="32px"
+                    borderWidth={'1px'}
+                    borderColor="buttonStroke"
+                    style={{ width: 42, height: 32, zIndex: 10 }}
+                    onClick={onFlip}
                   >
-                    <Stack alignHorizontal="center">
-                      <Box marginBottom="-4px">
-                        <ChevronDown color="labelTertiary" />
-                      </Box>
-                      <Box marginTop="-4px">
-                        <ChevronDown color="labelQuaternary" />
-                      </Box>
-                    </Stack>
-                  </Inline>
-                </Box>
+                    <Box width="full" height="full" alignItems="center">
+                      <Inline
+                        height="full"
+                        alignHorizontal="center"
+                        alignVertical="center"
+                      >
+                        <Stack alignHorizontal="center">
+                          <Box marginBottom="-4px">
+                            <ChevronDown color="labelTertiary" />
+                          </Box>
+                          <Box marginTop="-4px">
+                            <ChevronDown color="labelQuaternary" />
+                          </Box>
+                        </Stack>
+                      </Inline>
+                    </Box>
+                  </Box>
+                </Inline>
               </Box>
-            </Inline>
-          </Box>
-          <AccentColorProviderWrapper
-            color={
-              assetToReceive?.colors?.primary ||
-              assetToReceive?.colors?.fallback
-            }
-          >
-            <SwapTokenInput
-              asset={assetToReceive}
-              assets={assets}
-              selectAssetAddress={selectAssetToReceiveAddress}
-              onDropdownOpen={onAssetToReceiveInputClick}
-              dropdownClosed={assetToSwapDropdownVisible}
-              setSortMethod={setSortMethod}
-              sortMethod={sortMethod}
-              zIndex={1}
-              placeholder={i18n.t('swap.input_token_to_receive_placeholder')}
-            />
-          </AccentColorProviderWrapper>
-        </Stack>
+              <AccentColorProviderWrapper
+                color={
+                  assetToReceive?.colors?.primary ||
+                  assetToReceive?.colors?.fallback
+                }
+              >
+                <SwapTokenInput
+                  asset={assetToReceive}
+                  assets={assets}
+                  selectAssetAddress={selectAssetToReceiveAddress}
+                  onDropdownOpen={onAssetToReceiveInputClick}
+                  dropdownClosed={assetToSwapDropdownVisible}
+                  setSortMethod={setSortMethod}
+                  sortMethod={sortMethod}
+                  zIndex={1}
+                  placeholder={i18n.t(
+                    'swap.input_token_to_receive_placeholder',
+                  )}
+                />
+              </AccentColorProviderWrapper>
+            </Stack>
+          </Row>
+          <Row height="content">
+            <Box paddingHorizontal="8px">
+              <Button
+                height="44px"
+                variant="flat"
+                color="surfaceSecondary"
+                width="full"
+                disabled
+              >
+                <Text color="labelQuaternary" size="14pt" weight="bold">
+                  {i18n.t('swap.select_tokens_to_swap')}
+                </Text>
+              </Button>
+            </Box>
+          </Row>
+        </Rows>
       </Box>
     </>
   );
