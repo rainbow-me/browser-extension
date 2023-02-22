578a15e9ce09c78659fcc564eab36140f9b31230
Esteban Miño <efmino@uc.cl>
2023-02-22 17:00:03 -0300

    theme provider

--- src/entries/popup/components/Tooltip/Tooltip.tsx
+++ src/entries/popup/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,8 @@
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import React, { ReactNode } from 'react';
 
-import { Box, Inline, Text } from '~/design-system';
+import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
+import { Box, Inline, Text, ThemeProvider } from '~/design-system';
 
 export const Tooltip = ({
   children,
@@ -10,39 +11,43 @@ export const Tooltip = ({
   children: ReactNode;
   text: string;
 }) => {
+  const { currentTheme } = useCurrentThemeStore();
+
   return (
     <TooltipPrimitive.Provider>
       <TooltipPrimitive.Root>
         <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content className="TooltipContent" sideOffset={10}>
-            <Box borderRadius="6px" boxShadow="24px">
-              <Inline alignHorizontal="center">
+            <ThemeProvider theme={currentTheme}>
+              <Box borderRadius="6px" boxShadow="24px">
+                <Inline alignHorizontal="center">
+                  <Box
+                    background="surfaceSecondaryElevated"
+                    backdropFilter="blur(26px)"
+                    position="absolute"
+                    borderRadius="2px"
+                    marginBottom="-3px"
+                    bottom="0"
+                    style={{
+                      height: 10,
+                      width: 10,
+                      rotate: '45deg',
+                    }}
+                  />
+                </Inline>
                 <Box
                   background="surfaceSecondaryElevated"
+                  padding="7px"
+                  borderRadius="6px"
                   backdropFilter="blur(26px)"
-                  position="absolute"
-                  borderRadius="2px"
-                  marginBottom="-3px"
-                  bottom="0"
-                  style={{
-                    height: 10,
-                    width: 10,
-                    rotate: '45deg',
-                  }}
-                />
-              </Inline>
-              <Box
-                background="surfaceSecondaryElevated"
-                padding="7px"
-                borderRadius="6px"
-                backdropFilter="blur(26px)"
-              >
-                <Text color="label" size="16pt" weight="bold">
-                  {text}
-                </Text>
+                >
+                  <Text color="label" size="16pt" weight="bold">
+                    {text}
+                  </Text>
+                </Box>
               </Box>
-            </Box>
+            </ThemeProvider>
           </TooltipPrimitive.Content>
         </TooltipPrimitive.Portal>
       </TooltipPrimitive.Root>
