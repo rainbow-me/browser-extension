c62cb3cd5402ecef01e17ca57135ed5ee812b3f3
Esteban Miño <efmino@uc.cl>
2023-02-24 11:32:57 -0300

    adjust paddings

--- src/entries/popup/pages/swap/SwapTokenInput/TokenToReceiveRow.tsx
+++ src/entries/popup/pages/swap/SwapTokenInput/TokenToReceiveRow.tsx
@@ -1,19 +1,36 @@
-import React, { ReactNode, useMemo } from 'react';
+import React, { ReactNode, useCallback, useMemo } from 'react';
 
+import { i18n } from '~/core/languages';
 import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
 import { UniqueId } from '~/core/types/assets';
+import { ChainId } from '~/core/types/chains';
+import { truncateAddress } from '~/core/utils/address';
+import { getBlockExplorerHostForChain, isL2Chain } from '~/core/utils/chains';
 import {
+  Bleed,
   Box,
   ButtonSymbol,
   Column,
   Columns,
+  Inline,
   Inset,
   Row,
   Rows,
+  Stack,
+  Symbol,
+  Text,
   TextOverflow,
 } from '~/design-system';
 import { rowTransparentAccentHighlight } from '~/design-system/styles/rowTransparentAccentHighlight.css';
 import { CoinIcon } from '~/entries/popup/components/CoinIcon/CoinIcon';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '~/entries/popup/components/DropdownMenu/DropdownMenu';
 import { useUserAsset } from '~/entries/popup/hooks/useUserAsset';
 
 import {
@@ -86,17 +103,125 @@ export function TokenToReceiveRow({ uniqueId }: AssetRowProps) {
     [balanceDisplay, name],
   );
 
+  const viewOnExplorer = useCallback(() => {
+    const explorer = getBlockExplorerHostForChain(
+      asset?.chainId || ChainId.mainnet,
+    );
+    chrome.tabs.create({
+      url: `https://${explorer}/address/${asset?.address}`,
+    });
+  }, [asset?.address, asset?.chainId]);
+
+  const onValueChange = useCallback(
+    (value: string) => {
+      switch (value) {
+        case 'copy':
+          navigator.clipboard.writeText(asset?.address as string);
+          break;
+        case 'view':
+          viewOnExplorer();
+          break;
+      }
+    },
+    [asset?.address, viewOnExplorer],
+  );
+
   const rightColumn = useMemo(
     () => (
-      <ButtonSymbol
-        symbol="info"
-        height="24px"
-        variant="plain"
-        color="fillHorizontal"
-        symbolColor="labelSecondary"
-      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Box>
+            <ButtonSymbol
+              symbol="info"
+              height="24px"
+              variant="plain"
+              color="fillHorizontal"
+              symbolColor="labelSecondary"
+            />
+          </Box>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <Stack space="4px">
+            <Box paddingTop="8px" paddingBottom="12px">
+              <Text
+                align="center"
+                size="14pt"
+                weight="bold"
+                color="label"
+              >{`${asset?.name} (${asset?.symbol})`}</Text>
+            </Box>
+            <Stack space="4px">
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup onValueChange={onValueChange}>
+                <DropdownMenuRadioItem value="copy">
+                  <Box width="full">
+                    <Inline space="8px" alignVertical="center">
+                      <Box>
+                        <Inline alignVertical="center">
+                          <Symbol
+                            symbol="doc.on.doc.fill"
+                            weight="semibold"
+                            size={18}
+                          />
+                        </Inline>
+                      </Box>
+
+                      <Box>
+                        <Stack space="6px">
+                          <Text weight="semibold" size="14pt" color="label">
+                            {i18n.t('contacts.copy_address')}
+                          </Text>
+                          <Text
+                            weight="regular"
+                            size="11pt"
+                            color="labelTertiary"
+                          >
+                            {truncateAddress(asset?.address)}
+                          </Text>
+                        </Stack>
+                      </Box>
+                    </Inline>
+                  </Box>
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="view">
+                  <Box width="full">
+                    <Inline alignVertical="center" alignHorizontal="justify">
+                      <Inline alignVertical="center" space="8px">
+                        <Inline alignVertical="center">
+                          <Symbol
+                            size={18}
+                            symbol="binoculars.fill"
+                            weight="semibold"
+                          />
+                        </Inline>
+                        <Text size="14pt" weight="semibold">
+                          {i18n.t(
+                            `contacts.${
+                              isL2Chain(asset?.chainId || ChainId.mainnet)
+                                ? 'view_on_explorer'
+                                : 'view_on_etherscan'
+                            }`,
+                          )}
+                        </Text>
+                      </Inline>
+                      <Bleed vertical="8px">
+                        <Symbol
+                          size={14}
+                          symbol="arrow.up.forward.circle"
+                          weight="semibold"
+                          color="labelTertiary"
+                        />
+                      </Bleed>
+                    </Inline>
+                  </Box>
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </Stack>
+          </Stack>
+        </DropdownMenuContent>
+      </DropdownMenu>
     ),
-    [],
+    [asset?.address, asset?.chainId, asset?.name, asset?.symbol, onValueChange],
   );
 
   return (
