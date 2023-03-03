b7409ae1663512d97eccebee5476092a0697628b
Esteban Miño <efmino@uc.cl>
2023-03-03 14:17:31 -0300

    fix: BX-423 (#334)

--- src/entries/inpage/index.ts
+++ src/entries/inpage/index.ts
@@ -65,7 +65,7 @@ if (shouldInjectProvider()) {
       },
       addProvider: (provider: RainbowProvider | Ethereum) => {
         if (!window.walletRouter.providers.includes(provider)) {
-          window.walletRouter.providers.push(provider);
+          window.walletRouter.providers.unshift(provider);
         }
         if (rainbowProvider !== provider) {
           window.walletRouter.lastInjectedProvider = provider;
