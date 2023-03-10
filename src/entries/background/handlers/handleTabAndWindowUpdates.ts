842bd9755542831464d0210fcca29563421d1504
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2023-03-10 13:16:52 -0500

    More popup fixes (#347)

---
+++ src/entries/background/handlers/handleTabAndWindowUpdates.ts
@@ -0,0 +1,39 @@
+import { initializeMessenger } from '~/core/messengers';
+import { notificationWindowStore, pendingRequestStore } from '~/core/state';
+
+const bridgeMessenger = initializeMessenger({ connect: 'inpage' });
+
+export const handleTabAndWindowUpdates = () => {
+  // When a tab is removed, check if that was the last tab for that host
+  // if that's the case then we need to remove the pending requests
+  const clearPendingRequestsOnUpdate = (tabId: number) => {
+    const { pendingRequests, removePendingRequest } =
+      pendingRequestStore.getState();
+    pendingRequests.forEach((request) => {
+      if (request.meta?.sender?.tab?.id === tabId) {
+        bridgeMessenger.send(`message:${request?.id}`, null);
+        removePendingRequest(request.id);
+      }
+    });
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  chrome.tabs.onRemoved.addListener(function (tabId, _) {
+    clearPendingRequestsOnUpdate(tabId);
+  });
+
+  chrome.windows.onRemoved.addListener((id) => {
+    const { setNotificationWindow, notificationWindows } =
+      notificationWindowStore.getState();
+
+    for (const [tabId, notificationWindow] of Object.entries(
+      notificationWindows,
+    )) {
+      if (id === notificationWindow?.id) {
+        // The popup has been closed
+        clearPendingRequestsOnUpdate(Number(tabId));
+        setNotificationWindow(tabId, undefined);
+      }
+    }
+  });
+};
