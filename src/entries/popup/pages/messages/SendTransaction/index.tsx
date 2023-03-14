a86c3b741a1a01fb6b4e0d469f1ba0f5f5fbe380
Bruno Barbieri <brunobar79@gmail.com>
2023-03-14 17:16:13 -0400

    fix contract interactions

--- src/entries/popup/pages/messages/SendTransaction/index.tsx
+++ src/entries/popup/pages/messages/SendTransaction/index.tsx
@@ -61,9 +61,10 @@ export function SendTransaction({
         setWaitingForDevice(true);
       }
       const txData = {
-        from: getAddress(txRequest?.from ?? ''),
+        from: selectedWallet,
         to: getAddress(txRequest?.to ?? ''),
         value: txRequest.value,
+        data: txRequest.data ?? '0x',
         chainId: connectedToHardhat ? ChainId.hardhat : appSession.chainId,
       };
       const result = await wallet.sendTransaction(txData);
