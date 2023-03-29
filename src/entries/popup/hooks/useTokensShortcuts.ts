0cf37be9d35e4e5e75f2a8a50edd9e4430fb3f77
derHowie <derhowiedesigns@gmail.com>
2023-03-29 06:24:53 -0500

    gate swap behind flag

--- src/entries/popup/hooks/useTokensShortcuts.ts
+++ src/entries/popup/hooks/useTokensShortcuts.ts
@@ -1,27 +1,48 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
+import { i18n } from '~/core/languages';
 import { shortcuts } from '~/core/references/shortcuts';
+import { useFeatureFlagsStore } from '~/core/state/currentSettings/featureFlags';
 import { useSelectedTokenStore } from '~/core/state/selectedToken';
 
 import { ROUTES } from '../urls';
 
+import { useAlert } from './useAlert';
 import { useKeyboardShortcut } from './useKeyboardShortcut';
 import { useRainbowNavigate } from './useRainbowNavigate';
+import { useWallets } from './useWallets';
 
 export function useTokensShortcuts() {
-  const { selectedToken } = useSelectedTokenStore();
+  const { isWatchingWallet } = useWallets();
+  const { featureFlags } = useFeatureFlagsStore();
+  const { selectedToken, setSelectedToken } = useSelectedTokenStore();
+  const { triggerAlert } = useAlert();
   const navigate = useRainbowNavigate();
+
+  const allowSwap = useMemo(
+    () =>
+      (!isWatchingWallet || featureFlags.full_watching_wallets) &&
+      featureFlags.swaps,
+    [featureFlags.full_watching_wallets, featureFlags.swaps, isWatchingWallet],
+  );
+
   const handleTokenShortcuts = useCallback(
     (e: KeyboardEvent) => {
       if (selectedToken) {
         if (e.key === shortcuts.tokens.SWAP_ASSET.key) {
-          navigate(ROUTES.SWAP);
+          if (allowSwap) {
+            navigate(ROUTES.SWAP);
+          } else {
+            triggerAlert({ text: i18n.t('alert.coming_soon') });
+            // clear selected token
+            setSelectedToken();
+          }
         } else if (e.key === shortcuts.tokens.SEND_ASSET.key) {
           navigate(ROUTES.SEND);
         }
       }
     },
-    [navigate, selectedToken],
+    [allowSwap, navigate, selectedToken, setSelectedToken, triggerAlert],
   );
   useKeyboardShortcut({
     condition: () => !!selectedToken,
--- src/entries/popup/pages/home/TokenDetailsMenu.tsx
+++ src/entries/popup/pages/home/TokenDetailsMenu.tsx
@@ -1,7 +1,14 @@
-import React, { ReactNode, useCallback, useState } from 'react';
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
 import { i18n } from '~/core/languages';
 import { shortcuts } from '~/core/references/shortcuts';
+import { useFeatureFlagsStore } from '~/core/state/currentSettings/featureFlags';
 import { useSelectedTokenStore } from '~/core/state/selectedToken';
 import { ParsedAddressAsset } from '~/core/types/assets';
 import { ChainId } from '~/core/types/chains';
@@ -21,7 +28,9 @@ import {
   DetailsMenuRow,
   DetailsMenuWrapper,
 } from '../../components/DetailsMenu';
+import { useAlert } from '../../hooks/useAlert';
 import { useRainbowNavigate } from '../../hooks/useRainbowNavigate';
+import { useWallets } from '../../hooks/useWallets';
 import { ROUTES } from '../../urls';
 
 type TokenDetailsMenuOption = 'swap' | 'send' | 'view';
@@ -33,7 +42,11 @@ interface TokenDetailsMenuProps {
 export function TokenDetailsMenu({ children, token }: TokenDetailsMenuProps) {
   const [closed, setClosed] = useState(false);
   const onOpenChange = () => setClosed(false);
-  const { setSelectedToken } = useSelectedTokenStore();
+
+  const { isWatchingWallet } = useWallets();
+  const { featureFlags } = useFeatureFlagsStore();
+  const { selectedToken, setSelectedToken } = useSelectedTokenStore();
+  const { triggerAlert } = useAlert();
 
   const navigate = useRainbowNavigate();
 
@@ -47,6 +60,23 @@ export function TokenDetailsMenu({ children, token }: TokenDetailsMenuProps) {
     });
   }, [token]);
 
+  const allowSwap = useMemo(
+    () =>
+      (!isWatchingWallet || featureFlags.full_watching_wallets) &&
+      featureFlags.swaps,
+    [featureFlags.full_watching_wallets, featureFlags.swaps, isWatchingWallet],
+  );
+
+  const goToSwap = useCallback(() => {
+    if (allowSwap) {
+      navigate(ROUTES.SWAP);
+    } else {
+      triggerAlert({ text: i18n.t('alert.coming_soon') });
+      // clear selected token
+      setSelectedToken();
+    }
+  }, [allowSwap, navigate, setSelectedToken, triggerAlert]);
+
   const onValueChange = useCallback(
     (value: TokenDetailsMenuOption) => {
       switch (value) {
@@ -58,18 +88,24 @@ export function TokenDetailsMenu({ children, token }: TokenDetailsMenuProps) {
           setClosed(true);
           break;
         case 'swap':
-          navigate(ROUTES.SWAP);
+          goToSwap();
           setClosed(true);
           break;
       }
     },
-    [navigate, viewOnExplorer],
+    [goToSwap, navigate, viewOnExplorer],
   );
 
   const onTrigger = useCallback(
     () => setSelectedToken(token),
     [setSelectedToken, token],
   );
+
+  useEffect(() => {
+    if (!selectedToken) {
+      setClosed(true);
+    }
+  }, [selectedToken, setClosed]);
 
   return (
     <DetailsMenuWrapper closed={closed} onOpenChange={onOpenChange}>
