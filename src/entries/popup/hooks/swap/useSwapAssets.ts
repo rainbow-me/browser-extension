22a1d5f53354ce567728ea477a09d12e191ecba9
Esteban Miño <efmino@uc.cl>
2023-03-30 16:31:30 -0300

    fix auto flipping when selecting same asset

--- src/entries/popup/hooks/swap/useSwapAssets.ts
+++ src/entries/popup/hooks/swap/useSwapAssets.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { selectUserAssetsList } from '~/core/resources/_selectors';
 import { selectUserAssetsListByChainId } from '~/core/resources/_selectors/assets';
@@ -131,28 +131,65 @@ export const useSwapAssets = () => {
     });
   }, [assetToSell, assetToSellWithPrice, userAssets]);
 
-  const setAssetToSell = useCallback((asset: ParsedSearchAsset | null) => {
-    setAssetToSellState(asset);
-    asset?.chainId && setOutputChainId(asset?.chainId);
-  }, []);
+  const setAssetToSell = useCallback(
+    (asset: ParsedSearchAsset | null) => {
+      asset?.chainId && setOutputChainId(asset?.chainId);
+      if (
+        assetToBuy?.address === asset?.address &&
+        assetToBuy?.chainId === asset?.chainId
+      ) {
+        setAssetToBuy(
+          prevAssetToSell === undefined || prevAssetToSell === null
+            ? null
+            : prevAssetToSell,
+        );
+      }
+      setAssetToSellState(asset);
+    },
+    [assetToBuy?.address, assetToBuy?.chainId, prevAssetToSell],
+  );
 
+  // console.log('=========== ASSET TO BUy', assetToBuy);
   // if user selects assetToBuy as assetToSell we need to flip assets
-  useEffect(() => {
-    if (
-      assetToBuy?.address === assetToSell?.address &&
-      assetToBuy?.chainId === assetToSell?.chainId
-    ) {
-      setAssetToBuy(prevAssetToSell === undefined ? null : prevAssetToSell);
-    }
-  }, [
-    assetToBuy?.address,
-    assetToBuy?.chainId,
-    assetToSell?.address,
-    assetToSell?.chainId,
-    assetToSell?.uniqueId,
-    prevAssetToSell,
-    setAssetToBuy,
-  ]);
+  // useEffect(() => {
+  //   console.log('------------');
+  //   console.log(
+  //     'USE SWAP EFFECT assetToBuy',
+  //     assetToBuy?.address,
+  //     assetToBuy?.chainId,
+  //   );
+  //   console.log(
+  //     'USE SWAP EFFECT assetToSell',
+  //     assetToSell?.address,
+  //     assetToSell?.chainId,
+  //   );
+  //   console.log('USE SWAP EFFECT prevAssetToSell', prevAssetToSell);
+  //   if (
+  //     assetToBuy?.address === assetToSell?.address &&
+  //     assetToBuy?.chainId === assetToSell?.chainId
+  //   ) {
+  //     console.log('USE SWAP EFFECT in swappp', prevAssetToSell);
+  //     console.log(
+  //       'SETTING ASSET TO BUY',
+  //       prevAssetToSell === undefined || prevAssetToSell === null
+  //         ? null
+  //         : prevAssetToSell,
+  //     );
+  //     setAssetToBuy(
+  //       prevAssetToSell === undefined || prevAssetToSell === null
+  //         ? null
+  //         : prevAssetToSell,
+  //     );
+  //   }
+  // }, [
+  //   assetToBuy?.address,
+  //   assetToBuy?.chainId,
+  //   assetToSell?.address,
+  //   assetToSell?.chainId,
+  //   assetToSell?.uniqueId,
+  //   prevAssetToSell,
+  //   setAssetToBuy,
+  // ]);
 
   return {
     assetsToSell: filteredAssetsToSell,
