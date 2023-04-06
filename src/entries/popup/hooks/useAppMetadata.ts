92dce8349f274b5fbb0eeb0c9c752fd717e84425
Esteban Miño <efmino@uc.cl>
2023-04-06 13:48:27 -0400

    only use names

--- src/entries/popup/hooks/useAppMetadata.ts
+++ src/entries/popup/hooks/useAppMetadata.ts
@@ -12,12 +12,11 @@ interface AppMetadata {
 
 export function useAppMetadata({ url, title }: AppMetadata) {
   const appHostName = url ? getDappHostname(url) : '';
-  const hardcodedDappInfo = getHardcodedDappInformation(appHostName);
   const appHost = url ? getDappHost(url) : '';
-  const appLogo = url
-    ? hardcodedDappInfo?.uri || getPublicAppIcon(appHost)
+  const appLogo = url ? getPublicAppIcon(appHost) : '';
+  const appName = url
+    ? getHardcodedDappInformation(appHostName)?.name || title
     : '';
-  const appName = url ? hardcodedDappInfo?.name || title : '';
 
   return {
     appHost,
