e94136bde9a45601fce140982e015c47586a8a39
derHowie <derhowiedesigns@gmail.com>
2023-04-07 13:42:58 -0500

    finish settings rows

--- src/entries/popup/pages/settings/settings.tsx
+++ src/entries/popup/pages/settings/settings.tsx
@@ -185,6 +185,7 @@ export function Settings() {
               open={themeDropdownOpen}
               renderMenuTrigger={
                 <MenuItem
+                  tabIndex={-1}
                   hasChevron
                   leftComponent={
                     <Symbol
