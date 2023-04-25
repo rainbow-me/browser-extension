a1f0595ab08faa36bf6b460fd2b7387f78352ce6
Christopher Howard <derhowiedesigns@gmail.com>
2023-04-25 15:44:31 -0500

    [BX-596] Home Screen Indexes Regression (#476)

--- src/entries/popup/pages/home/NetworkMenu.tsx
+++ src/entries/popup/pages/home/NetworkMenu.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import appConnectionImageMask from 'static/assets/appConnectionImageMask.svg';
 import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
 import { Bleed, Box, Inline, Symbol } from '~/design-system';
+import { Lens } from '~/design-system/components/Lens/Lens';
 import {
   transformScales,
   transitions,
@@ -15,6 +16,7 @@ import ExternalImage from '../../components/ExternalImage/ExternalImage';
 import { Navbar } from '../../components/Navbar/Navbar';
 import { useAppMetadata } from '../../hooks/useAppMetadata';
 import { useAppSession } from '../../hooks/useAppSession';
+import { tabIndexes } from '../../utils/tabIndexes';
 
 export const AppConnection = () => {
   const [url, setUrl] = React.useState('');
@@ -50,66 +52,45 @@ export const AppConnection = () => {
       url={url}
     >
       {url ? (
-        <Box
-          as={motion.div}
-          initial={{ zIndex: 0 }}
-          whileHover={{ scale: transformScales['1.04'] }}
-          whileTap={{ scale: transformScales['0.96'] }}
-          transition={transitions.bounce}
-          style={{
-            height: 32,
-            width: 32,
-          }}
+        <Lens
+          tabIndex={tabIndexes.WALLET_HEADER_LEFT_BUTTON}
           borderRadius="round"
-          background="surfaceSecondaryElevated"
-          borderColor="separatorTertiary"
-          borderWidth="1px"
         >
-          <Inline alignHorizontal="center" alignVertical="center" height="full">
-            <Box
-              position="absolute"
-              style={{
-                paddingLeft: 24,
-                paddingBottom: 24,
-              }}
+          <Box
+            as={motion.div}
+            initial={{ zIndex: 0 }}
+            whileHover={{ scale: transformScales['1.04'] }}
+            whileTap={{ scale: transformScales['0.96'] }}
+            transition={transitions.bounce}
+            style={{
+              height: 32,
+              width: 32,
+            }}
+            borderRadius="round"
+            background="surfaceSecondaryElevated"
+            borderColor="separatorTertiary"
+            borderWidth="1px"
+          >
+            <Inline
+              alignHorizontal="center"
+              alignVertical="center"
+              height="full"
             >
-              <Box
-                backdropFilter="opacity(0%)"
-                style={{
-                  height: 10,
-                  width: 10,
-                  borderRadius: 5,
-                  backgroundColor:
-                    currentTheme === 'dark' ? '#1D1E21' : '#F8F8F9',
-                }}
-              >
-                <Inline
-                  alignHorizontal="center"
-                  alignVertical="center"
-                  height="full"
-                >
-                  <Symbol
-                    size={6}
-                    color={appSession ? 'green' : 'labelQuaternary'}
-                    symbol="circle.fill"
-                    weight="semibold"
-                  />
-                </Inline>
-              </Box>
-            </Box>
-            {appSession ? (
               <Box
                 position="absolute"
                 style={{
-                  marginRight: 16,
-                  marginTop: 8,
+                  paddingLeft: 24,
+                  paddingBottom: 24,
                 }}
               >
                 <Box
+                  backdropFilter="opacity(0%)"
                   style={{
                     height: 10,
                     width: 10,
                     borderRadius: 5,
+                    backgroundColor:
+                      currentTheme === 'dark' ? '#1D1E21' : '#F8F8F9',
                   }}
                 >
                   <Inline
@@ -117,35 +98,68 @@ export const AppConnection = () => {
                     alignVertical="center"
                     height="full"
                   >
-                    <Bleed top="7px">
-                      <ChainBadge chainId={appSession?.chainId} size="micro" />
-                    </Bleed>
+                    <Symbol
+                      size={6}
+                      color={appSession ? 'green' : 'labelQuaternary'}
+                      symbol="circle.fill"
+                      weight="semibold"
+                    />
                   </Inline>
                 </Box>
               </Box>
-            ) : null}
-            <Box
-              style={{
-                height: 16,
-                width: 16,
-                borderRadius: 3,
-                overflow: 'hidden',
-              }}
-            >
-              <ExternalImage
-                mask={appSession ? appConnectionImageMask : undefined}
-                src={appLogo}
-                width="16"
-                height="16"
-              />
-            </Box>
-          </Inline>
-        </Box>
+              {appSession ? (
+                <Box
+                  position="absolute"
+                  style={{
+                    marginRight: 16,
+                    marginTop: 8,
+                  }}
+                >
+                  <Box
+                    style={{
+                      height: 10,
+                      width: 10,
+                      borderRadius: 5,
+                    }}
+                  >
+                    <Inline
+                      alignHorizontal="center"
+                      alignVertical="center"
+                      height="full"
+                    >
+                      <Bleed top="7px">
+                        <ChainBadge
+                          chainId={appSession?.chainId}
+                          size="micro"
+                        />
+                      </Bleed>
+                    </Inline>
+                  </Box>
+                </Box>
+              ) : null}
+              <Box
+                style={{
+                  height: 16,
+                  width: 16,
+                  borderRadius: 3,
+                  overflow: 'hidden',
+                }}
+              >
+                <ExternalImage
+                  mask={appSession ? appConnectionImageMask : undefined}
+                  src={appLogo}
+                  width="16"
+                  height="16"
+                />
+              </Box>
+            </Inline>
+          </Box>
+        </Lens>
       ) : (
         <Navbar.SymbolButton
           symbol="app.badge.checkmark"
           variant="flat"
-          tabIndex={1}
+          tabIndex={tabIndexes.WALLET_HEADER_LEFT_BUTTON}
         />
       )}
     </AppConnectionMenu>
