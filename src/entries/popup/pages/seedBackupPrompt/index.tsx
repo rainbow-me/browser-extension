0b03355e7be6776970022916ec9a3a9eb1d1c82c
Esteban Miño <efmino@uc.cl>
2023-05-01 17:13:24 -0400

    fix backup

--- src/entries/popup/pages/seedBackupPrompt/index.tsx
+++ src/entries/popup/pages/seedBackupPrompt/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { i18n } from '~/core/languages';
 import { Box, Button, Rows, Symbol, Text } from '~/design-system';
 
+import { FullScreenContainer } from '../../components/FullScreen/FullScreenContainer';
 import {
   IconAndCopyItem,
   IconAndCopyList,
@@ -37,99 +38,97 @@ const iconAndCopyList: IconAndCopyItem[] = [
 export function SeedBackupPrompt() {
   const navigate = useRainbowNavigate();
 
-  const handleShowRecoveryPhraseClick = React.useCallback(async () => {
+  const handleShowRecoveryPhraseClick = React.useCallback(() => {
     navigate(ROUTES.SEED_REVEAL);
   }, [navigate]);
 
-  const handleSkipClick = React.useCallback(async () => {
+  const handleSkipClick = React.useCallback(() => {
     navigate(ROUTES.CREATE_PASSWORD);
   }, [navigate]);
 
   return (
-    <Box
-      borderColor="separatorSecondary"
-      borderWidth="1px"
-      background="surfaceSecondary"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      padding="24px"
-      paddingTop="32px"
-    >
+    <FullScreenContainer>
       <Box
-        borderRadius="round"
-        boxShadow="18px accent"
-        borderWidth="1px"
-        style={{
-          height: 60,
-          width: 60,
-          overflow: 'hidden',
-          position: 'absolute',
-          borderColor: 'rgba(255, 255, 255, 0.03)',
-        }}
+        background="surfaceSecondary"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
       >
         <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          height="full"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          position="relative"
+          borderRadius="round"
+          boxShadow="18px accent"
+          borderWidth="1px"
           style={{
-            background:
-              'radial-gradient(100% 144.46% at 0% 50%, #FF9233 0%, #FA423C 100%)',
-            zIndex: 1,
+            height: 60,
+            width: 60,
+            overflow: 'hidden',
+            position: 'absolute',
+            borderColor: 'rgba(255, 255, 255, 0.03)',
           }}
         >
-          <Symbol
-            symbol="exclamationmark.triangle.fill"
-            size={26}
-            color="label"
-            weight={'bold'}
-          />
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            height="full"
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
+            position="relative"
+            style={{
+              background:
+                'radial-gradient(100% 144.46% at 0% 50%, #FF9233 0%, #FA423C 100%)',
+              zIndex: 1,
+            }}
+          >
+            <Symbol
+              symbol="exclamationmark.triangle.fill"
+              size={26}
+              color="label"
+              weight={'bold'}
+            />
+          </Box>
+        </Box>
+        <Box
+          alignItems="center"
+          style={{ paddingTop: '84px', paddingBottom: '49px' }}
+        >
+          <Text size="16pt" weight="bold" color="label" align="center">
+            {i18n.t('seed_backup_prompt.title')}
+          </Text>
+        </Box>
+        <Box padding="12px">
+          <IconAndCopyList iconAndCopyList={iconAndCopyList} />
+        </Box>
+        <Box width="full" paddingTop="52px">
+          <Rows alignVertical="top" space="8px">
+            <Button
+              color="accent"
+              height="44px"
+              variant="flat"
+              width="full"
+              symbol="checkmark.circle.fill"
+              blur="26px"
+              onClick={handleShowRecoveryPhraseClick}
+              testId="show-recovery-phrase-button"
+            >
+              {i18n.t('seed_backup_prompt.reveal_your_recovery_phrase')}
+            </Button>
+
+            <Button
+              color="labelSecondary"
+              height="44px"
+              variant="transparent"
+              width="full"
+              onClick={handleSkipClick}
+              testId="skip-button"
+            >
+              {i18n.t('seed_backup_prompt.skip')}
+            </Button>
+          </Rows>
         </Box>
       </Box>
-      <Box
-        alignItems="center"
-        style={{ paddingTop: '84px', paddingBottom: '49px' }}
-      >
-        <Text size="16pt" weight="bold" color="label" align="center">
-          {i18n.t('seed_backup_prompt.title')}
-        </Text>
-      </Box>
-      <Box padding="12px">
-        <IconAndCopyList iconAndCopyList={iconAndCopyList} />
-      </Box>
-      <Box width="full" paddingTop="52px">
-        <Rows alignVertical="top" space="8px">
-          <Button
-            color="accent"
-            height="44px"
-            variant="flat"
-            width="full"
-            symbol="checkmark.circle.fill"
-            blur="26px"
-            onClick={handleShowRecoveryPhraseClick}
-            testId="show-recovery-phrase-button"
-          >
-            {i18n.t('seed_backup_prompt.reveal_your_recovery_phrase')}
-          </Button>
-
-          <Button
-            color="labelSecondary"
-            height="44px"
-            variant="transparent"
-            width="full"
-            onClick={handleSkipClick}
-            testId="skip-button"
-          >
-            {i18n.t('seed_backup_prompt.skip')}
-          </Button>
-        </Rows>
-      </Box>
-    </Box>
+    </FullScreenContainer>
   );
 }
