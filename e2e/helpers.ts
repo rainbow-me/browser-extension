0d45704cd29b8228a22d2a7636054b497d895570
gregs <ukfaeku@gmail.com>
2023-05-18 08:59:06 -0300

    helpers

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -2,7 +2,14 @@
 /* eslint-disable no-promise-executor-return */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { ethers } from 'ethers';
-import { Builder, By, WebDriver, until } from 'selenium-webdriver';
+import {
+  Builder,
+  By,
+  Locator,
+  WebDriver,
+  WebElementCondition,
+  until,
+} from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 import { erc20ABI } from 'wagmi';
 
@@ -18,6 +25,10 @@ const BINARY_PATHS = {
     brave: process.env.BRAVE_BIN,
   },
 };
+
+export const byTestId = (id: string) => By.css(`[data-testid="${id}"]`);
+export const byText = (text: string) =>
+  By.xpath(`//*[contains(text(),"${text}")]`);
 
 export async function querySelector(driver, selector) {
   const el = await driver.wait(
@@ -255,3 +266,12 @@ export async function getWindowHandle({ driver }) {
   const windowHandle = await driver.getWindowHandle();
   return windowHandle;
 }
+
+export const untilIsClickable = (locator: Locator) =>
+  new WebElementCondition('until element is clickable', async (driver) => {
+    const element = driver.findElement(locator);
+    const isDisplayed = await element.isDisplayed();
+    const isEnabled = await element.isEnabled();
+    if (isDisplayed && isEnabled) return element;
+    return null;
+  });
