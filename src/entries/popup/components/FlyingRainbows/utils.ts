bc981db39adcd91f71fd2a1c868e5f4a7f3156f4
Esteban Miño <efmino@uc.cl>
2023-05-19 11:11:31 -0400

    feat: beta invite code (#539)

---
+++ src/entries/popup/components/FlyingRainbows/utils.ts
@@ -0,0 +1,111 @@
+import { Transition } from 'framer-motion';
+
+export type FlyingRainbowsScreen = 'welcome' | 'invite_code';
+export type RainbowType =
+  | 'rainbowPixel'
+  | 'rainbowWhite'
+  | 'rainbowOg'
+  | 'rainbowLight'
+  | 'rainbowNeon';
+
+export const RAINBOW_POSITION: {
+  [key in RainbowType]: {
+    [key in FlyingRainbowsScreen]: {
+      left?: string;
+      right?: string;
+      top?: string;
+      bottom?: string;
+    };
+  };
+} = {
+  rainbowPixel: {
+    welcome: {
+      left: '207.36px',
+      top: '-77px',
+    },
+    invite_code: {
+      left: '223px',
+      top: '-156px',
+    },
+  },
+  rainbowWhite: {
+    welcome: {
+      left: '-33.98px',
+      top: '-4.21px',
+    },
+    invite_code: {
+      left: '-92.64px',
+      top: '-49.64px',
+    },
+  },
+  rainbowOg: {
+    welcome: {
+      left: '-46.1px',
+      top: '378.17px',
+    },
+    invite_code: {
+      left: '-76.1px',
+      top: '348.17px',
+    },
+  },
+  rainbowLight: {
+    welcome: {
+      left: '120.92px',
+      top: '385.78px',
+    },
+    invite_code: {
+      left: '16.03px',
+      top: '525.03px',
+    },
+  },
+  rainbowNeon: {
+    welcome: {
+      left: '234.12px',
+      top: '549.4px',
+    },
+    invite_code: {
+      left: '298.05px',
+      top: '540.05px',
+    },
+  },
+};
+
+export const RAINBOW_TRANSITION: Transition = {
+  type: 'spring',
+  stiffness: 100,
+  damping: 15,
+  mass: 1,
+};
+
+export const RAINBOW_STYLE: {
+  [key in RainbowType]: {
+    width?: string;
+    height?: string;
+    rotate?: string;
+  };
+} = {
+  rainbowPixel: {
+    width: '225.17px',
+    height: '225.17px',
+  },
+  rainbowWhite: {
+    width: '171.34px',
+    height: '171.34px',
+    rotate: '150deg',
+  },
+  rainbowOg: {
+    height: '95.58px',
+    width: '95.58px',
+    rotate: '-33.07deg',
+  },
+  rainbowLight: {
+    width: '142.69px',
+    height: '142.69px',
+    rotate: '75deg',
+  },
+  rainbowNeon: {
+    width: '112.58px',
+    height: '112.58px',
+    rotate: '34.75deg',
+  },
+};
