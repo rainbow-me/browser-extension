99939361d13a277ccbe22fb28a6c0d758f22b66a
Esteban Miño <efmino@uc.cl>
2023-05-19 10:35:50 -0400

    update url

--- src/core/network/inviteCode.ts
+++ src/core/network/inviteCode.ts
@@ -1,5 +1,5 @@
 import { createHttpClient } from './internal/createHttpClient';
 
 export const betaInviteCodesHttp = createHttpClient({
-  baseUrl: 'https://rainbow-beta-invite-codes.vercel.app',
+  baseUrl: 'https://beta-invites.rainbow.me',
 });
