e731b40b7ea1beb8af5d3b1e4010b549e241b487
derHowie <derhowiedesigns@gmail.com>
2023-05-25 16:42:25 -0500

    back 2 screens after successful watch

--- src/entries/popup/hooks/useRainbowNavigate.ts
+++ src/entries/popup/hooks/useRainbowNavigate.ts
@@ -9,8 +9,13 @@ export function useRainbowNavigate() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  return function (to: To, options?: NavigateOptions) {
-    navigate(to, {
+  return function (to: To | number, options?: NavigateOptions) {
+    if (typeof to === 'number') {
+      navigate(to);
+      return;
+    }
+
+    navigate(to as To, {
       ...(options || {}),
       state: { ...options?.state, from: location.pathname },
     });
