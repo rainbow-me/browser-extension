4d81469e7376edcf9eaac37b1196445bfa427393
Bruno Barbieri <brunobar79@gmail.com>
2023-06-05 18:13:55 -0400

    use useQuery

--- src/entries/popup/hooks/useENSAvatar.ts
+++ src/entries/popup/hooks/useENSAvatar.ts
@@ -1,58 +1,28 @@
-import { isAddress } from '@ethersproject/address';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Address } from 'wagmi';
 
-import { metadataClient } from '~/core/graphql';
-import { isENSAddressFormat } from '~/core/utils/ethereum';
+import { useResolveEnsProfileAvatar } from '~/core/resources/metadata/resolveEnsProfileAvatar';
 
 export const useENSAvatar = ({
   addressOrName,
 }: {
   addressOrName: Address | string | undefined;
 }) => {
-  const [avatar, setAvatar] = useState<string | null>(null);
   const [isFetched, setIsFeched] = useState<boolean>(false);
 
-  const reverseResolveENSProfile = useCallback(async (address: Address) => {
-    const response = await metadataClient.reverseResolveENSProfile({
-      chainId: 1,
-      address: address,
-      fields: ['avatar'],
-    });
-
-    if (response?.reverseResolveENSProfile?.fields?.length) {
-      return response.reverseResolveENSProfile.fields[0].value;
-    }
-    return null;
-  }, []);
-
-  const resolveENSProfile = useCallback(async (name: string) => {
-    const response = await metadataClient.resolveENSProfile({
-      chainId: 1,
-      name: name,
-      fields: ['avatar'],
-    });
-
-    if (response?.resolveENSProfile?.fields?.length) {
-      return response.resolveENSProfile.fields[0].value;
-    }
-    return null;
-  }, []);
+  const {
+    data: avatar,
+    isLoading,
+    isError,
+  } = useResolveEnsProfileAvatar({
+    addressOrName: addressOrName || '',
+  });
 
   useEffect(() => {
-    const fetchAvatar = async () => {
-      let data = null;
-      if (addressOrName && isAddress(addressOrName)) {
-        data = await reverseResolveENSProfile(addressOrName);
-        setIsFeched(true);
-      } else if (addressOrName && isENSAddressFormat(addressOrName)) {
-        data = await resolveENSProfile(addressOrName);
-        setIsFeched(true);
-      }
-      setAvatar(data);
-    };
-    fetchAvatar();
-  }, [addressOrName, resolveENSProfile, reverseResolveENSProfile]);
+    if (avatar && !isLoading && !isError) {
+      setIsFeched(true);
+    }
+  }, [avatar, isError, isLoading]);
 
   return { data: avatar, isFetched };
 };
