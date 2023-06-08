53c1e9a75693ce70c99a7b2a7af31ca86ec32453
Christopher Howard <derhowiedesigns@gmail.com>
2023-06-08 17:32:51 -0500

    [BX-786][BX-710] Prevent Flip Shortcut From Clearing Swap Input Values (#615)
    Co-authored-by: Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>

--- src/entries/popup/pages/swap/index.tsx
+++ src/entries/popup/pages/swap/index.tsx
@@ -336,26 +336,8 @@ export function Swap() {
   useKeyboardShortcut({
     handler: (e: KeyboardEvent) => {
       if (e.key === shortcuts.swap.FLIP_ASSETS.key) {
-        const activeElement = document.activeElement;
-        const focusingAssetToSell =
-          activeElement === assetToSellInputRef.current;
-        const focusingAssetToBuy = activeElement === assetToBuyInputRef.current;
-        const focusNewInput = () => {
-          setTimeout(() => {
-            if (focusingAssetToSell) {
-              assetToBuyInputRef.current?.focus();
-            } else if (focusingAssetToBuy) {
-              assetToSellInputRef.current?.focus();
-            }
-          }, 100);
-        };
-        if (focusingAssetToSell && assetToSell) {
-          flipAssets();
-          focusNewInput();
-        } else if (focusingAssetToBuy && assetToBuy) {
-          flipAssets();
-          focusNewInput();
-        }
+        e.preventDefault();
+        flipAssets();
       }
     },
   });
