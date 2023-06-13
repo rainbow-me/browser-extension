70b0c5fc3a3b69575775aedc0483e92b95418557
gregs <ukfaeku@gmail.com>
2023-06-13 16:13:04 -0300

    remove optional chainings

--- src/core/resources/_selectors/transactions.ts
+++ src/core/resources/_selectors/transactions.ts
@@ -10,10 +10,10 @@ export const selectTransactionsByDate = (
     if (tx1.pending && tx2.pending) return (tx2.nonce || 0) - (tx1.nonce || 0);
     if (tx1.pending || tx2.pending) return -1;
 
-    if (!tx1?.minedAt) return -1;
-    if (!tx2?.minedAt) return 1;
+    if (!tx1.minedAt) return -1;
+    if (!tx2.minedAt) return 1;
 
-    return (tx2?.minedAt || 0) - (tx1?.minedAt || 0);
+    return (tx2.minedAt || 0) - (tx1.minedAt || 0);
   });
   return groupBy(sortedTransactions, groupTransactionByDate);
 };
