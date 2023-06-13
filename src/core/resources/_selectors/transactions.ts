ab94788c0054ed746f33c9cf5d87a83ee207c894
gregs <ukfaeku@gmail.com>
2023-06-13 16:03:25 -0300

    fix pending activity order

--- src/core/resources/_selectors/transactions.ts
+++ src/core/resources/_selectors/transactions.ts
@@ -7,11 +7,13 @@ export const selectTransactionsByDate = (
   transactions: RainbowTransaction[],
 ) => {
   const sortedTransactions = transactions.sort((tx1, tx2) => {
-    if (!tx1?.minedAt) {
-      return -1;
-    } else if (!tx2?.minedAt) {
-      return 1;
-    }
+    if (tx1.pending && tx2.pending) return (tx2.nonce || 0) - (tx1.nonce || 0);
+    if (tx1.pending) return 1;
+    if (tx2.pending) return 1;
+
+    if (!tx1?.minedAt) return -1;
+    if (!tx2?.minedAt) return 1;
+
     return (tx2?.minedAt || 0) - (tx1?.minedAt || 0);
   });
   return groupBy(sortedTransactions, groupTransactionByDate);
