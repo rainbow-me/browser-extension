ebd56e18c5f339075186499ae688a80ad3aab5bf
Bruno Barbieri <brunobar79@gmail.com>
2023-06-21 19:18:49 -0400

    disable plugin in builds

--- scripts/webpack.js
+++ scripts/webpack.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const webpack = require('webpack');
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+// const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 const config = require('../webpack.config');
 const DepShieldPlugin = require('../DepShield');
@@ -33,19 +33,19 @@ webpack({ ...config,
   mode: 'production',
   plugins: [
     ...config.plugins,
-    new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /node_modules/,
-      // include specific files based on a RegExp
-      include: /src/,
-      // add errors to webpack instead of warnings
-      failOnError: true,
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
-      allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
-      cwd: process.cwd(),
-    }),
+    // new CircularDependencyPlugin({
+    //   // exclude detection of files based on a RegExp
+    //   exclude: /node_modules/,
+    //   // include specific files based on a RegExp
+    //   include: /src/,
+    //   // add errors to webpack instead of warnings
+    //   failOnError: true,
+    //   // allow import cycles that include an asyncronous import,
+    //   // e.g. via import(/* webpackMode: "weak" */ './file.js')
+    //   allowAsyncCycles: false,
+    //   // set the current working directory for displaying module paths
+    //   cwd: process.cwd(),
+    // }),
     new DepShieldPlugin({
       failOnError: true,
       cwd: process.cwd(),
