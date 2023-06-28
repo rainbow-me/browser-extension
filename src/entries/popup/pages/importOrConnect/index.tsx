bdd062407f599ed3f20e907d85fba84a56b1e529
Christopher Howard <derhowiedesigns@gmail.com>
2023-06-28 14:24:53 -0500

    Add TabIndex To A Few Place in HWW Flow (#696)

--- src/entries/popup/pages/importOrConnect/index.tsx
+++ src/entries/popup/pages/importOrConnect/index.tsx
@@ -6,7 +6,6 @@ import { i18n } from '~/core/languages';
 import { useFeatureFlagsStore } from '~/core/state/currentSettings/featureFlags';
 import { Box, Separator, Stack, Text } from '~/design-system';
 import { triggerAlert } from '~/design-system/components/Alert/util';
-import { Lens } from '~/design-system/components/Lens/Lens';
 
 import { FullScreenContainer } from '../../components/FullScreen/FullScreenContainer';
 import { OnboardMenu } from '../../components/OnboardMenu/OnboardMenu';
@@ -72,57 +71,35 @@ export function ImportOrConnect() {
         </Box>
         <Box>
           <OnboardMenu>
-            <Lens
-              onKeyDown={onImportWalletClick}
-              borderRadius="16px"
-              marginHorizontal="-20px"
-              paddingHorizontal="20px"
-            >
-              <OnboardMenu.Item
-                onClick={onImportWalletClick}
-                title={i18n.t('import_or_connect.import_wallet')}
-                subtitle={i18n.t('import_or_connect.import_wallet_description')}
-                symbol="lock.rotation"
-                symbolColor="purple"
-                testId="import-wallet-option"
-              />
-            </Lens>
+            <OnboardMenu.Item
+              first
+              onClick={onImportWalletClick}
+              title={i18n.t('import_or_connect.import_wallet')}
+              subtitle={i18n.t('import_or_connect.import_wallet_description')}
+              symbol="lock.rotation"
+              symbolColor="purple"
+              testId="import-wallet-option"
+            />
+            <OnboardMenu.Separator />
+            <OnboardMenu.Item
+              onClick={onConnectHardwareWallet}
+              title={i18n.t('import_or_connect.connect_wallet')}
+              subtitle={i18n.t('import_or_connect.connect_wallet_description')}
+              symbol="doc.text.magnifyingglass"
+              symbolColor="blue"
+              testId="connect-wallet-option"
+            />
 
             <OnboardMenu.Separator />
-            <Lens
-              onKeyDown={onConnectHardwareWallet}
-              borderRadius="16px"
-              marginHorizontal="-20px"
-              paddingHorizontal="20px"
-            >
-              <OnboardMenu.Item
-                onClick={onConnectHardwareWallet}
-                title={i18n.t('import_or_connect.connect_wallet')}
-                subtitle={i18n.t(
-                  'import_or_connect.connect_wallet_description',
-                )}
-                symbol="doc.text.magnifyingglass"
-                symbolColor="blue"
-                testId="connect-wallet-option"
-              />
-            </Lens>
-
-            <OnboardMenu.Separator />
-            <Lens
-              onKeyDown={onWatchEthereumAddress}
-              borderRadius="16px"
-              marginHorizontal="-20px"
-              paddingHorizontal="20px"
-            >
-              <OnboardMenu.Item
-                onClick={onWatchEthereumAddress}
-                title={i18n.t('import_or_connect.watch_address')}
-                subtitle={i18n.t('import_or_connect.watch_address_description')}
-                symbol="magnifyingglass.circle"
-                symbolColor="green"
-                testId="watch-wallet-option"
-              />
-            </Lens>
+            <OnboardMenu.Item
+              onClick={onWatchEthereumAddress}
+              title={i18n.t('import_or_connect.watch_address')}
+              subtitle={i18n.t('import_or_connect.watch_address_description')}
+              symbol="magnifyingglass.circle"
+              symbolColor="green"
+              testId="watch-wallet-option"
+              last
+            />
           </OnboardMenu>
         </Box>
       </Stack>
