33f0ab80a532f9793fe044f078324ee8eadbaef7
gregs <gregs@rainbow.me>
2023-07-01 23:15:00 -0300

    flow transitions (#691)

--- src/entries/popup/pages/home/TabBar.tsx
+++ src/entries/popup/pages/home/TabBar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { useAccount, useBalance } from 'wagmi';
 
 import { supportedCurrencies } from '~/core/references';
