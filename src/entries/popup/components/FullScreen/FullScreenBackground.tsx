52cd5241c7357b34656c6a61350abc55696409e3
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2023-07-09 13:05:43 -0400

    Remove animated full screen blur
    Was causing significant resource usage when in full screen mode

--- src/entries/popup/components/FullScreen/FullScreenBackground.tsx
+++ src/entries/popup/components/FullScreen/FullScreenBackground.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
+import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
 import { POPUP_DIMENSIONS } from '~/core/utils/dimensions';
 import { Box } from '~/design-system';
 
 import { useIsFullScreen } from '../../hooks/useIsFullScreen';
-
-import { Blur } from './Blur';
 
 export function FullScreenBackground({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { currentTheme } = useCurrentThemeStore();
   const isFullscreen = useIsFullScreen();
+
   if (!isFullscreen) return children as JSX.Element;
+
   return (
     <Box
       position="absolute"
@@ -23,32 +25,31 @@ export function FullScreenBackground({
       alignItems="center"
       justifyContent="center"
       flexDirection="column"
-      background="surfacePrimary"
       style={{
+        backgroundColor: currentTheme === 'dark' ? '#131314' : '#E7E8E9',
         width: '100vw',
         height: '100vh',
       }}
     >
-      <Blur />
       <Box
         width="full"
         height="full"
         style={{
           position: 'absolute',
           top: '0px',
+          backgroundImage: 'url(assets/bg/noise.png)',
+          backgroundSize: '50%',
+          backgroundRepeat: 'repeat',
+          opacity: 0.25,
+          mixBlendMode: 'overlay',
           left: '0px',
           width: '100vw',
           height: '100vh',
-          backgroundImage: 'url(assets/bg/noise.png)',
-          opacity: 0.4,
-          backgroundRepeat: 'repeat',
-          mixBlendMode: 'soft-light',
         }}
-        background="surfacePrimary"
       />
       <Box
         borderRadius="32px"
-        background="surfaceSecondary"
+        background="surfacePrimaryElevated"
         style={{
           width: POPUP_DIMENSIONS.width,
           height: POPUP_DIMENSIONS.height,
