5d6297e46428670eaf9d801a5215a08673ac3b33
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2023-07-09 13:19:57 -0400

    Fix jagged <Symbol> icon edges
    Bit of a hack to smooth out the edges of <Symbol> icons — Chrome otherwise renders the SVGs with jagged edges

--- src/design-system/components/Symbol/Symbol.tsx
+++ src/design-system/components/Symbol/Symbol.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { Box } from '~/design-system';
 import { FontWeight, SymbolName } from '~/design-system/styles/designTokens';
 
 import { SymbolStyles, symbolStyles } from '../../styles/core.css';
@@ -25,19 +26,30 @@ export function Symbol({
   const symbol = symbols[name as keyof typeof symbols][weight];
 
   return (
-    <svg
-      cursor={cursor}
-      viewBox={`0 0 ${symbol.viewBox.width} ${symbol.viewBox.height}`}
-      fill="none"
-      className={symbolStyles({ color })}
-      style={{ width: size, height: size }}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {gradient ? <defs>{gradient}</defs> : null}
-      <path
-        d={symbol.path}
-        fill={gradient ? 'url(#gradient)' : 'currentColor'}
-      />
-    </svg>
+    <Box style={{ height: size, width: size }}>
+      <Box
+        style={{
+          transform: 'scale(0.5)',
+          transformOrigin: 'top left',
+          willChange: 'transform',
+        }}
+      >
+        <svg
+          cursor={cursor}
+          viewBox={`0 0 ${symbol.viewBox.width} ${symbol.viewBox.height}`}
+          fill="none"
+          className={symbolStyles({ color })}
+          style={{ width: size * 2, height: size * 2 }}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {gradient ? <defs>{gradient}</defs> : null}
+          <path
+            d={symbol.path}
+            fill={gradient ? 'url(#gradient)' : 'currentColor'}
+            shapeRendering="geometricPrecision"
+          />
+        </svg>
+      </Box>
+    </Box>
   );
 }
