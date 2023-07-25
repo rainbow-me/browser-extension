70ba6e361e1a7973b00e809b73366f7a03f2f488
Daniel Sinclair <d@niel.nyc>
2023-07-25 17:48:12 -0400

    fix: amend screen event naming scheme

--- src/analytics/screen.ts
+++ src/analytics/screen.ts
@@ -5,7 +5,11 @@ import { ROUTES } from '~/entries/popup/urls';
 /**
  * All screen names by path, used by `analytics.screen()`.
  * We're flipping the key/value pairs from `ROUTES` to avoid maintenance.
+ * SETTINGS__PRIVACY__WALLETS_AND_KEYS -> settings.privacy.wallets_and_keys
  */
 export const screen = Object.fromEntries(
-  Object.entries(ROUTES).map(([key, value]) => [value, key.toLowerCase()]),
+  Object.entries(ROUTES).map(([key, value]) => [
+    value,
+    key.toLowerCase().replace('__', '.'),
+  ]),
 );
