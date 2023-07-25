d0af590b46c3e37681020ebfc221f70f52c376ae
Daniel Sinclair <d@niel.nyc>
2023-07-25 18:36:43 -0400

    feat: wallet type user properties

--- src/analytics/userProperties.ts
+++ src/analytics/userProperties.ts
@@ -1,3 +1,11 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 // these are all reported seperately so they must be optional
-export interface UserProperties {}
+export interface UserProperties {
+  hotWallets?: number;
+  hardwareWallets?: number;
+  watchedWallets?: number;
+  recoveryPhrases?: number;
+  privateKeys?: number;
+  trezorDevices?: number;
+  ledgerDevices?: number;
+}
