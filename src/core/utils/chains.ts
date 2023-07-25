61578dc866145ae47fb491415ba08d7810292b00
Daniel Sinclair <d@niel.nyc>
2023-07-25 17:20:04 -0400

    fix: remove extra bsc merge conflict

--- src/core/utils/chains.ts
+++ src/core/utils/chains.ts
@@ -2,7 +2,7 @@ import { arbitrum, bsc, mainnet, optimism, polygon } from '@wagmi/chains';
 import type { Address, Chain } from 'wagmi';
 
 import { ETH_ADDRESS, NATIVE_ASSETS_PER_CHAIN } from '~/core/references';
-import { ChainId, ChainName, ChainNameDisplay, bsc } from '~/core/types/chains';
+import { ChainId, ChainName, ChainNameDisplay } from '~/core/types/chains';
 
 import { isLowerCaseMatch } from './strings';
 
