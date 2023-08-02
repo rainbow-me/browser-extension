80e33a3a0069be477563ec804de4a686aec6bf65
Bruno Barbieri <brunobar79@gmail.com>
2023-08-02 16:18:53 -0400

    undo stuff

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    threads: true,
-    maxThreads: 4,
-    minThreads: 4,
-    useAtomics: true,
     include: ['./**/**/*.test.ts'],
-    testTimeout: 60_000,
+    testTimeout: 90_000,
     watch: false,
     retry: 2,
     bail: 1,
