29b64a5b5a17f78d955420b1b886c3f6e6595d27
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2023-08-02 13:03:41 -0500

    new parallel config

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -3,16 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    threads: true,
     maxThreads: 4,
     minThreads: 4,
+    useAtomics: true,
     include: ['./**/**/*.test.ts'],
     testTimeout: 60_000,
     watch: false,
     retry: 2,
     bail: 1,
-    sequence: {
-      concurrent: true,
-    },
   },
 }) as UserConfig;
