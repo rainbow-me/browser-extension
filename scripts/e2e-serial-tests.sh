835f9d36b3b7f452d67b1b11375b4cd6585468c6
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2023-08-03 15:58:55 -0500

    .

--- scripts/e2e-serial-tests.sh
+++ scripts/e2e-serial-tests.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
+ANVIL_PORT=8545
 
 # Launch anvil in the bg
-yarn anvil --chain-id 1337 &
-# Give it some time to boot (CI is slower)
-if [ "$CI" = "true" ]; then
-  sleep 15
-else
-  sleep 5
-fi
-# Run the tests and store the result
-yarn vitest e2e/serial/$1 --config ./e2e/serial/vitest.config.ts
-TEST_RESULT=$?
-# kill anvil
-yarn anvil:kill
-# return the result of the tests
-exit "$TEST_RESULT"
+pkill -f anvil
+yarn anvil --chain-id 1337 > anvil-e2e.log 2>&1 &
+echo "Launching Anvil..."
 
+# Give it some time to boot
+interval=5
+until nc -z localhost $ANVIL_PORT; do
+  sleep $interval
+  interval=$((interval * 2))
+done
+
+# Check if Anvil has launched successfully
+if ! nc -z localhost $ANVIL_PORT; then
+  echo "Anvil failed to launch within the timeout."
+  exit 1
+fi
+echo "Anvil Launched..."
+
+# Run the tests and store the result
+echo "Running Tests..."
+yarn vitest e2e/serial/$1 --config ./e2e/serial/vitest.config.ts --reporter=verbose 
+
+# Store exit code
+TEST_EXIT_CODE=$?
+
+# kill anvil
+echo "Cleaning Up..."
+kill %1 || true
+
+# return the result of the tests
+exit "$TEST_EXIT_CODE"
--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+ANVIL_PORT=8545
 
 # Launch anvil in the bg
-yarn anvil &
-# Give it some time to boot (CI is slower)
-if [ "$CI" = "true" ]; then
-  sleep 15
-else
-  sleep 5
-fi
-# Run the tests and store the result
-vitest
-TEST_RESULT=$?
-# kill anvil
-yarn anvil:kill
-# return the result of the tests
-exit "$TEST_RESULT"
+pkill -f anvil
+yarn anvil > anvil-unit.log 2>&1 &
+echo "Launching Anvil..."
 
+# Give it some time to boot
+interval=5
+until nc -z localhost $ANVIL_PORT; do
+  sleep $interval
+  interval=$((interval * 2))
+done
+
+# Check if Anvil has launched successfully
+if ! nc -z localhost $ANVIL_PORT; then
+  echo "Anvil failed to launch within the timeout."
+  exit 1
+fi
+echo "Anvil Launched..."
+
+# Run the tests and store the result
+echo "Running Tests..."
+vitest
+TEST_EXIT_CODE=$?
+
+# kill anvil
+echo "Cleaning Up..."
+kill %1 || true
+
+# return the result of the tests
+exit "$TEST_EXIT_CODE"
