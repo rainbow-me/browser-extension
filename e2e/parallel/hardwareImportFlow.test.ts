2042d19ab212803c83085dc87432d72c924ed9aa
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2023-08-08 11:57:55 -0500

    testing testing!

---
+++ e2e/parallel/hardwareImportFlow.test.ts
@@ -0,0 +1,38 @@
+import 'chromedriver';
+import 'geckodriver';
+import { WebDriver } from 'selenium-webdriver';
+import { afterAll, beforeAll, describe, it } from 'vitest';
+
+import {
+  findElementByTestIdAndClick,
+  getExtensionIdByName,
+  goToWelcome,
+  initDriverWithOptions,
+} from '../helpers';
+
+let rootURL = 'chrome-extension://';
+let driver: WebDriver;
+
+const browser = process.env.BROWSER || 'chrome';
+const os = process.env.OS || 'mac';
+
+describe('Import wallet with a secret phrase flow', () => {
+  beforeAll(async () => {
+    driver = await initDriverWithOptions({
+      browser,
+      os,
+    });
+    const extensionId = await getExtensionIdByName(driver, 'Rainbow');
+    if (!extensionId) throw new Error('Extension not found');
+    rootURL += extensionId;
+  });
+  afterAll(async () => driver.quit());
+
+  it('should be able import a wallet via seed', async () => {
+    await goToWelcome(driver, rootURL);
+    await findElementByTestIdAndClick({
+      id: 'import-wallet-button',
+      driver,
+    });
+  });
+});
