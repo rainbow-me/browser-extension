f6a8790bfb8326d9f2c1e9dac19073cae5e6caf3
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2023-08-10 15:46:27 -0400

    More Firefox stuff (#846)

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -16,7 +16,8 @@ const BINARY_PATHS = {
   mac: {
     chrome: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     brave: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
-    firefox: '/Applications/Firefox.app/Contents/MacOS/Firefox',
+    firefox:
+      '/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox',
   },
   linux: {
     chrome: process.env.CHROMIUM_BIN,
@@ -90,6 +91,8 @@ export async function getWindowHandle({ driver }) {
 // setup functions
 
 export async function initDriverWithOptions(opts) {
+  let driver;
+
   const args = [
     'load-extension=build/',
     // '--auto-open-devtools-for-tabs',
@@ -101,12 +104,13 @@ export async function initDriverWithOptions(opts) {
     const options = new firefox.Options()
       .setBinary(BINARY_PATHS[opts.os][opts.browser])
       .addArguments(...args.slice(1))
+      .setPreference('xpinstall.signatures.required', false)
+      .setPreference('extensions.langpacks.signatures.required', false)
       .addExtensions('rainbowbx.xpi');
-    options.setAcceptInsecureCerts(true);
 
     const service = new firefox.ServiceBuilder().setStdio('inherit');
 
-    return await new Builder()
+    driver = await new Builder()
       .setFirefoxService(service)
       .forBrowser('firefox')
       .setFirefoxOptions(options)
@@ -119,29 +123,43 @@ export async function initDriverWithOptions(opts) {
 
     const service = new chrome.ServiceBuilder().setStdio('inherit');
 
-    return await new Builder()
+    driver = await new Builder()
       .setChromeService(service)
       .forBrowser('chrome')
       .setChromeOptions(options)
       .build();
   }
+
+  driver.browser = opts.browser;
+  return driver;
 }
 
 export async function getExtensionIdByName(driver, extensionName) {
-  await driver.get('chrome://extensions');
-  return await driver.executeScript(`
-      const extensions = document.querySelector("extensions-manager").shadowRoot
-        .querySelector("extensions-item-list").shadowRoot
-        .querySelectorAll("extensions-item")
-      for (let i = 0; i < extensions.length; i++) {
-        const extension = extensions[i].shadowRoot
-        const name = extension.querySelector('#name').textContent
-        if (name.startsWith("${extensionName}")) {
-          return extensions[i].getAttribute("id")
+  if (driver?.browser === 'firefox') {
+    await driver.get('about:debugging#addons');
+    const text = await driver
+      .wait(
+        until.elementLocated(By.xpath("//dl/div[contains(., 'UUID')]/dd")),
+        1000,
+      )
+      .getText();
+    return text;
+  } else {
+    await driver.get('chrome://extensions');
+    return await driver.executeScript(`
+        const extensions = document.querySelector("extensions-manager").shadowRoot
+          .querySelector("extensions-item-list").shadowRoot
+          .querySelectorAll("extensions-item")
+        for (let i = 0; i < extensions.length; i++) {
+          const extension = extensions[i].shadowRoot
+          const name = extension.querySelector('#name').textContent
+          if (name.startsWith("${extensionName}")) {
+            return extensions[i].getAttribute("id")
+          }
         }
-      }
-      return undefined
-    `);
+        return undefined
+      `);
+  }
 }
 
 // search functions
