5ae870f39356875c81a1f65ae6ad93c1a2da7ca5
Daniel Sinclair <d@niel.nyc>
2023-08-16 17:54:20 -0400

    create wallet in metamask

--- e2e/serial/dapp/3_metamaskActiveDappFlow.test.ts
+++ e2e/serial/dapp/3_metamaskActiveDappFlow.test.ts
@@ -4,11 +4,13 @@ import { WebDriver } from 'selenium-webdriver';
 import { afterAll, beforeAll, describe, it } from 'vitest';
 
 import {
-  delayTime,
+  findElementByTestIdAndClick,
+  findElementByTextAndClick,
   getExtensionIdByName,
   getRootUrl,
   importWalletFlow,
   initDriverWithOptions,
+  typeOnTextInput,
   untilDocumentLoaded,
 } from '../../helpers';
 import { TEST_VARIABLES } from '../../walletVariables';
@@ -45,9 +47,52 @@ describe('MetaMask active dApp flow', () => {
     );
   });
 
-  it('should be able open metamask', async () => {
-    await driver.get(metamaskUrl + '/popup.html');
-    await driver.wait(untilDocumentLoaded(), 20_000);
-    await delayTime('very-long');
+  it('should be able open metamask and import a wallet via private key', async () => {
+    const [, metamaskHandler] = await driver.getAllWindowHandles();
+    await driver.switchTo().window(metamaskHandler);
+    await driver.get(metamaskUrl + '/home.html#onboarding/welcome');
+    await driver.wait(untilDocumentLoaded(), 5_000);
+
+    // Welcome screen
+    await findElementByTestIdAndClick({
+      id: 'onboarding-terms-checkbox',
+      driver,
+    });
+    await findElementByTextAndClick(driver, 'Create a new wallet');
+
+    // Analytics screen
+    await findElementByTextAndClick(driver, 'No thanks');
+
+    // Create password
+    await typeOnTextInput({
+      id: 'create-password-new',
+      driver,
+      text: 'test1234',
+    });
+    await typeOnTextInput({
+      id: 'create-password-confirm',
+      driver,
+      text: 'test1234',
+    });
+    await findElementByTestIdAndClick({ driver, id: 'create-password-terms' });
+    await findElementByTextAndClick(driver, 'Create a new wallet');
+
+    // Secure your wallet
+    await findElementByTextAndClick(
+      driver,
+      'Remind me later (not recommended)',
+    );
+
+    // Alert
+    await findElementByTextAndClick(
+      driver,
+      'I understand that until I back up my Secret Recovery Phrase, I may lose my accounts and all of their assets.',
+    );
+    await findElementByTestIdAndClick({ driver, id: 'skip-srp-backup' });
+
+    // Wallet creation successful
+    await findElementByTextAndClick(driver, 'Got it!');
+    await findElementByTextAndClick(driver, 'Next');
+    await findElementByTextAndClick(driver, 'Done');
   });
 });
