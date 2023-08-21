2f1b872b73d953ddb6cbac86f9988a882574c38a
Esteban Miño <efmino@uc.cl>
2023-08-21 17:11:24 -0400

    rm AppConnectionWalletItemConnectedWrapper

--- src/entries/popup/components/AppConnection/AppConnectionWalletItem/AppConnectionWalletItem.tsx
+++ src/entries/popup/components/AppConnection/AppConnectionWalletItem/AppConnectionWalletItem.tsx
@@ -1,11 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import React, {
-  ReactElement,
-  useCallback,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import React, { useMemo, useState } from 'react';
 import { Address } from 'wagmi';
 
 import appConnectionWalletItemImageMask from 'static/assets/appConnectionWalletItemImageMask.svg';
@@ -16,7 +10,6 @@ import { useCurrentCurrencyStore } from '~/core/state';
 import { useConnectedToHardhatStore } from '~/core/state/currentSettings/connectedToHardhat';
 import { ChainId, ChainNameDisplay } from '~/core/types/chains';
 import { convertAmountToNativeDisplay } from '~/core/utils/numbers';
-import { goToNewTab } from '~/core/utils/tabs';
 import {
   Box,
   Column,
@@ -31,169 +24,11 @@ import {
 import { Lens } from '~/design-system/components/Lens/Lens';
 
 import { AppMetadata } from '../../../hooks/useAppMetadata';
-import { useAppSession } from '../../../hooks/useAppSession';
 import { useWalletName } from '../../../hooks/useWalletName';
-import { AppInteractionItem } from '../../AppConnectionMenu/AppInteractionItem';
 import { ChainBadge } from '../../ChainBadge/ChainBadge';
-import {
-  ContextMenu,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
-  ContextMenuTrigger,
-} from '../../ContextMenu/ContextMenu';
-import { ContextMenuContentWithSubMenu } from '../../ContextMenu/ContextSubMenu';
-import { DropdownMenuRadioGroup } from '../../DropdownMenu/DropdownMenu';
-import { DropdownSubMenu } from '../../DropdownMenu/DropdownSubMenu';
-import { SwitchNetworkMenuSelector } from '../../SwitchMenu/SwitchNetworkMenu';
 import { WalletAvatar } from '../../WalletAvatar/WalletAvatar';
 
 import { appConnectionWalletItem } from './AppConnectionWalletItem.css';
-
-interface WalletItemConnectedWrapperProps {
-  children: ReactElement;
-  appMetadata: AppMetadata;
-  address: Address;
-  onClose?: () => null;
-  onOpen?: () => null;
-}
-
-export const AppConnectionWalletItemConnectedWrapper = React.forwardRef(
-  (
-    props: WalletItemConnectedWrapperProps,
-    ref: React.ForwardedRef<HTMLDivElement>,
-  ) => {
-    const { children, appMetadata, address } = props;
-    const dropdownMenuRef = useRef<HTMLDivElement | null>(null);
-    const [subMenuOpen, setSubMenuOpen] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const { updateSessionChainId, disconnectSession, appSession } =
-      useAppSession({ host: appMetadata.appHost });
-
-    const changeChainId = useCallback(
-      (chainId: string) => {
-        updateSessionChainId({ address, chainId: Number(chainId) });
-      },
-      [address, updateSessionChainId],
-    );
-
-    const disconnect = useCallback(() => {
-      disconnectSession({ address, host: appMetadata.appHost });
-      setSubMenuOpen(false);
-      setMenuOpen(false);
-    }, [address, appMetadata.appHost, disconnectSession]);
-
-    const onValueChange = useCallback(
-      (value: 'disconnect' | 'switch-networks' | 'open-dapp') => {
-        switch (value) {
-          case 'disconnect':
-            disconnect();
-            break;
-          case 'switch-networks':
-            setSubMenuOpen(!subMenuOpen);
-            break;
-          case 'open-dapp':
-            goToNewTab({
-              url: appMetadata.url,
-              active: false,
-            });
-            break;
-        }
-      },
-      [appMetadata.url, disconnect, subMenuOpen],
-    );
-
-    return (
-      <ContextMenu onOpenChange={setMenuOpen} open={menuOpen}>
-        <ContextMenuTrigger asChild>
-          <Box ref={ref}>{children}</Box>
-        </ContextMenuTrigger>
-
-        <ContextMenuContentWithSubMenu subMenuRef={dropdownMenuRef}>
-          <ContextMenuRadioGroup
-            onValueChange={(value) =>
-              onValueChange(
-                value as 'disconnect' | 'switch-networks' | 'open-dapp',
-              )
-            }
-          >
-            <Box key="switch-networks">
-              <DropdownSubMenu
-                menuOpen={menuOpen}
-                parentRef={dropdownMenuRef}
-                setMenuOpen={setMenuOpen}
-                subMenuOpen={subMenuOpen}
-                setSubMenuOpen={setSubMenuOpen}
-                subMenuContent={
-                  <DropdownMenuRadioGroup
-                    value={`${appSession.sessions[address]}`}
-                    onValueChange={changeChainId}
-                  >
-                    <SwitchNetworkMenuSelector
-                      type="dropdown"
-                      highlightAccentColor
-                      selectedValue={`${appSession.sessions[address]}`}
-                      onNetworkSelect={(e) => {
-                        e?.preventDefault();
-                        setSubMenuOpen(false);
-                        // without this timeout the collapse of the context menu freezes the screen
-                        setTimeout(() => {
-                          setMenuOpen(false);
-                        }, 1);
-                      }}
-                      onShortcutPress={changeChainId}
-                      showDisconnect={false}
-                      disconnect={disconnect}
-                    />
-                  </DropdownMenuRadioGroup>
-                }
-                subMenuElement={
-                  <AppInteractionItem
-                    type="context"
-                    appSession={appSession}
-                    chevronDirection={subMenuOpen ? 'down' : 'right'}
-                    showChevron
-                  />
-                }
-              />
-            </Box>
-            <Box key="disconnect">
-              <ContextMenuRadioItem value="disconnect">
-                <Inline
-                  height="full"
-                  alignHorizontal="center"
-                  alignVertical="center"
-                  space="8px"
-                >
-                  <Box height="fit" style={{ width: '18px', height: '18px' }}>
-                    <Inline
-                      height="full"
-                      alignHorizontal="center"
-                      alignVertical="center"
-                    >
-                      <Symbol
-                        size={12}
-                        symbol="xmark"
-                        weight="semibold"
-                        color="label"
-                      />
-                    </Inline>
-                  </Box>
-                  <Text size="14pt" weight="semibold" color="label">
-                    {i18n.t('app_connection_switcher.wallet_item.disconnect')}
-                  </Text>
-                </Inline>
-              </ContextMenuRadioItem>
-            </Box>
-          </ContextMenuRadioGroup>
-        </ContextMenuContentWithSubMenu>
-      </ContextMenu>
-    );
-  },
-);
-
-AppConnectionWalletItemConnectedWrapper.displayName =
-  'AppConnectionWalletItemConnectedWrapper';
 
 interface WalletItemProps {
   address: Address;
