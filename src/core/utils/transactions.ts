d5727b3c35b63d751a8ec1dfc3ce041dcbe49bcd
Esteban Miño <efmino@uc.cl>
2023-08-28 15:35:11 -0400

    fix: nonces state logic (#891)

--- src/core/utils/transactions.ts
+++ src/core/utils/transactions.ts
@@ -587,7 +587,6 @@ export async function getNextNonce({
   const localNonceData = getNonce({ address, chainId });
   const localNonce = localNonceData?.currentNonce || 0;
   const provider = getProvider({ chainId });
-
   const txCountIncludingPending = await provider.getTransactionCount(
     address,
     'pending',
