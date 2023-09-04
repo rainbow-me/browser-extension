b25a3c2a61c5f01f87fe38fcbe441a38708fe37d
Esteban Miño <efmino@uc.cl>
2023-09-04 11:17:52 -0300

    update refetch time per chainid

--- src/core/resources/gas/gasData.ts
+++ src/core/resources/gas/gasData.ts
@@ -4,13 +4,29 @@ import { meteorologySupportsChain } from '~/core/utils/gas';
 import { useMeteorology } from './meteorology';
 import { useProviderGas } from './providerGas';
 
+const getRefetchTime = (chainId: ChainId) => {
+  switch (chainId) {
+    case ChainId.arbitrum:
+    case ChainId.mainnet:
+    case ChainId.hardhat:
+      return 5000;
+    case ChainId.base:
+    case ChainId.bsc:
+    case ChainId.optimism:
+    case ChainId.polygon:
+    case ChainId.zora:
+    default:
+      return 2000;
+  }
+};
+
 export const useGasData = ({ chainId }: { chainId: ChainId }) => {
   const meteorologySupportsChainId = meteorologySupportsChain(chainId);
   const { data: meteorologyData, isLoading: meteorologyDataIsLoading } =
     useMeteorology(
       { chainId },
       {
-        refetchInterval: 5000,
+        refetchInterval: getRefetchTime(chainId),
         enabled: meteorologySupportsChainId,
       },
     );
@@ -20,7 +36,7 @@ export const useGasData = ({ chainId }: { chainId: ChainId }) => {
       { chainId },
       {
         enabled: !meteorologySupportsChainId,
-        refetchInterval: 5000,
+        refetchInterval: getRefetchTime(chainId),
       },
     );
 
