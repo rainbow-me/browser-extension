414129dd53f9e4fb7e450260a6caeb85be4200ac
Esteban Miño <efmino@uc.cl>
2023-09-07 11:22:03 -0300

    Revert "permit on hardhat"
    This reverts commit 413a287271b803760b611ef6b3d794c00849c28e.

--- src/core/raps/unlockAndSwap.ts
+++ src/core/raps/unlockAndSwap.ts
@@ -128,7 +128,7 @@ export const createUnlockAndSwapRap = async (
 
   const allowsPermit =
     !nativeAsset &&
-    (chainId === ChainId.mainnet || chainId === 1337) &&
+    chainId === ChainId.mainnet &&
     ALLOWS_PERMIT[
       assetToSell.address?.toLowerCase() as keyof PermitSupportedTokenList
     ];
