e757910cfdf07b9ac43625915967a254991c754c
Esteban Miño <efmino@uc.cl>
2023-09-11 16:00:26 -0300

    rm getnextinqueue

--- src/core/state/homePromptsQueue/index.ts
+++ src/core/state/homePromptsQueue/index.ts
@@ -5,17 +5,12 @@ import { createStore } from '~/core/state/internal/createStore';
 type HomePrompt = 'app-connection' | 'wallet-backup';
 export interface HomePromptsQueue {
   queue: HomePrompt[];
-  getNextInQueue: () => HomePrompt | undefined;
   popQueue: () => void;
 }
 
 export const homePromptsQueueStore = createStore<HomePromptsQueue>(
   (set, get) => ({
     queue: ['wallet-backup', 'app-connection'],
-    getNextInQueue: () => {
-      const { queue } = get();
-      return queue[0];
-    },
     popQueue: () => {
       const { queue } = get();
       queue.splice(0, 1);
