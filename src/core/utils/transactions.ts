9163de8c7629f85d74ac2442a68ca31341679b40
gregs <ukfaeku@gmail.com>
2023-09-14 19:01:53 -0300

    fix activity details display of new txs

--- src/core/utils/transactions.ts
+++ src/core/utils/transactions.ts
@@ -243,7 +243,7 @@ export const parseNewTransaction = (
     }),
   }));
 
-  const asset = changes?.[0]?.asset;
+  const asset = changes?.[0]?.asset || tx.asset;
   const methodName = 'Unknown method';
 
   return {
@@ -261,6 +261,7 @@ export const parseNewTransaction = (
     to: tx.to,
     type: tx.type,
     flashbots: tx.flashbots,
+    feeType: 'maxFeePerGas' in tx ? 'eip-1559' : 'legacy',
     gasPrice: tx.gasPrice,
     maxFeePerGas: tx.maxFeePerGas,
     maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
