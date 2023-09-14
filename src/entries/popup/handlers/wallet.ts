7405c9e7c064f2bd2d565436f407855dfa494703
Bruno Barbieri <brunobar79@gmail.com>
2023-09-14 17:20:57 -0400

    remove console log

--- src/entries/popup/handlers/wallet.ts
+++ src/entries/popup/handlers/wallet.ts
@@ -74,8 +74,6 @@ export const signTransactionFromHW = async (
     chainId: transactionRequest.chainId as number,
   });
 
-  console.log('selectedGas', JSON.stringify(selectedGas, null, 2));
-
   const params = {
     ...transactionRequest,
     ...selectedGas.transactionGasParams,
