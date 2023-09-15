ffd885ba3a14f343048ceb568e2d7329a48fd894
derHowie <derhowiedesigns@gmail.com>
2023-09-14 23:51:13 -0500

    use fullscreen conditional for account name tooltips

--- src/entries/popup/pages/home/index.tsx
+++ src/entries/popup/pages/home/index.tsx
@@ -43,6 +43,7 @@ import { useScroll } from '../../hooks/useScroll';
 import { useSwitchWalletShortcuts } from '../../hooks/useSwitchWalletShortcuts';
 import { StickyHeader } from '../../layouts/StickyHeader';
 import { ROUTES } from '../../urls';
+import { isFullScreen } from '../../utils/windows';
 
 import { Activities } from './Activity/ActivitiesList';
 import { Header } from './Header';
@@ -317,8 +318,8 @@ const TopNav = memo(function TopNav() {
                     textSize="12pt"
                     textColor="labelSecondary"
                     arrowDirection={'up'}
-                    marginLeft="-620px"
-                    marginTop="-48px"
+                    marginLeft={isFullScreen ? '-620px' : '-20px'}
+                    marginTop={isFullScreen ? '-48px' : '40px'}
                     hint={shortcuts.home.GO_TO_WALLETS.display}
                   >
                     {content}
