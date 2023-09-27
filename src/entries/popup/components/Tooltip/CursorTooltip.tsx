983352a86db8d65383b094aea4aa2442a90c0c45
derHowie <derhowiedesigns@gmail.com>
2023-09-27 12:39:07 -0500

    combine tooltip and cursor tooltip

--- src/entries/popup/components/Tooltip/CursorTooltip.tsx
+++ src/entries/popup/components/Tooltip/CursorTooltip.tsx
@@ -1,3 +1,4 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import React, {
   ReactNode,
   useEffect,
@@ -7,17 +8,23 @@ import React, {
   useState,
 } from 'react';
 
-import { Box } from '~/design-system';
+import { useCurrentThemeStore } from '~/core/state/currentSettings/currentTheme';
+import { Box, TextOverflow, ThemeProvider } from '~/design-system';
+import {
+  AlignHorizontal,
+  Inline,
+} from '~/design-system/components/Inline/Inline';
 import { TextStyles } from '~/design-system/styles/core.css';
 
 import useComponentWillUnmount from '../../hooks/useComponentWillUnmount';
+import { ShortcutHint } from '../ShortcutHint/ShortcutHint';
 
-import { Tooltip } from './Tooltip';
+const { innerWidth: windowWidth } = window;
 
 export const CursorTooltip = ({
   align,
-  arrowAlignment,
-  arrowDirection,
+  arrowAlignment = 'center',
+  arrowDirection = 'down',
   arrowCentered,
   text,
   textWeight,
@@ -70,38 +77,115 @@ export const CursorTooltip = ({
     checkChildWidth();
   }, []);
 
+  const { alignHorizontal, left, right } = useMemo(() => {
+    switch (arrowAlignment) {
+      case 'left':
+        return {
+          alignHorizontal: 'left' as AlignHorizontal,
+          left: 6,
+          right: undefined,
+        };
+      case 'center':
+        return {
+          alignHorizontal: 'center' as AlignHorizontal,
+          left: undefined,
+          right: undefined,
+        };
+      case 'right':
+        return {
+          alignHorizontal: 'right' as AlignHorizontal,
+          left: undefined,
+          right: 6,
+        };
+    }
+  }, [arrowAlignment]);
+
+  const { currentTheme } = useCurrentThemeStore();
+
   if (process.env.IS_TESTING === 'true') {
     return <Box>{children}</Box>;
   }
 
   return (
-    <>
-      <Tooltip
-        align={align}
-        alignOffset={alignOffset}
-        arrowAlignment={arrowAlignment}
-        arrowDirection={arrowDirection}
-        text={text}
-        textWeight={textWeight}
-        textSize={textSize}
-        textColor={textColor}
-        open={open}
-        hint={hint}
-      >
-        <Box
-          ref={childElementWrapperRef}
-          onMouseEnter={() => {
-            setIsHovering(true);
-            showTimer.current = setTimeout(() => setOpen(true), 750);
-          }}
-          onMouseLeave={() => {
-            setIsHovering(false);
-            setOpen(false);
-          }}
-        >
-          {children}
-        </Box>
-      </Tooltip>
-    </>
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root open={open}>
+        <TooltipPrimitive.Trigger asChild>
+          <Box
+            ref={childElementWrapperRef}
+            onMouseEnter={() => {
+              setIsHovering(true);
+              showTimer.current = setTimeout(() => setOpen(true), 750);
+            }}
+            onMouseLeave={() => {
+              setIsHovering(false);
+              setOpen(false);
+              showTimer.current && clearTimeout(showTimer.current);
+            }}
+          >
+            {children}
+          </Box>
+        </TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Portal>
+          <TooltipPrimitive.Content
+            align={align}
+            className="TooltipContent"
+            side={arrowDirection === 'up' ? 'bottom' : 'top'}
+            sideOffset={10}
+            alignOffset={alignOffset}
+          >
+            <ThemeProvider theme={currentTheme}>
+              <Box
+                style={{ maxWidth: windowWidth - 60 }}
+                borderRadius="6px"
+                boxShadow="24px"
+              >
+                <Inline alignHorizontal={alignHorizontal}>
+                  <Box
+                    background="surfaceSecondaryElevated"
+                    backdropFilter="blur(26px)"
+                    position="absolute"
+                    borderRadius="2px"
+                    marginBottom="-3px"
+                    bottom="0"
+                    style={{
+                      height: 10,
+                      width: 10,
+                      rotate: '45deg',
+                      left,
+                      right,
+                      ...(arrowDirection === 'up' ? { top: -4 } : {}),
+                    }}
+                  />
+                </Inline>
+                <Box
+                  style={{
+                    alignItems: 'center',
+                    borderRadius: 6,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    minHeight: 22,
+                  }}
+                  background="surfaceSecondaryElevated"
+                  padding="7px"
+                  paddingHorizontal="6px"
+                  backdropFilter="blur(26px)"
+                >
+                  <Inline alignVertical="center" space={'6px'}>
+                    <TextOverflow
+                      color={textColor || 'label'}
+                      size={textSize || '16pt'}
+                      weight={textWeight || 'bold'}
+                    >
+                      {text}
+                    </TextOverflow>
+                    {hint ? <ShortcutHint hint={hint} small /> : null}
+                  </Inline>
+                </Box>
+              </Box>
+            </ThemeProvider>
+          </TooltipPrimitive.Content>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
   );
 };
