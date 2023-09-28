894378243d5a6450fb635da8317e8ba41e9c308e
Daniel Sinclair <d@niel.nyc>
2023-09-28 13:08:06 -0400

    fix: capitalization

--- src/core/languages/index.ts
+++ src/core/languages/index.ts
@@ -30,7 +30,7 @@ export const supportedLanguages = {
     label: '日本語',
   },
   [Language.PT_BR]: {
-    label: 'Português Brasileiro',
+    label: 'português brasileiro',
   },
   [Language.ZH_CN]: {
     label: '中文',
