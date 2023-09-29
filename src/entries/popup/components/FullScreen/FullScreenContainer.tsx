f0841ebf53404812c806ee2aad718bad35ece120
brdy <41711440+BrodyHughes@users.noreply.github.com>
2023-09-29 13:18:59 -0500

    Fix background coloring in the `/import/select` sheet of onboarding (#1006)
    Co-authored-by: Jin <jin.chung17@gmail.com>

--- src/entries/popup/components/FullScreen/FullScreenContainer.tsx
+++ src/entries/popup/components/FullScreen/FullScreenContainer.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import { POPUP_DIMENSIONS } from '~/core/utils/dimensions';
 import { Box } from '~/design-system';
+import { BackgroundColor } from '~/design-system/styles/designTokens';
 
 import { NAVBAR_HEIGHT } from '../Navbar/Navbar';
 
 export function FullScreenContainer({
   children,
+  background,
 }: {
   children: React.ReactNode;
+  background?: BackgroundColor;
 }) {
   return (
     <Box
@@ -16,6 +19,7 @@ export function FullScreenContainer({
       flexDirection="column"
       alignItems="center"
       paddingHorizontal="20px"
+      background={background}
       style={{
         minHeight: POPUP_DIMENSIONS.height - NAVBAR_HEIGHT,
         height: '100%',
