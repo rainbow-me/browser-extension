4eaff46219743d254c41294421c8b29bde4518d3
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2023-10-05 16:18:44 -0500

    simplify

--- src/entries/popup/pages/messages/BottomActions/index.tsx
+++ src/entries/popup/pages/messages/BottomActions/index.tsx
@@ -239,7 +239,7 @@ export const BottomNetwork = ({
             gap: 4,
             padding: 2,
           }}
-          bubbles={true}
+          bubbles
           tabIndex={displaySymbol ? 0 : -1}
         >
           <ChainBadge chainId={selectedChainId} size="18" />
