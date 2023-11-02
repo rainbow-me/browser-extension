a11fd97c4898e51f11eef303dc12e6d5f6847ded
Bruno Barbieri <brunobar79@gmail.com>
2023-11-02 13:53:19 -0400

    fix activity list

--- src/core/resources/assets/userAssets.ts
+++ src/core/resources/assets/userAssets.ts
@@ -29,6 +29,7 @@ import {
   getSupportedChainIds,
   getSupportedTestnetChainIds,
 } from '~/core/utils/chains';
+import { isCustomNetwork } from '~/core/utils/customNetworks';
 import { greaterThan } from '~/core/utils/numbers';
 import { RainbowError, logger } from '~/logger';
 import {
@@ -139,7 +140,7 @@ async function userAssetsQueryFunction({
   try {
     const supportedChainIds = testnetMode
       ? getSupportedTestnetChainIds()
-      : getSupportedChainIds();
+      : getSupportedChainIds().filter((chainId) => !isCustomNetwork(chainId));
     const url = `/${supportedChainIds.join(',')}/${address}/assets`;
     const res = await addysHttp.get<AddressAssetsReceivedMessage>(url, {
       params: {
