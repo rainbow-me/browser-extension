df3d241db5d42137eb789d0a9a4e4f4732ed98fc
Esteban Miño <efmino@uc.cl>
2023-11-15 12:57:35 -0300

    dappHandler

--- e2e/parallel/networksAndTestnetModeFlow.test.ts
+++ e2e/parallel/networksAndTestnetModeFlow.test.ts
@@ -94,7 +94,7 @@ describe('Networks & Testnet Mode flows', () => {
       dappHandler,
     });
 
-    console.log('-- 12 popupHandler', popupHandler);
+    console.log('-- 12 dappHandler, popupHandler', dappHandler, popupHandler);
     await driver.switchTo().window(popupHandler);
 
     console.log('-- 13');
