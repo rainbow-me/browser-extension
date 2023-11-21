ce7c59e2d7bb103ebb5ad483da5a6d92a403a98d
Esteban Miño <efmino@uc.cl>
2023-11-21 13:24:22 -0300

    feat: custom rpc form validations (#1144)

---
+++ src/core/resources/chains/chainMetadata.ts
@@ -0,0 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
+
+import {
+  QueryConfig,
+  QueryFunctionArgs,
+  QueryFunctionResult,
+  createQueryKey,
+} from '~/core/react-query';
+import { getChainMetadataRPCUrl } from '~/core/utils/chains';
+import { isValidUrl } from '~/core/utils/connectedApps';
+
+// ///////////////////////////////////////////////
+// Query Types
+
+type ChainMetadataArgs = {
+  rpcUrl?: string;
+};
+
+// ///////////////////////////////////////////////
+// Query Key
+
+export const chainMetadataQueryKey = ({ rpcUrl }: ChainMetadataArgs) =>
+  createQueryKey('chainMetadata', { rpcUrl }, { persisterVersion: 1 });
+
+type AssetMetadataQueryKey = ReturnType<typeof chainMetadataQueryKey>;
+
+// ///////////////////////////////////////////////
+// Query Function
+
+async function chainMetadataQueryFunction({
+  queryKey: [{ rpcUrl }],
+}: QueryFunctionArgs<typeof chainMetadataQueryKey>) {
+  if (rpcUrl && isValidUrl(rpcUrl)) {
+    const metadata = await getChainMetadataRPCUrl({
+      rpcUrl,
+    });
+    return {
+      chainId: metadata?.chainId,
+    };
+  }
+  return null;
+}
+
+type AssetMetadataResult = QueryFunctionResult<
+  typeof chainMetadataQueryFunction
+>;
+
+// ///////////////////////////////////////////////
+// Query Hook
+
+export function useChainMetadata(
+  { rpcUrl }: ChainMetadataArgs,
+  config: QueryConfig<
+    AssetMetadataResult,
+    Error,
+    AssetMetadataResult,
+    AssetMetadataQueryKey
+  > = {},
+) {
+  return useQuery(
+    chainMetadataQueryKey({
+      rpcUrl,
+    }),
+    chainMetadataQueryFunction,
+    {
+      ...config,
+    },
+  );
+}
