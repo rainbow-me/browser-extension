af59d6fc4cde940d2b5f145f07cb50875d19ee06
Esteban Miño <efmino@uc.cl>
2023-12-06 17:58:45 -0300

    comments

--- src/entries/popup/pages/home/Points/utils.ts
+++ src/entries/popup/pages/home/Points/utils.ts
@@ -1,11 +1,6 @@
 import { i18n } from '~/core/languages';
 
-import {
-  CATEGORY_DISPLAY_TYPE,
-  CATEGORY_TYPE,
-  EXISTING_USER_ERROR,
-  INVALID_REFERRAL_CODE_ERROR,
-} from './references';
+import { EXISTING_USER_ERROR, INVALID_REFERRAL_CODE_ERROR } from './references';
 
 export const CHARACTER_TYPING_SPEED = 0.1;
 
@@ -37,70 +32,4 @@ export const getDelayForRows = (
 ): number => {
   const delay = getDelayForRow(rows.flat(), row * 2 + column - 1);
   return delay;
-};
-
-export const DUMMY_USER = {
-  categories: [
-    {
-      data: {
-        owned_collections: 0,
-        total_collections: 0,
-        usd_amount: 1,
-      },
-      type: 'metamask-swaps' as CATEGORY_TYPE,
-      display_type: 'USD_AMOUNT' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 0 },
-    },
-    {
-      data: {
-        owned_collections: 0,
-        total_collections: 0,
-        usd_amount: 1,
-      },
-      type: 'rainbow-bridges' as CATEGORY_TYPE,
-      display_type: 'USD_AMOUNT' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 0 },
-    },
-    {
-      data: {
-        owned_collections: 1,
-        total_collections: 10,
-        usd_amount: 1,
-      },
-      type: 'nft-collections' as CATEGORY_TYPE,
-      display_type: 'NFT_COLLECTION' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 0 },
-    },
-    {
-      data: {
-        owned_collections: 0,
-        total_collections: 0,
-        usd_amount: 1,
-      },
-      type: 'historic-balance' as CATEGORY_TYPE,
-      display_type: 'USD_AMOUNT' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 0 },
-    },
-    {
-      data: {
-        owned_collections: 0,
-        total_collections: 0,
-        usd_amount: 1,
-      },
-      type: 'bonus' as CATEGORY_TYPE,
-      display_type: 'BONUS' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 1 },
-    },
-    {
-      data: {
-        owned_collections: 0,
-        total_collections: 0,
-        usd_amount: 1,
-      },
-      type: 'rainbow-swaps' as CATEGORY_TYPE,
-      display_type: 'USD_AMOUNT' as CATEGORY_DISPLAY_TYPE,
-      earnings: { total: 0 },
-    },
-  ],
-  earnings: { total: 100 },
 };
