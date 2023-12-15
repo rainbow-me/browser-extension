aa6ba0bd36f360ff66a763c53f70a25d3e159e8e
Bruno Barbieri <brunobar79@gmail.com>
2023-12-15 15:39:46 -0500

    more fixes

--- src/entries/popup/pages/settings/customChain/index.tsx
+++ src/entries/popup/pages/settings/customChain/index.tsx
@@ -274,9 +274,9 @@ export function SettingsCustomChain() {
   }>({
     testnet: chain?.testnet,
     chainId: chain?.id,
-    name: chain?.name,
     symbol: chain?.nativeCurrency.symbol,
     explorerUrl: chain?.blockExplorers?.default.url,
+    active: !chain, // True only if adding a new network
   });
   const [validations, setValidations] = useState<{
     rpcUrl: boolean;
