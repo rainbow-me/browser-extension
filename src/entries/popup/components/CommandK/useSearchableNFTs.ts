8a19fb9805a0b4a08b4196491f8807ada4d5b796
derHowie <derhowiedesigns@gmail.com>
2023-12-19 16:59:16 -0500

    add testnet flag to searchable nfts hook

--- src/entries/popup/components/CommandK/useSearchableNFTs.ts
+++ src/entries/popup/components/CommandK/useSearchableNFTs.ts
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import { selectNfts } from '~/core/resources/_selectors/nfts';
 import { useNfts } from '~/core/resources/nfts';
 import { useCurrentAddressStore } from '~/core/state';
+import { useTestnetModeStore } from '~/core/state/currentSettings/testnetMode';
 
 import { useRainbowNavigate } from '../../hooks/useRainbowNavigate';
 import { ROUTES } from '../../urls';
@@ -21,8 +22,9 @@ const parseNftName = (name: string, id: string) => {
 export const useSearchableNFTs = () => {
   const { currentAddress: address } = useCurrentAddressStore();
   const navigate = useRainbowNavigate();
+  const { testnetMode } = useTestnetModeStore();
 
-  const { data } = useNfts({ address });
+  const { data } = useNfts({ address, testnetMode });
 
   const searchableNFTs = useMemo(() => {
     const nfts = selectNfts(data) || [];
