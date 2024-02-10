69242c700eda6903b575e420af2a9586ad3cefca
xisux <87016182+xisux@users.noreply.github.com>
2024-02-10 17:34:14 +0000

    fix: don't route the rpcUrl through proxy if on local ip range

--- src/core/providers/proxy.ts
+++ src/core/providers/proxy.ts
@@ -3,7 +3,10 @@ import { ChainId } from '../types/chains';
 export const proxyRpcEndpoint = (endpoint: string, chainId: ChainId) => {
   if (
     endpoint !== 'http://127.0.0.1:8545' &&
-    endpoint !== 'http://localhost:8545'
+    endpoint !== 'http://localhost:8545' &&
+    !endpoint.includes('http://10.') &&
+    !endpoint.includes('http://192.') &&
+    !endpoint.includes('http://172.')
   ) {
     return `${process.env.RPC_PROXY_BASE_URL}/${chainId}/${
       process.env.RPC_PROXY_API_KEY
