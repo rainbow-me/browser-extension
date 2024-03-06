3cdb0e22cfbfb7c693341a551661090c87806126
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-03-06 16:00:17 -0600

    .

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -108,6 +108,16 @@ export async function getWindowHandle({ driver }: { driver: WebDriver }) {
 
 // setup functions
 
+async function closeOtherHandles({ driver }: { driver: WebDriver }) {
+  const [firstHandle, ...handles] = await driver.getAllWindowHandles();
+  for (const handle of handles) {
+    await driver.switchTo().window(handle);
+    await driver.close();
+  }
+  await driver.switchTo().window(firstHandle);
+  await delayTime('short');
+}
+
 export async function initDriverWithOptions(opts: {
   browser: string;
   os: string;
@@ -136,6 +146,9 @@ export async function initDriverWithOptions(opts: {
       .forBrowser('firefox')
       .setFirefoxOptions(options)
       .build();
+    // fixes on-install behavior in firefox that opens onboarding in new tab
+    await delayTime('long');
+    await closeOtherHandles({ driver });
   } else {
     const options = new chrome.Options()
       // @ts-ignore
