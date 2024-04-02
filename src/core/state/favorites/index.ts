be59c8f5496c577dec21b2bfcec2836dc4ff7dd9
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-04-02 15:25:57 -0500

    changed === to <=

--- src/core/state/favorites/index.ts
+++ src/core/state/favorites/index.ts
@@ -102,10 +102,7 @@ const defaultFavorites = {
     USDC_AVALANCHE_ADDRESS,
     WBTC_AVALANCHE_ADDRESS,
   ],
-  [ChainId.blast]: [
-    ETH_BLAST_ADDRESS,
-    USDB_BLAST_ADDRESS,
-  ]
+  [ChainId.blast]: [ETH_BLAST_ADDRESS, USDB_BLAST_ADDRESS],
 } satisfies FavoritesState['favorites'];
 
 const mergeNewOfficiallySupportedChainsState = (
@@ -158,7 +155,7 @@ export const favoritesStore = createStore<FavoritesState>(
           return mergeNewOfficiallySupportedChainsState(state, [
             ChainId.avalanche,
           ]);
-        } else if (version === 2) {
+        } else if (version <= 2) {
           // version 3 added support for Blast
           return mergeNewOfficiallySupportedChainsState(state, [ChainId.blast]);
         }
