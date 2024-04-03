506793381b534088c572533cfafa71d5d5f00ee9
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-04-03 12:18:51 -0500

    .

--- scripts/e2e-serial-tests.sh
+++ scripts/e2e-serial-tests.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 ANVIL_PORT=8545
+MAX_RETRIES=2
+RETRY_COUNT=0
 
-# Launch anvil in the bg
-yarn anvil:kill
-yarn anvil --chain-id 1337 &
-echo "Launching Anvil..."
+# Function to launch and verify Anvil
+launch_anvil() {
+  # Launch anvil in the bg
+  yarn anvil:kill
+  yarn anvil --chain-id 1337 &
+  echo "Launching Anvil..."
 
-# Give it some time to boot
-interval=5
-until nc -z localhost $ANVIL_PORT; do
-  sleep $interval
-  interval=$((interval * 2))
+  # Give it some time to boot
+  interval=5
+  until nc -z localhost $ANVIL_PORT; do
+    sleep $interval
+    interval=$((interval * 2))
+  done
+  echo "Anvil Launched..."
+}
+
+# Function to run tests
+run_tests() {
+  echo "Running Tests..."
+  yarn vitest e2e/serial/$1 --config ./e2e/serial/vitest.config.ts --reporter=verbose --bail 1
+}
+
+# Main loop for retry logic
+TEST_RESULT=1
+while [ $RETRY_COUNT -lt $MAX_RETRIES ] && [ $TEST_RESULT -ne 0 ]; do
+  if [ $RETRY_COUNT -gt 0 ]; then
+    echo "Test failed, attempting retry $RETRY_COUNT..."
+  fi
+  
+  launch_anvil
+  run_tests $1
+  TEST_RESULT=$?
+  
+  # kill anvil
+  echo "Cleaning Up..."
+  yarn anvil:kill
+
+  RETRY_COUNT=$((RETRY_COUNT+1))
 done
-echo "Anvil Launched..."
 
-# Run the tests and store the result
-echo "Running Tests..."
-yarn vitest e2e/serial/$1 --config ./e2e/serial/vitest.config.ts --reporter=verbose --bail 1
-
-# Store exit code
-TEST_RESULT=$?
-
-# kill anvil
-echo "Cleaning Up..."
-yarn anvil:kill
-
-# return the result of the tests
-exit "$TEST_RESULT"
+# Return the result of the tests
+exit $TEST_RESULT
