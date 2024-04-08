559401cda541de6c32b63531d7e5272545303046
Daniel Sinclair <d@niel.nyc>
2024-04-08 19:19:29 -0400

    chore: alphabetize TestnetFaucet

--- src/core/utils/faucets.ts
+++ src/core/utils/faucets.ts
@@ -3,17 +3,17 @@ import { moonbaseAlpha, polygonZkEvm, scrollSepolia } from 'viem/chains';
 import { ChainId } from '../types/chains';
 
 export const TestnetFaucet = {
-  [ChainId.sepolia]: 'https://sepoliafaucet.com',
-  [ChainId.holesky]: 'https://faucet.quicknode.com/ethereum/holesky',
-  [ChainId.optimismSepolia]: 'https://app.optimism.io/faucet',
-  [ChainId.bscTestnet]: 'https://www.bnbchain.org/en/testnet-faucet',
   [ChainId.arbitrumSepolia]: 'https://faucet.quicknode.com/arbitrum/sepolia',
-  [ChainId.baseSepolia]: 'https://app.optimism.io/faucet',
-  [ChainId.zoraSepolia]: 'https://app.optimism.io/faucet',
   [ChainId.avalancheFuji]: 'https://faucet.quicknode.com/avalanche/fuji',
+  [ChainId.baseSepolia]: 'https://app.optimism.io/faucet',
   [ChainId.blastSepolia]: 'https://faucet.quicknode.com/blast/sepolia',
-  [ChainId.polygonAmoy]: 'https://faucet.polygon.technology',
+  [ChainId.bscTestnet]: 'https://www.bnbchain.org/en/testnet-faucet',
+  [ChainId.holesky]: 'https://faucet.quicknode.com/ethereum/holesky',
   [moonbaseAlpha.id]: 'https://faucet.paradigm.xyz',
-  [scrollSepolia.id]: 'https://faucet.quicknode.com/scroll/sepolia',
+  [ChainId.optimismSepolia]: 'https://app.optimism.io/faucet',
+  [ChainId.polygonAmoy]: 'https://faucet.polygon.technology',
   [polygonZkEvm.id]: 'https://faucet.polygon.technology',
+  [scrollSepolia.id]: 'https://faucet.quicknode.com/scroll/sepolia',
+  [ChainId.sepolia]: 'https://sepoliafaucet.com',
+  [ChainId.zoraSepolia]: 'https://app.optimism.io/faucet',
 } as const;
