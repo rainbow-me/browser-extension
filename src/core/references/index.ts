305a03207073c84cc27654aafb94336a642b7458
Daniel Sinclair <d@niel.nyc>
2024-04-08 18:44:53 -0400

    chore: alphabetize getDefaultRPC

--- src/core/references/index.ts
+++ src/core/references/index.ts
@@ -264,44 +264,44 @@ export const SUPPORTED_TESTNET_CHAIN_IDS: number[] =
 
 export const getDefaultRPC = (chainId: ChainId) => {
   switch (chainId) {
-    case ChainId.mainnet:
-      return { http: process.env.ETH_MAINNET_RPC };
-    case ChainId.optimism:
-      return { http: process.env.OPTIMISM_MAINNET_RPC };
     case ChainId.arbitrum:
       return { http: process.env.ARBITRUM_MAINNET_RPC };
-    case ChainId.polygon:
-      return { http: process.env.POLYGON_MAINNET_RPC };
-    case ChainId.base:
-      return { http: process.env.BASE_MAINNET_RPC };
-    case ChainId.zora:
-      return { http: process.env.ZORA_MAINNET_RPC };
-    case ChainId.bsc:
-      return { http: process.env.BSC_MAINNET_RPC };
-    case ChainId.sepolia:
-      return { http: process.env.ETH_SEPOLIA_RPC };
-    case ChainId.holesky:
-      return { http: process.env.ETH_HOLESKY_RPC };
-    case ChainId.optimismSepolia:
-      return { http: process.env.OPTIMISM_SEPOLIA_RPC };
-    case ChainId.bscTestnet:
-      return { http: process.env.BSC_TESTNET_RPC };
     case ChainId.arbitrumSepolia:
       return { http: process.env.ARBITRUM_SEPOLIA_RPC };
-    case ChainId.baseSepolia:
-      return { http: process.env.BASE_SEPOLIA_RPC };
-    case ChainId.zoraSepolia:
-      return { http: process.env.ZORA_SEPOLIA_RPC };
     case ChainId.avalanche:
       return { http: process.env.AVALANCHE_MAINNET_RPC };
     case ChainId.avalancheFuji:
       return { http: process.env.AVALANCHE_FUJI_RPC };
+    case ChainId.base:
+      return { http: process.env.BASE_MAINNET_RPC };
+    case ChainId.baseSepolia:
+      return { http: process.env.BASE_SEPOLIA_RPC };
     case ChainId.blast:
       return { http: process.env.BLAST_MAINNET_RPC };
     case ChainId.blastSepolia:
       return { http: process.env.BLAST_SEPOLIA_RPC };
+    case ChainId.bsc:
+      return { http: process.env.BSC_MAINNET_RPC };
+    case ChainId.bscTestnet:
+      return { http: process.env.BSC_TESTNET_RPC };
+    case ChainId.holesky:
+      return { http: process.env.ETH_HOLESKY_RPC };
+    case ChainId.mainnet:
+      return { http: process.env.ETH_MAINNET_RPC };
+    case ChainId.optimism:
+      return { http: process.env.OPTIMISM_MAINNET_RPC };
+    case ChainId.optimismSepolia:
+      return { http: process.env.OPTIMISM_SEPOLIA_RPC };
+    case ChainId.polygon:
+      return { http: process.env.POLYGON_MAINNET_RPC };
     case ChainId.polygonAmoy:
       return { http: process.env.POLYGON_AMOY_RPC };
+    case ChainId.sepolia:
+      return { http: process.env.ETH_SEPOLIA_RPC };
+    case ChainId.zora:
+      return { http: process.env.ZORA_MAINNET_RPC };
+    case ChainId.zoraSepolia:
+      return { http: process.env.ZORA_SEPOLIA_RPC };
     default:
       return null;
   }
