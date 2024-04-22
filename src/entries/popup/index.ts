51646a90d1ed9d6f0d1dc602385ec9e08d8d40de
Esteban Miño <efmino@uc.cl>
2024-04-22 13:51:41 -0400

    initThemingLocal

--- src/entries/popup/index.ts
+++ src/entries/popup/index.ts
@@ -4,11 +4,13 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { syncStores } from '~/core/state/internal/syncStores';
+import { initThemingLocal } from '~/design-system/styles/initThemingLocal';
 
 import { App } from './App';
 
 require('../../core/utils/lockdown');
 
+initThemingLocal();
 syncStores();
 
 const domContainer = document.querySelector('#app') as Element;
