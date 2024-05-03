8f3e60e5e6fe005307374f0b29470da625dac769
gregs <ukfaeku@gmail.com>
2024-05-03 17:55:46 -0300

    remove error store, let the router handle it

--- src/entries/popup/Routes.tsx
+++ src/entries/popup/Routes.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence } from 'framer-motion';
 import * as React from 'react';
 import {
+  Navigate,
   Outlet,
   RouteObject,
   RouterProvider,
@@ -13,13 +14,14 @@ import { analytics } from '~/analytics';
 import { screen } from '~/analytics/screen';
 import { i18n } from '~/core/languages';
 import { shortcuts } from '~/core/references/shortcuts';
-import { useErrorStore } from '~/core/state/error';
 import { useNavRestorationStore } from '~/core/state/navRestoration';
 import { POPUP_DIMENSIONS } from '~/core/utils/dimensions';
+import { goToNewTab } from '~/core/utils/tabs';
 import { Box } from '~/design-system';
-import { Alert } from '~/design-system/components/Alert/Alert';
+import { Alert, triggerAlert } from '~/design-system/components/Alert/Alert';
 import { AnimatedRoute } from '~/design-system/components/AnimatedRoute/AnimatedRoute';
 import { BoxProps } from '~/design-system/components/Box/Box';
+import { RainbowError, logger } from '~/logger';
 
 import { AppConnectionWatcher } from './components/AppConnection/AppConnectionWatcher';
 import { CommandK } from './components/CommandK/CommandK';
@@ -33,7 +35,6 @@ import { WindowStroke } from './components/WindowStroke/WindowStroke';
 import { useCommandKShortcuts } from './hooks/useCommandKShortcuts';
 import useKeyboardAnalytics from './hooks/useKeyboardAnalytics';
 import { useKeyboardShortcut } from './hooks/useKeyboardShortcut';
-import { useRainbowNavigate } from './hooks/useRainbowNavigate';
 import { Buy } from './pages/buy';
 import { CreatePassword } from './pages/createPassword';
 import { Home } from './pages/home';
@@ -967,8 +968,11 @@ const ROUTE_DATA = [
 
 const RootLayout = () => {
   const { pathname, state } = useLocation();
-  const { setLastPage, setLastState, shouldRestoreNavigation } =
-    useNavRestorationStore();
+  const shouldRestoreNavigation = useNavRestorationStore(
+    (s) => s.shouldRestoreNavigation,
+  );
+  const setLastPage = useNavRestorationStore((s) => s.setLastPage);
+  const setLastState = useNavRestorationStore((s) => s.setLastState);
 
   React.useLayoutEffect(() => {
     window.scrollTo(0, 0);
@@ -1058,15 +1062,23 @@ const useGlobalShortcuts = () => {
 };
 
 function Rerouter() {
-  const error = useRouteError() as Error;
-  const navigate = useRainbowNavigate();
-  const { setError } = useErrorStore();
+  const error = useRouteError();
 
-  React.useEffect(() => {
-    setError(error);
-    navigate(ROUTES.HOME, { replace: true });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  triggerAlert({
+    action: () =>
+      goToNewTab({
+        url: 'https://rainbow.me/extension/support?report=true',
+      }),
+    actionText: i18n.t('errors.report_error'),
+    text: i18n.t('errors.error_encountered'),
+  });
 
-  return null;
+  if (error instanceof Error) {
+    logger.error(new RainbowError('Error Boundary Did Catch: '), {
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+
+  return <Navigate to={ROUTES.HOME} />;
 }
