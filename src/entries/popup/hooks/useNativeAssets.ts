607c4d135527adcc2541a576d9ee7cdc020a502f
Esteban Miño <efmino@uc.cl>
2024-06-05 13:55:01 -0400

    using nativeAssetChains

--- src/entries/popup/hooks/useNativeAssets.ts
+++ src/entries/popup/hooks/useNativeAssets.ts
@@ -1,23 +1,15 @@
-import { Address } from 'viem';
-
-import {
-  AVAX_AVALANCHE_ADDRESS,
-  BNB_BSC_ADDRESS,
-  DEGEN_DEGEN_ADDRESS,
-  ETH_ADDRESS,
-  MATIC_POLYGON_ADDRESS,
-} from '~/core/references';
+import { nativeAssetChains } from '~/core/references/chains';
 import { useAssets } from '~/core/resources/assets';
 import { fetchAssets } from '~/core/resources/assets/assets';
 import { currentCurrencyStore, useCurrentCurrencyStore } from '~/core/state';
 import { ChainId } from '~/core/types/chains';
 
 const NATIVE_ASSETS = [
-  { address: ETH_ADDRESS as Address, chainId: ChainId.mainnet },
-  { address: BNB_BSC_ADDRESS as Address, chainId: ChainId.bsc },
-  { address: MATIC_POLYGON_ADDRESS as Address, chainId: ChainId.polygon },
-  { address: AVAX_AVALANCHE_ADDRESS as Address, chainId: ChainId.avalanche },
-  { address: DEGEN_DEGEN_ADDRESS as Address, chainId: ChainId.degen },
+  { address: nativeAssetChains[ChainId.mainnet], chainId: ChainId.mainnet },
+  { address: nativeAssetChains[ChainId.bsc], chainId: ChainId.bsc },
+  { address: nativeAssetChains[ChainId.polygon], chainId: ChainId.polygon },
+  { address: nativeAssetChains[ChainId.avalanche], chainId: ChainId.avalanche },
+  { address: nativeAssetChains[ChainId.degen], chainId: ChainId.degen },
 ];
 export async function getNativeAssets() {
   const { currentCurrency } = currentCurrencyStore.getState();
