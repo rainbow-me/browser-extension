70cbe027f3b59070be5bbca3a3e7875c8c3c54b7
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2024-07-29 15:00:32 -0400

    fix: token details info text selection (#1644)

--- src/entries/popup/pages/home/TokenDetails/TokenDetails.tsx
+++ src/entries/popup/pages/home/TokenDetails/TokenDetails.tsx
@@ -142,7 +142,7 @@ function BalanceValue({
             weight="semibold"
             color={color}
             cursor="text"
-            userSelect="all"
+            userSelect="text"
           >
             {hideAssetBalances ? <HiddenValue /> : balance.value}{' '}
             {balance.symbol}
@@ -160,7 +160,7 @@ function BalanceValue({
             color={color}
             align="right"
             cursor="text"
-            userSelect="all"
+            userSelect="text"
           >
             {getPrice(nativeBalance, chainId)}
           </TextOverflow>
