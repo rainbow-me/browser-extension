f9b793d363b20b1b85860e46509cdb6ad1d63efb
Christopher Howard <derhowiedesigns@gmail.com>
2024-08-19 09:41:14 -0400

    perceived finality (#1649)
    Co-authored-by: Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>

---
+++ src/core/state/staleBalances/index.ts
@@ -0,0 +1,127 @@
+import { Address } from 'viem';
+import create from 'zustand';
+
+import { ChainId } from '~/core/types/chains';
+
+import { createStore } from '../internal/createStore';
+
+const TIME_TO_WATCH = 600000;
+
+interface StaleBalanceInfo {
+  address: Address;
+  expirationTime?: number;
+  transactionHash: string;
+}
+
+interface StaleBalances {
+  [key: Address]: StaleBalanceInfo;
+}
+interface StaleBalancesByChainId {
+  [key: number]: StaleBalances;
+}
+
+export interface StaleBalancesState {
+  addStaleBalance: ({
+    address,
+    chainId,
+    info,
+  }: {
+    address: Address;
+    chainId: ChainId;
+    info: StaleBalanceInfo;
+  }) => void;
+  clearExpiredData: (address: Address) => void;
+  getStaleBalancesQueryParam: (address: Address) => string;
+  staleBalances: Record<Address, StaleBalancesByChainId>;
+}
+
+export const staleBalancesStore = createStore<StaleBalancesState>(
+  (set, get) => ({
+    addStaleBalance: ({
+      address,
+      chainId,
+      info,
+    }: {
+      address: Address;
+      chainId: ChainId;
+      info: StaleBalanceInfo;
+    }) => {
+      set((state) => {
+        const { staleBalances } = state;
+        const staleBalancesForUser = staleBalances[address] || {};
+        const staleBalancesForChain = staleBalancesForUser[chainId] || {};
+        const newStaleBalancesForChain = {
+          ...staleBalancesForChain,
+          [info.address]: {
+            ...info,
+            expirationTime: info.expirationTime || Date.now() + TIME_TO_WATCH,
+          },
+        };
+        const newStaleBalancesForUser = {
+          ...staleBalancesForUser,
+          [chainId]: newStaleBalancesForChain,
+        };
+        return {
+          staleBalances: {
+            ...staleBalances,
+            [address]: newStaleBalancesForUser,
+          },
+        };
+      });
+    },
+    clearExpiredData: (address: Address) => {
+      set((state) => {
+        const { staleBalances } = state;
+        const staleBalancesForUser = staleBalances[address] || {};
+        const newStaleBalancesForUser: StaleBalancesByChainId = {
+          ...staleBalancesForUser,
+        };
+        for (const c of Object.keys(staleBalancesForUser)) {
+          const chainId = parseInt(c, 10);
+          const newStaleBalancesForChain = {
+            ...(staleBalancesForUser[chainId] || {}),
+          };
+          for (const staleBalance of Object.values(newStaleBalancesForChain)) {
+            if (
+              typeof staleBalance.expirationTime === 'number' &&
+              staleBalance.expirationTime <= Date.now()
+            ) {
+              delete newStaleBalancesForChain[staleBalance.address];
+            }
+          }
+          newStaleBalancesForUser[chainId] = newStaleBalancesForChain;
+        }
+        return {
+          staleBalances: {
+            ...staleBalances,
+            [address]: newStaleBalancesForUser,
+          },
+        };
+      });
+    },
+    getStaleBalancesQueryParam: (address: Address) => {
+      let queryStringFragment = '';
+      const { staleBalances } = get();
+      const staleBalancesForUser = staleBalances[address];
+      for (const c of Object.keys(staleBalancesForUser)) {
+        const chainId = parseInt(c, 10);
+        const staleBalancesForChain = staleBalancesForUser[chainId];
+        for (const staleBalance of Object.values(staleBalancesForChain)) {
+          if (typeof staleBalance.expirationTime === 'number') {
+            queryStringFragment += `&token=${chainId}.${staleBalance.address}`;
+          }
+        }
+      }
+      return queryStringFragment;
+    },
+    staleBalances: {},
+  }),
+  {
+    persist: {
+      name: 'staleBalances',
+      version: 0,
+    },
+  },
+);
+
+export const useStaleBalancesStore = create(staleBalancesStore);
