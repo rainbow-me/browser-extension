b2188294441166f022410e5fec80e63ec2bbf51f
gregs <ukfaeku@gmail.com>
2024-09-09 18:07:58 -0300

    fix isNativeAsset

--- src/core/resources/search/parseTokenSearch.ts
+++ src/core/resources/search/parseTokenSearch.ts
@@ -1,25 +1,7 @@
-import { Address } from 'viem';
-
-import {
-  BNB_BSC_ADDRESS,
-  ETH_ADDRESS,
-  MATIC_POLYGON_ADDRESS,
-} from '~/core/references';
+import { AddressOrEth } from '~/core/types/assets';
 import { ChainId } from '~/core/types/chains';
 import { SearchAsset } from '~/core/types/search';
-
-const NATIVE_ASSET_UNIQUE_IDS = [
-  `${ETH_ADDRESS}_${ChainId.mainnet}`,
-  `${ETH_ADDRESS}_${ChainId.optimism}`,
-  `${ETH_ADDRESS}_${ChainId.arbitrum}`,
-  `${BNB_BSC_ADDRESS}_${ChainId.bsc}`,
-  `${MATIC_POLYGON_ADDRESS}_${ChainId.polygon}`,
-  `${ETH_ADDRESS}_${ChainId.base}`,
-  `${ETH_ADDRESS}_${ChainId.zora}`,
-  `${ETH_ADDRESS}_${ChainId.avalanche}`,
-  `${ETH_ADDRESS}_${ChainId.blast}`,
-  `${ETH_ADDRESS}_${ChainId.degen}`,
-];
+import { isNativeAsset } from '~/core/utils/chains';
 
 export function parseTokenSearch(
   asset: SearchAsset,
@@ -32,10 +14,8 @@ export function parseTokenSearch(
     address: networkInfo ? networkInfo.address : asset.address,
     chainId,
     decimals: networkInfo ? networkInfo.decimals : asset.decimals,
-    isNativeAsset: NATIVE_ASSET_UNIQUE_IDS.includes(
-      `${asset.uniqueId}_${chainId}`,
-    ),
-    mainnetAddress: asset.uniqueId as Address,
+    isNativeAsset: isNativeAsset(asset.address, chainId),
+    mainnetAddress: asset.uniqueId as AddressOrEth,
     uniqueId: `${networkInfo?.address || asset.uniqueId}_${chainId}`,
   };
 }
