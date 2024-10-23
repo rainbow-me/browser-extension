380c6a4a3f6c570ef078047929e951da9a0315c2
Daniel Sinclair <d@niel.nyc>
2024-10-22 21:26:43 -0400

    feat: fetch custom networks

--- scripts/networks.js
+++ scripts/networks.js
@@ -1,13 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-
 const fs = require('fs-extra');
 
-/**
- * Fetches data from the GraphQL API and saves it to a JSON file.
- */
-async function fetchData() {
-  const graphqlQuery = `
+/* GraphQL query to fetch mainnet networks data */
+const networksQuery = `
   query getNetworks($device: Device!, $includeTestnets: Boolean!) {
     networks(device: $device, includeTestnets: $includeTestnets) {
       id
@@ -90,17 +85,51 @@ async function fetchData() {
   }
 `;
 
+/* GraphQL query to fetch custom networks data */
+const customNetworksQuery = `
+  query GetCustomNetworks($includeTestnets: Boolean) {
+    customNetworks(includeTestnets: $includeTestnets) {
+      id
+      name
+      iconURL
+      nativeAsset {
+        symbol
+        decimals
+        iconURL
+      }
+      defaultRPCURL
+      defaultExplorerURL
+      testnet {
+        FaucetURL
+        isTestnet
+        mainnetChainID
+      }
+    }
+  }
+`;
+
+/**
+ * Fetches data from the GraphQL API
+ */
+async function fetchData(query) {
   const response = await fetch('https://metadata.p.rainbow.me/v1/graph', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      query: graphqlQuery,
+      query,
       variables: { device: 'BX', includeTestnets: true },
     }),
   });
 
   const { data } = await response.json();
-  const filePath = path.join(__dirname, '../static/data/networks.json');
+  return data;
+}
+
+/**
+ * Persists the network data to a JSON file.
+ */
+async function persistData(fileName, data) {
+  const filePath = path.join(__dirname, `../static/data/${fileName}.json`);
 
   await fs.ensureFile(filePath);
   await fs.writeJson(filePath, data);
@@ -109,8 +138,15 @@ async function fetchData() {
 async function main() {
   try {
     console.log('Fetching networks ...');
-    await fetchData();
-    console.log('Networks data fetched and available.');
+    const { networks } = await fetchData(networksQuery);
+
+    console.log('Fetching custom networks ...');
+    const { customNetworks } = await fetchData(customNetworksQuery);
+
+    console.log('Persisting networks data ...');
+    await persistData('networks', { networks, customNetworks });
+    
+    console.log('Networks data fetched and persisted.');
     process.exit(0);
   } catch (error) {
     console.error('Error fetching networks data:', error);
