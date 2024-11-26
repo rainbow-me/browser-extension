7f2dee3f0bcac2c1cefb973def63be52fd95d4d2
Daniel Sinclair <d@niel.nyc>
2024-11-26 03:47:36 -0500

    refactor: remove getStatus listener

--- src/core/telemetry/index.ts
+++ src/core/telemetry/index.ts
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 
 import { analytics } from '~/analytics';
 import { getWalletContext } from '~/analytics/util';
-import { getStatus } from '~/entries/popup/handlers/wallet';
 import { useAuth } from '~/entries/popup/hooks/useAuth';
 
 import { setSentryUser } from '../sentry';
@@ -26,28 +25,11 @@ export const TelemetryIdentifier = () => {
       analytics.setDeviceId(deviceId);
       analytics.identify();
     };
-
-    // check if the keychain is ready for a full identify
-    const identifyWhenReady = async () => {
-      let status = await getStatus();
-      // if it's not ready, storage can throw and we wouldn't get the ready flag
-      while (!status.ready || !status.unlocked) {
-        // wait till the keychain bootstrap is done
-        // eslint-disable-next-line no-promise-executor-return, no-await-in-loop
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        // eslint-disable-next-line no-await-in-loop
-        status = await getStatus();
-      }
-      await identify();
-    };
-
     // Disable analytics & sentry for e2e and dev mode
     if (process.env.IS_TESTING !== 'true' && process.env.IS_DEV !== 'true') {
-      if (authStatus === 'READY')
-        identifyWhenReady(); // wait for the keychain to be ready for a full identify
-      else if (authStatus !== '')
-        // wait for auth state to settle
-        identify(); // assign partial wallet context immediately if available
+      if (authStatus === '') return; // wait for auth state to settle
+      else if (authStatus === 'READY') identify(); // assign full wallet context
+      else identify(); // assign partial wallet context immediately if available
     }
   }, [deviceId, currentAddress, authStatus]);
 
