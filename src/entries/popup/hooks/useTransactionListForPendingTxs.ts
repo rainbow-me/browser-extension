5337f9770ff33edf372837273b6955143df4ae2e
jinchung <jin.chung17@gmail.com>
2024-12-08 15:33:15 -0500

    Remove unnecessary updating of nonces from pending txn watcher as it clobbers over data during for loop

--- src/entries/popup/hooks/useTransactionListForPendingTxs.ts
+++ src/entries/popup/hooks/useTransactionListForPendingTxs.ts
@@ -5,7 +5,6 @@ import { userAssetsFetchQuery } from '~/core/resources/assets/userAssets';
 import { useConsolidatedTransactions } from '~/core/resources/transactions/consolidatedTransactions';
 import {
   currentCurrencyStore,
-  nonceStore,
   pendingTransactionsStore,
   useCurrentAddressStore,
   useCurrentCurrencyStore,
@@ -70,47 +69,13 @@ function watchForPendingTransactionsReportedByRainbowBackend({
   currentAddress: Address;
   latestTransactions: Map<ChainId, RainbowTransaction | null>;
 }) {
-  const { setNonce } = nonceStore.getState();
   const {
     setPendingTransactions,
     pendingTransactions: storePendingTransactions,
   } = pendingTransactionsStore.getState();
   const pendingTransactions = storePendingTransactions[currentAddress] || [];
-  const supportedChainIds = getSupportedChains({
-    testnets: false,
-  }).map(({ id }) => id);
   const { addStaleBalance } = staleBalancesStore.getState();
   const { currentCurrency } = currentCurrencyStore.getState();
-  for (const supportedChainId of supportedChainIds) {
-    const latestTxConfirmedByBackend = latestTransactions.get(supportedChainId);
-    if (latestTxConfirmedByBackend) {
-      const latestNonceConfirmedByBackend =
-        latestTxConfirmedByBackend.nonce || 0;
-
-      const latestPendingTx = pendingTransactions.filter(
-        (tx) => tx?.chainId === supportedChainId,
-      )[-1];
-
-      let currentNonce;
-      if (latestPendingTx) {
-        const latestPendingNonce = latestPendingTx?.nonce || 0;
-        const latestTransactionIsPending =
-          latestPendingNonce > latestNonceConfirmedByBackend;
-        currentNonce = latestTransactionIsPending
-          ? latestPendingNonce
-          : latestNonceConfirmedByBackend;
-      } else {
-        currentNonce = latestNonceConfirmedByBackend;
-      }
-
-      setNonce({
-        address: currentAddress,
-        chainId: supportedChainId,
-        currentNonce,
-        latestConfirmedNonce: latestNonceConfirmedByBackend,
-      });
-    }
-  }
 
   const newlyConfirmedTransactions: RainbowTransaction[] = [];
   const updatedPendingTransactions: RainbowTransaction[] = [];
