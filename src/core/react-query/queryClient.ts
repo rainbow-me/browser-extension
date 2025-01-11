e8cfab562a01ed88398c045ad75065a80b27a9f7
Daniel Sinclair <d@niel.nyc>
2025-01-10 19:57:56 -0500

    persistence fetching/setting logging in dev mode
    chore: tweak naming

--- src/core/react-query/queryClient.ts
+++ src/core/react-query/queryClient.ts
@@ -4,6 +4,8 @@ import { PersistQueryClientOptions } from '@tanstack/react-query-persist-client'
 
 import { LocalStorage } from '../storage';
 
+const IS_DEV = process.env.IS_DEV === 'true';
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -18,13 +20,30 @@ export const queryClient = new QueryClient({
   },
 });
 
+const loggingPersisterStorage = {
+  getItem: async (key: string) => {
+    console.log(`[react-query persister] get: ${key}`);
+    return LocalStorage.get(key);
+  },
+  setItem: async (key: string, value: string) => {
+    console.log(`[react-query persister] set: ${key}`);
+    return LocalStorage.set(key, value);
+  },
+  removeItem: async (key: string) => {
+    console.log(`[react-query persister] remove: ${key}`);
+    return LocalStorage.remove(key);
+  },
+};
+
 const asyncStoragePersister = createAsyncStoragePersister({
   key: 'rainbow.react-query',
-  storage: {
-    getItem: LocalStorage.get,
-    setItem: LocalStorage.set,
-    removeItem: LocalStorage.remove,
-  },
+  storage: IS_DEV
+    ? loggingPersisterStorage
+    : {
+        getItem: LocalStorage.get,
+        setItem: LocalStorage.set,
+        removeItem: LocalStorage.remove,
+      },
   throttleTime: 10000,
 });
 
