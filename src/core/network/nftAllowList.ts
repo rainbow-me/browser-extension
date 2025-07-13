f187d28f8a4263f6bcf47a134783bb7e4962a74f
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2025-07-13 00:40:44 -0400

    chore: remove deprecated allow list client

--- src/core/network/nftAllowList.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated The Polygon allow list endpoint is deprecated and the client is no longer used.
- */
-import { createHttpClient } from './internal/createHttpClient';
-
-export const nftAllowListClient = createHttpClient({
-  baseUrl: 'https://metadata.p.rainbow.me/token-list',
-});
