71c8652d78e68d6fd6e408d7d068c1558579d0a5
Janek <janek.rahrt@me.com>
2025-07-23 08:19:19 +0200

    fix: handle loading for activity details (#1945)

--- src/entries/popup/pages/home/Skeletons.tsx
+++ src/entries/popup/pages/home/Skeletons.tsx
@@ -116,3 +116,63 @@ export function ActivitySkeleton() {
     </Box>
   );
 }
+
+export function ActivityDetailsContentSkeleton() {
+  return (
+    <>
+      {/* ToFrom section */}
+      <Stack space="24px">
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="60px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="120px" height="12px" />
+        </Box>
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="40px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="120px" height="12px" />
+        </Box>
+      </Stack>
+
+      {/* ConfirmationData section */}
+      <Stack space="24px">
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="40px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="140px" height="12px" />
+        </Box>
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="80px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="100px" height="12px" />
+        </Box>
+      </Stack>
+
+      {/* NetworkData section */}
+      <Stack space="24px">
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="50px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="80px" height="12px" />
+        </Box>
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="60px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="90px" height="12px" />
+        </Box>
+        <Box display="flex" alignItems="center" gap="8px">
+          <Skeleton circle width="16px" height="16px" />
+          <Skeleton width="30px" height="12px" />
+          <Box flexGrow="1" />
+          <Skeleton width="70px" height="12px" />
+        </Box>
+      </Stack>
+    </>
+  );
+}
