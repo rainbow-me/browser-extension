855e13cd0e2c53b1d23e219eff4aa7c30e5593bd
Daniel Sinclair <d@niel.nyc>
2025-07-23 01:28:26 -0400

    fix: nft tests follow remote config

--- e2e/parallel/nftsFlow.test.ts
+++ e2e/parallel/nftsFlow.test.ts
@@ -4,6 +4,8 @@ import 'geckodriver';
 import { WebDriver } from 'selenium-webdriver';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
+import config from '~/core/firebase/remoteConfig';
+
 import {
   delayTime,
   findElementByTestId,
@@ -23,132 +25,139 @@ let driver: WebDriver;
 const browser = process.env.BROWSER || 'chrome';
 const os = process.env.OS || 'mac';
 
-describe('Visit NFTs Gallery and Details Pages', () => {
-  beforeAll(async () => {
-    driver = await initDriverWithOptions({
-      browser,
-      os,
+describe.skipIf(!config.nfts_enabled)(
+  'Visit NFTs Gallery and Details Pages',
+  () => {
+    beforeAll(async () => {
+      driver = await initDriverWithOptions({
+        browser,
+        os,
+      });
+      const extensionId = await getExtensionIdByName(driver, 'Rainbow');
+      if (!extensionId) throw new Error('Extension not found');
+      rootURL += extensionId;
     });
-    const extensionId = await getExtensionIdByName(driver, 'Rainbow');
-    if (!extensionId) throw new Error('Extension not found');
-    rootURL += extensionId;
-  });
-  afterAll(async () => await driver.quit());
+    afterAll(async () => await driver.quit());
 
-  it('should be able import a wallet via seed', async () => {
-    await importWalletFlow(driver, rootURL, TEST_VARIABLES.EMPTY_WALLET.SECRET);
-  });
-
-  it('should be able to navigate to NFT details', async () => {
-    await goToPopup(driver, rootURL);
-    await findElementByTestIdAndClick({ id: 'bottom-tab-nfts', driver });
-    await delayTime('short');
-    await findElementByTestIdAndClick({
-      id: 'nft-thumbnail-https://lh3.googleusercontent.com/ICZJsSKEfLTquCvn1o-W7wD75EdtCksjf3bMtm2IQsYdw7K8-_de9gGQBXJE09fHy33OtBBrgWqMUAfX2ve6ZsW200JnLrX-m3s=s1000-0',
-      driver,
+    it('should be able import a wallet via seed', async () => {
+      await importWalletFlow(
+        driver,
+        rootURL,
+        TEST_VARIABLES.EMPTY_WALLET.SECRET,
+      );
     });
-    const nftName = await findElementByText(driver, 'Citizen 297');
-    expect(nftName).toBeTruthy();
-  });
 
-  it('should display last sales price', async () => {
-    const lastSalesPrice = await findElementByText(driver, '0.0179');
-    expect(lastSalesPrice).toBeTruthy();
-  });
-
-  it('should display NFT description', async () => {
-    const description = await findElementByText(
-      driver,
-      'Bring your citizen to life',
-    );
-    expect(description).toBeTruthy();
-  });
-
-  it('should display NFT traits', async () => {
-    const bodyTrait = await findElementByText(driver, 'ALPINEMIKI');
-    expect(bodyTrait).toBeTruthy();
-    const materialTrait = await findElementByText(driver, 'ORANGE');
-    expect(materialTrait).toBeTruthy();
-  });
-
-  it('should display token standard', async () => {
-    const tokenStandard = await findElementByText(driver, 'ERC721');
-    expect(tokenStandard).toBeTruthy();
-  });
-
-  it('should display token contract', async () => {
-    const contractAddress = await findElementByText(driver, '0x6171…f23b');
-    expect(contractAddress).toBeTruthy();
-  });
-
-  it('should display contract creator', async () => {
-    const creatorAddress = await findElementByText(driver, 'adworld.eth');
-    expect(creatorAddress).toBeTruthy();
-  });
-
-  it('should display chain', async () => {
-    const networkName = await findElementByText(driver, 'Base');
-    expect(networkName).toBeTruthy();
-  });
-
-  it('should correctly display external link', async () => {
-    const externalLinkButton = await findElementByTestId({
-      id: 'nft-link-button-adworld.game',
-      driver,
+    it('should be able to navigate to NFT details', async () => {
+      await goToPopup(driver, rootURL);
+      await findElementByTestIdAndClick({ id: 'bottom-tab-nfts', driver });
+      await delayTime('short');
+      await findElementByTestIdAndClick({
+        id: 'nft-thumbnail-https://lh3.googleusercontent.com/ICZJsSKEfLTquCvn1o-W7wD75EdtCksjf3bMtm2IQsYdw7K8-_de9gGQBXJE09fHy33OtBBrgWqMUAfX2ve6ZsW200JnLrX-m3s=s1000-0',
+        driver,
+      });
+      const nftName = await findElementByText(driver, 'Citizen 297');
+      expect(nftName).toBeTruthy();
     });
-    expect(externalLinkButton).toBeTruthy();
-  });
 
-  it('should return back to gallery and select another NFT (ENS)', async () => {
-    await findElementByTestIdAndClick({
-      id: 'navbar-button-with-back',
-      driver,
+    it('should display last sales price', async () => {
+      const lastSalesPrice = await findElementByText(driver, '0.0179');
+      expect(lastSalesPrice).toBeTruthy();
     });
-    await delayTime('medium');
-    await findElementByTestIdAndClick({
-      id: 'nft-thumbnail-https://lh3.googleusercontent.com/O_dtxR4ggdzoCNEAZ89s7w5eBiu8rP5TELBQcuFZyIHc-raU2qj48LSkJmEKeN64JaGa7m9X5EFYUreCCJBlx9lXW0rgjrZUL0E=s1000-1',
-      driver,
-    });
-    const ensName = findElementByText(driver, 'testmar27.eth');
-    expect(ensName).toBeTruthy();
-  });
 
-  it('should display ens registration date accurately', async () => {
-    const registeredLabel = await findElementByText(driver, 'Registered on');
-    expect(registeredLabel).toBeTruthy();
-    const registeredDate = await findElementByText(driver, 'Mar 27, 2023');
-    expect(registeredDate).toBeTruthy();
-  });
+    it('should display NFT description', async () => {
+      const description = await findElementByText(
+        driver,
+        'Bring your citizen to life',
+      );
+      expect(description).toBeTruthy();
+    });
 
-  it('should display and format ens expiration date', async () => {
-    const expirationLabel = await findElementByText(driver, 'Expires in');
-    expect(expirationLabel).toBeTruthy();
-    const expirationValue = await findElementByTestId({
-      id: 'ens-expiry-value',
-      driver,
+    it('should display NFT traits', async () => {
+      const bodyTrait = await findElementByText(driver, 'ALPINEMIKI');
+      expect(bodyTrait).toBeTruthy();
+      const materialTrait = await findElementByText(driver, 'ORANGE');
+      expect(materialTrait).toBeTruthy();
     });
-    expect(expirationValue).toBeTruthy();
-    const expirationValueText = await expirationValue.getText();
-    expect(expirationValueText !== 'Invalid Date');
-  });
 
-  it('should be able to change nfts display mode', async () => {
-    await findElementByTestIdAndClick({
-      id: 'navbar-button-with-back',
-      driver,
+    it('should display token standard', async () => {
+      const tokenStandard = await findElementByText(driver, 'ERC721');
+      expect(tokenStandard).toBeTruthy();
     });
-    await findElementByTestIdAndClick({
-      id: 'nfts-displaymode-dropdown',
-      driver,
+
+    it('should display token contract', async () => {
+      const contractAddress = await findElementByText(driver, '0x6171…f23b');
+      expect(contractAddress).toBeTruthy();
     });
-    await findElementByTestIdAndClick({
-      id: 'nfts-displaymode-option-byCollection',
-      driver,
+
+    it('should display contract creator', async () => {
+      const creatorAddress = await findElementByText(driver, 'adworld.eth');
+      expect(creatorAddress).toBeTruthy();
     });
-    const emptyState = await findElementByText(
-      driver,
-      'Your NFTs will appear here!',
-    );
-    expect(emptyState).toBeTruthy();
-  });
-});
+
+    it('should display chain', async () => {
+      const networkName = await findElementByText(driver, 'Base');
+      expect(networkName).toBeTruthy();
+    });
+
+    it('should correctly display external link', async () => {
+      const externalLinkButton = await findElementByTestId({
+        id: 'nft-link-button-adworld.game',
+        driver,
+      });
+      expect(externalLinkButton).toBeTruthy();
+    });
+
+    it('should return back to gallery and select another NFT (ENS)', async () => {
+      await findElementByTestIdAndClick({
+        id: 'navbar-button-with-back',
+        driver,
+      });
+      await delayTime('medium');
+      await findElementByTestIdAndClick({
+        id: 'nft-thumbnail-https://lh3.googleusercontent.com/O_dtxR4ggdzoCNEAZ89s7w5eBiu8rP5TELBQcuFZyIHc-raU2qj48LSkJmEKeN64JaGa7m9X5EFYUreCCJBlx9lXW0rgjrZUL0E=s1000-1',
+        driver,
+      });
+      const ensName = findElementByText(driver, 'testmar27.eth');
+      expect(ensName).toBeTruthy();
+    });
+
+    it('should display ens registration date accurately', async () => {
+      const registeredLabel = await findElementByText(driver, 'Registered on');
+      expect(registeredLabel).toBeTruthy();
+      const registeredDate = await findElementByText(driver, 'Mar 27, 2023');
+      expect(registeredDate).toBeTruthy();
+    });
+
+    it('should display and format ens expiration date', async () => {
+      const expirationLabel = await findElementByText(driver, 'Expires in');
+      expect(expirationLabel).toBeTruthy();
+      const expirationValue = await findElementByTestId({
+        id: 'ens-expiry-value',
+        driver,
+      });
+      expect(expirationValue).toBeTruthy();
+      const expirationValueText = await expirationValue.getText();
+      expect(expirationValueText !== 'Invalid Date');
+    });
+
+    it('should be able to change nfts display mode', async () => {
+      await findElementByTestIdAndClick({
+        id: 'navbar-button-with-back',
+        driver,
+      });
+      await findElementByTestIdAndClick({
+        id: 'nfts-displaymode-dropdown',
+        driver,
+      });
+      await findElementByTestIdAndClick({
+        id: 'nfts-displaymode-option-byCollection',
+        driver,
+      });
+      const emptyState = await findElementByText(
+        driver,
+        'Your NFTs will appear here!',
+      );
+      expect(emptyState).toBeTruthy();
+    });
+  },
+);
--- e2e/serial/send/4_nft-sendFlow.test.ts
+++ e2e/serial/send/4_nft-sendFlow.test.ts
@@ -11,6 +11,8 @@ import {
   it,
 } from 'vitest';
 
+import config from '~/core/firebase/remoteConfig';
+
 import {
   delayTime,
   doNotFindElementByTestId,
@@ -36,106 +38,109 @@ let driver: WebDriver;
 const browser = process.env.BROWSER || 'chrome';
 const os = process.env.OS || 'mac';
 
-describe('should be able to perform the nft send flow', () => {
-  beforeAll(async () => {
-    driver = await initDriverWithOptions({
-      browser,
-      os,
+describe.skipIf(!config.nfts_enabled)(
+  'should be able to perform the nft send flow',
+  () => {
+    beforeAll(async () => {
+      driver = await initDriverWithOptions({
+        browser,
+        os,
+      });
+      const extensionId = await getExtensionIdByName(driver, 'Rainbow');
+      if (!extensionId) throw new Error('Extension not found');
+      rootURL += extensionId;
     });
-    const extensionId = await getExtensionIdByName(driver, 'Rainbow');
-    if (!extensionId) throw new Error('Extension not found');
-    rootURL += extensionId;
-  });
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  beforeEach(async (context: any) => {
-    context.driver = driver;
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  afterEach(async (context: any) => {
-    await takeScreenshotOnFailure(context);
-  });
-
-  afterAll(() => driver.quit());
-
-  it('should be able import a wallet via pk', async () => {
-    await importWalletFlow(driver, rootURL, TEST_VARIABLES.SEED_WALLET.PK);
-  });
-
-  it('should be able to go to settings', async () => {
-    await goToPopup(driver, rootURL);
-    await findElementByTestIdAndClick({
-      id: 'home-page-header-right',
-      driver,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    beforeEach(async (context: any) => {
+      context.driver = driver;
     });
-    await findElementByTestIdAndClick({ id: 'settings-link', driver });
-  });
 
-  it('should be able to connect to hardhat and go to send flow', async () => {
-    const btn = await querySelector(
-      driver,
-      '[data-testid="connect-to-hardhat"]',
-    );
-    await waitAndClick(btn, driver);
-    const button = await findElementByText(driver, 'Disconnect from Hardhat');
-    expect(button).toBeTruthy();
-    await findElementByTestIdAndClick({
-      id: 'navbar-button-with-back',
-      driver,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    afterEach(async (context: any) => {
+      await takeScreenshotOnFailure(context);
     });
-  });
 
-  it('should be able to filter nfts and make selection on send flow', async () => {
-    await findElementByTestIdAndClick({ id: 'bottom-tab-nfts', driver });
-    await delayTime('very-long');
-    const NFTsort = await findElementByTestId({
-      id: 'nfts-displaymode-dropdown',
-      driver,
-    });
-    expect(NFTsort).toBeTruthy();
-    await findElementByTestIdAndClick({ id: 'header-link-send', driver });
-    const input = await findElementByTestId({
-      id: 'to-address-input',
-      driver,
-    });
-    await input.sendKeys('rainbowwallet.eth');
+    afterAll(() => driver.quit());
 
-    await findElementByTestIdAndClick({
-      id: 'input-wrapper-dropdown-token-input',
-      driver,
+    it('should be able import a wallet via pk', async () => {
+      await importWalletFlow(driver, rootURL, TEST_VARIABLES.SEED_WALLET.PK);
     });
-    const assetInput = await findElementByTestId({
-      id: 'token-input',
-      driver,
-    });
-    await assetInput.click();
-    await assetInput.sendKeys('poap');
-    const poapSection = await findElementByTestId({
-      id: 'nfts-collection-section-POAP',
-      driver,
-    });
-    const learnWeb3Badges = await doNotFindElementByTestId({
-      id: 'nfts-collection-section-LearnWeb3 Badges',
-      driver,
-    });
-    expect(poapSection).toBeTruthy();
-    expect(learnWeb3Badges).toBeFalsy();
 
-    await poapSection.click();
-    await findElementByTextAndClick(driver, '#7054159');
-  });
-
-  it('should be able to go to review on send flow', async () => {
-    await findElementByTestIdAndClick({ id: 'send-review-button', driver });
-  });
-
-  it('should be able to send transaction on review on send flow', async () => {
-    await findElementByTestIdAndClick({
-      id: 'review-confirm-button',
-      driver,
+    it('should be able to go to settings', async () => {
+      await goToPopup(driver, rootURL);
+      await findElementByTestIdAndClick({
+        id: 'home-page-header-right',
+        driver,
+      });
+      await findElementByTestIdAndClick({ id: 'settings-link', driver });
     });
-    const sendTransaction = await transactionStatus();
-    expect(await sendTransaction).toBe('success');
-  });
-});
+
+    it('should be able to connect to hardhat and go to send flow', async () => {
+      const btn = await querySelector(
+        driver,
+        '[data-testid="connect-to-hardhat"]',
+      );
+      await waitAndClick(btn, driver);
+      const button = await findElementByText(driver, 'Disconnect from Hardhat');
+      expect(button).toBeTruthy();
+      await findElementByTestIdAndClick({
+        id: 'navbar-button-with-back',
+        driver,
+      });
+    });
+
+    it('should be able to filter nfts and make selection on send flow', async () => {
+      await findElementByTestIdAndClick({ id: 'bottom-tab-nfts', driver });
+      await delayTime('very-long');
+      const NFTsort = await findElementByTestId({
+        id: 'nfts-displaymode-dropdown',
+        driver,
+      });
+      expect(NFTsort).toBeTruthy();
+      await findElementByTestIdAndClick({ id: 'header-link-send', driver });
+      const input = await findElementByTestId({
+        id: 'to-address-input',
+        driver,
+      });
+      await input.sendKeys('rainbowwallet.eth');
+
+      await findElementByTestIdAndClick({
+        id: 'input-wrapper-dropdown-token-input',
+        driver,
+      });
+      const assetInput = await findElementByTestId({
+        id: 'token-input',
+        driver,
+      });
+      await assetInput.click();
+      await assetInput.sendKeys('poap');
+      const poapSection = await findElementByTestId({
+        id: 'nfts-collection-section-POAP',
+        driver,
+      });
+      const learnWeb3Badges = await doNotFindElementByTestId({
+        id: 'nfts-collection-section-LearnWeb3 Badges',
+        driver,
+      });
+      expect(poapSection).toBeTruthy();
+      expect(learnWeb3Badges).toBeFalsy();
+
+      await poapSection.click();
+      await findElementByTextAndClick(driver, '#7054159');
+    });
+
+    it('should be able to go to review on send flow', async () => {
+      await findElementByTestIdAndClick({ id: 'send-review-button', driver });
+    });
+
+    it('should be able to send transaction on review on send flow', async () => {
+      await findElementByTestIdAndClick({
+        id: 'review-confirm-button',
+        driver,
+      });
+      const sendTransaction = await transactionStatus();
+      expect(await sendTransaction).toBe('success');
+    });
+  },
+);
