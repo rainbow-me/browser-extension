72520bfdbce645205591220495e4260ba1878e46
Daniel Sinclair <d@niel.nyc>
2025-08-04 17:13:33 -0400

    chore: remove chromedriver, geckodriver imports

--- e2e/parallel/commandKFlow.test.ts
+++ e2e/parallel/commandKFlow.test.ts
@@ -1,6 +1,4 @@
 /* eslint-disable no-await-in-loop */
-import 'chromedriver';
-import 'geckodriver';
 import { WebDriver } from 'selenium-webdriver';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
