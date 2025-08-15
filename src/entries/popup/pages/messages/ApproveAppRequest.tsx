a09b84cf4a2f66849a72442fb50a3b76b9bbdbe0
Janek <janek.rahrt@me.com>
2025-08-15 10:50:51 +0200

    fix: cleanup

--- src/entries/popup/pages/messages/ApproveAppRequest.tsx
+++ src/entries/popup/pages/messages/ApproveAppRequest.tsx
@@ -86,7 +86,6 @@ export const ApproveAppRequest = () => {
         notificationWindows?.[
           Number(pendingRequest?.meta?.sender?.tab?.id)?.toString()
         ];
-      console.log('pendingRequests.length', pendingRequests.length);
       if (
         !preventWindowClose &&
         pendingRequests.length <= 1 &&
