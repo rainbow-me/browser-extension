c29a9dbe4a5acaab6620c1b74f7da2507c0771e9
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2025-08-15 11:23:05 -0500

    test

--- src/core/resources/assets/userAssets.ts
+++ src/core/resources/assets/userAssets.ts
@@ -264,7 +264,7 @@ export function useUserAssets<TSelectResult = UserAssetsResult>(
     queryFn: userAssetsQueryFunction,
     ...config,
     refetchInterval: USER_ASSETS_REFETCH_INTERVAL,
-    staleTime: 1000,
+    staleTime: process.env.IS_TESTING === 'true' ? 0 : 1000,
     placeholderData: (previousData) => previousData,
   });
 }
