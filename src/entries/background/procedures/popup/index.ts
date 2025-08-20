423f143d047ef561d54657981ee7d9a8ad4e3332
Janek <janek.rahrt@me.com>
2025-08-20 16:16:52 +0200

    keep-alive to orpc

--- src/entries/background/procedures/popup/index.ts
+++ src/entries/background/procedures/popup/index.ts
@@ -1,6 +1,7 @@
 import { os } from '@orpc/server';
 import { RPCHandler } from '@orpc/server/message-port';
 
+import { healthRouter } from './health';
 import { walletOs } from './os';
 import { stateRouter } from './state';
 import { walletRouter } from './wallet';
@@ -8,6 +9,7 @@ import { walletRouter } from './wallet';
 export const popupRouter = {
   wallet: walletOs.router(walletRouter),
   state: os.router(stateRouter),
+  health: os.router(healthRouter),
 };
 
 export type PopupRouter = typeof popupRouter;
