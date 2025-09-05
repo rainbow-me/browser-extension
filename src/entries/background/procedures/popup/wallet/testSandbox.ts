b9720af145108fd3a935d410f6b956190861e276
Daniel Sinclair <d@niel.nyc>
2025-09-05 17:31:10 -0400

    fix: use logger over console

--- src/entries/background/procedures/popup/wallet/testSandbox.ts
+++ src/entries/background/procedures/popup/wallet/testSandbox.ts
@@ -1,11 +1,13 @@
+import { logger } from '~/logger';
+
 import { walletOs } from '../os';
 
 export const testSandboxHandler = walletOs.testSandbox.handler(async () => {
   try {
-    console.log('about to leak...');
+    logger.warn('about to leak...');
     const r = await fetch('https://api.ipify.org?format=json');
     const res = await r.json();
-    console.log('response from server after leaking', res);
+    logger.warn('response from server after leaking', res);
     return 'Background leaked!';
   } catch (e) {
     return 'Background sandboxed!';
