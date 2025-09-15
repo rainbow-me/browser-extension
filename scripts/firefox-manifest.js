50f923d5fc0bb1513c7d548260e59b32203b86a5
Daniel Sinclair <d@niel.nyc>
2025-09-15 12:04:54 -0400

    fix: remove gecko_android from manifest to prevent listed android support

--- scripts/firefox-manifest.js
+++ scripts/firefox-manifest.js
@@ -11,11 +11,6 @@ const manifestFF = {
         "id": "browserextension@rainbow.me",
         "strict_min_version": "116.0"
       },
-      // We don't want to allow sideloading on Firefox Android
-      // Don't support version >120 where Add-ons were introduced
-      "gecko_android": {
-        "strict_max_version": "119.*"
-      },
     },
     host_permissions: [
       ...manifestBase.host_permissions,
