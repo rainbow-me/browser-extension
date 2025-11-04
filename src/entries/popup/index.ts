9245ee8f50421b2344d5d34323e4426cb54f1661
Janek <janek.rahrt@me.com>
2025-11-04 12:21:28 +0800

    feat: migrate asset and chain data stores to stores package
    Migrate asset and chain data stores from createRainbowStore to createBaseStore:
    - rainbowChains (version 13, has onRehydrateStorage hook)
    - userChains (version 8, has onRehydrateStorage hook)
    - rainbowChainAssets (version 2, has migrate function)
    - staleBalances (version 0)
    - nfts (version 1)
    - dappMetadata (version 0)
    - dominantColor (version 0)
    - degenMode (version 1)
    - walletBackups (version 0)
    - networks/migration (version 0, has onRehydrateStorage hook)
    - gas (version 0)
    
    Ephemeral stores (no persistence):
    - selectedToken
    - selectedNft
    - swapAssetsToRefresh
    - walletBackupReminder
    
    Also migrated useNetworkStore (query store) from internal createQueryStore to external stores package with createExtensionStoreOptions.
    
    All stores now use createBaseStore/createQueryStore with createExtensionStoreOptions for consistency, except ephemeral stores which remain ephemeral.

--- src/entries/popup/index.ts
+++ src/entries/popup/index.ts
@@ -3,10 +3,7 @@ import './global.css';
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import {
-  syncNetworksStore,
-  syncStores,
-} from '~/core/state/internal/syncStores';
+import { syncStores } from '~/core/state/internal/syncStores';
 import { initThemingLocal } from '~/design-system/styles/initThemingLocal';
 
 import { App } from './App';
@@ -15,7 +12,6 @@ require('../../core/utils/lockdown');
 
 initThemingLocal();
 syncStores();
-syncNetworksStore('popup');
 
 if (process.env.IS_TESTING === 'true') {
   await import('../../../e2e/mockFetch').then((m) => m.mockFetch());
