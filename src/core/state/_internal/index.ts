f67f91b5549a040e8115d9af28ab3e7ed236e8d8
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-12-09 15:46:58 -0500

    Update `stores` package, reuse sync/storage adapters

--- src/core/state/_internal/index.ts
+++ src/core/state/_internal/index.ts
@@ -1,35 +1,57 @@
 import { BaseStoreOptions, SyncOption } from 'stores';
-
-import { ChromeExtensionSyncEngine } from './chromeExtensionSyncEngine';
-import { ChromeStorageAdapter } from './chromeStorageAdapter';
+import { AreaName, createSyncedChromeStorage } from 'stores/plugins/chrome';
 
 type CustomExtensionOptions = {
-  area?: 'local' | 'session' | 'sync' | 'managed';
+  area?: AreaName;
   useRainbowNamingSchema?: boolean;
 };
 
-export const createExtensionStoreOptions = <T>({
+type SyncedStorage = ReturnType<typeof createSyncedChromeStorage>;
+
+const storageCache = new Map<string, SyncedStorage>();
+
+export const createExtensionStoreOptions = <S>({
   area = 'local',
   useRainbowNamingSchema = true,
   storageKey,
   ...options
-}: BaseStoreOptions<T, Partial<T>, Promise<void>> &
-  CustomExtensionOptions): BaseStoreOptions<T, Partial<T>, Promise<void>> => {
+}: BaseStoreOptions<S, Partial<S>, Promise<void>> &
+  CustomExtensionOptions): BaseStoreOptions<S, Partial<S>, Promise<void>> => {
   if (!storageKey) {
-    throw new Error('storageKey is required');
+    throw new Error('[createExtensionStoreOptions]: storageKey is required');
   }
-  const storage = new ChromeStorageAdapter({
+
+  const { storage, syncEngine } = getSyncedStorage({
     area,
     namespace: useRainbowNamingSchema ? 'rainbow.zustand.' : undefined,
   });
-  const syncEngine = new ChromeExtensionSyncEngine({ storage });
 
   return {
     ...options,
     storage,
     storageKey,
-    sync: { engine: syncEngine } as unknown as T extends Record<string, unknown>
-      ? SyncOption<T>
+    sync: (options.sync && typeof options.sync === 'object'
+      ? { ...options.sync, engine: syncEngine }
+      : { engine: syncEngine }) as unknown as S extends Record<string, unknown>
+      ? SyncOption<S>
       : never,
   };
 };
+
+function getSyncedStorage(options: {
+  area: AreaName;
+  namespace: string | undefined;
+}): SyncedStorage {
+  const key = buildStorageKey(options.area, options.namespace);
+  const cached = storageCache.get(key);
+  if (cached) return cached;
+
+  const syncedStorage = createSyncedChromeStorage(options);
+  storageCache.set(key, syncedStorage);
+  return syncedStorage;
+}
+
+function buildStorageKey(area: AreaName, namespace: string | undefined) {
+  if (!namespace) return area;
+  return `${area}.${namespace}`;
+}
